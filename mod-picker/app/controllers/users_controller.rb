ba76cf9a6fb59a5612b3c11bef04b12c8fde161d
Mator <why101@msn.com>
2016-01-13 13:12:20 -0800

    Cleaned up some unneeded comments in users_controller

--- mod-picker/app/controllers/users_controller.rb
+++ mod-picker/app/controllers/users_controller.rb
@@ -54,9 +54,6 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    #@user.bio.destroy
-    #@user.settings.destroy
-    #@user.reputation.destroy
     @user.destroy
     respond_to do |format|
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
