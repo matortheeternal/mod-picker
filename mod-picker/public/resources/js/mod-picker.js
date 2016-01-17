c09a9595e27f483e38123bbf6e219d4183b059c6
r79 <r@r79.io>
2016-01-17 16:34:07 +0100

    ng-route working, search has been HTML-Templated, everything needs some CSS though

---
+++ mod-picker/public/resources/js/mod-picker.js
@@ -0,0 +1,63 @@
+var app = angular.module('modPicker', [
+    'ngRoute'
+])
+
+.config(['$routeProvider',
+    function($routeProvider){
+        $routeProvider.
+            when('/search', {
+                templateUrl: '/resources/partials/search.html',
+                controller: 'searchController'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    }])
+
+.controller('mainController', function($scope) {
+        $scope.testMessage = "Test works";
+})
+
+.controller('searchController', function($scope){
+        $scope.options = {
+            updateRanges: [
+                {
+                    id: 1,
+                    text: 'Skyrim'
+                },
+                {
+                    id: 2,
+                    text: 'Dawnguard'
+                }
+            ]
+        };
+
+        $scope.search = {};
+
+        function processSearch() {
+            console.log('searchQuery: name=\'' + $scope.search.name + '\' | range: \'' + $scope.search.lastUpdated + '\'');
+            $scope.results = [
+                {
+                    link: '',
+                    text: 'CBBE'
+                },
+                {
+                    link: '',
+                    text: 'Sky UI'
+                },
+                {
+                    link: '',
+                    text: 'Skyfart: Replace Shouts with Farts'
+                },
+                {
+                    link: '',
+                    text: 'immersive Smells'
+                },
+                {
+                    link: '',
+                    text: 'Neo from Matrix as Follower'
+                }
+            ]
+        }
+        $scope.processSearch = processSearch;
+});
