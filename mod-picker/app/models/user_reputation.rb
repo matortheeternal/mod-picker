fb6a0553892962ae147519233b715de08cb617d6
Mator <why101@msn.com>
2016-01-18 17:00:48 -0800

    added belongs_to to user_bio, user_reputation, and user_setting

--- mod-picker/app/models/user_reputation.rb
+++ mod-picker/app/models/user_reputation.rb
@@ -1,12 +1,14 @@
 class UserReputation < ActiveRecord::Base
   after_initialize :init
+
+  belongs_to :user
   
   def init
-    self.overall ||= 5.0;
-    self.offset ||= 0;
-    self.audiovisual_design ||= 0;
-    self.plugin_design ||= 0;
-    self.utility_design ||= 0;
-    self.script_design ||= 0;
+    self.overall ||= 5.0
+    self.offset ||= 0
+    self.audiovisual_design ||= 0
+    self.plugin_design ||= 0
+    self.utility_design ||= 0
+    self.script_design ||= 0
   end
 end
