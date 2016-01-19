b16368ad5b1830acaabdf825c5f1ed24a6913b68
Mator <why101@msn.com>
2016-01-18 22:44:23 -0800

    Added associations to categories, category_priorities, and mods

--- mod-picker/app/models/mod.rb
+++ mod-picker/app/models/mod.rb
@@ -1,2 +1,5 @@
 class Mod < ActiveRecord::Base
+  belongs_to :game
+  belongs_to :primary_category, :class_name => 'Category', :foreign_key => 'category_id'
+  belongs_to :secondary_category, :class_name => 'Category', :foreign_key => 'category_id'
 end
