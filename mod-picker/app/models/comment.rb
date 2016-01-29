66da4ecc87c70df11f1e2dbe3dbbc0217ed157c9
Mator <why101@msn.com>
2016-01-28 22:16:34 -0800

    fixed polymorphic relationship on comment model

--- mod-picker/app/models/comment.rb
+++ mod-picker/app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ActiveRecord::Base
   belongs_to :user, :foreign_key => 'submitted_by', :inverse_of => 'comments'
   belongs_to :parent, :class_name => 'Comment', :foreign_key => 'parent_comment', :inverse_of => 'children'
   has_many :children, :class_name => 'Comment', :foreign_key => 'parent_comment', :inverse_of => 'parent'
-  has_one :commentable, :polymorphic => true
+  belongs_to :commentable, :polymorphic => true
 end
