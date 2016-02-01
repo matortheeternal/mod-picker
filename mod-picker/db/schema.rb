833230a14ef4bf2178081a4d6dfa192829c9dc77
Mator <why101@msn.com>
2016-02-01 01:51:34 -0800

    moved unsigned column modification to schema.rb

--- mod-picker/db/schema.rb
+++ mod-picker/db/schema.rb
@@ -410,7 +410,126 @@ ActiveRecord::Schema.define(version: 20160131215150) do
   create_table "workshop_infos", force: :cascade do |t|
     t.integer "mod_id", limit: 4, null: false
   end
+  
+  # unsigned
+  execute("ALTER TABLE agreement_marks MODIFY incorrect_note_id INT UNSIGNED;")
+  execute("ALTER TABLE agreement_marks MODIFY submitted_by INT UNSIGNED;")
 
+  execute("ALTER TABLE categories MODIFY id INT UNSIGNED NOT NULL AUTO_INCREMENT;")
+  execute("ALTER TABLE categories MODIFY parent_id INT UNSIGNED;")
+
+  execute("ALTER TABLE category_priorities MODIFY dominant_id INT UNSIGNED;")
+  execute("ALTER TABLE category_priorities MODIFY recessive_id INT UNSIGNED;")
+
+  execute("ALTER TABLE comments MODIFY id INT UNSIGNED NOT NULL AUTO_INCREMENT;")
+  execute("ALTER TABLE comments MODIFY submitted_by INT UNSIGNED;")
+  execute("ALTER TABLE comments MODIFY parent_comment INT UNSIGNED;")
+  execute("ALTER TABLE comments MODIFY commentable_id INT UNSIGNED;")
+
+  execute("ALTER TABLE compatibility_notes MODIFY id INT UNSIGNED NOT NULL AUTO_INCREMENT;")
+  execute("ALTER TABLE compatibility_notes MODIFY submitted_by INT UNSIGNED;")
+  execute("ALTER TABLE compatibility_notes MODIFY compatibility_plugin_id INT UNSIGNED;")
+  execute("ALTER TABLE compatibility_notes MODIFY installation_note_id INT UNSIGNED;")
+
+  execute("ALTER TABLE games MODIFY id INT UNSIGNED NOT NULL AUTO_INCREMENT;")
+
+  execute("ALTER TABLE helpful_marks MODIFY submitted_by INT UNSIGNED;")
+  execute("ALTER TABLE helpful_marks MODIFY helpfulable_id INT UNSIGNED;")
+
+  execute("ALTER TABLE incorrect_notes MODIFY id INT UNSIGNED NOT NULL AUTO_INCREMENT;")
+  execute("ALTER TABLE incorrect_notes MODIFY submitted_by INT UNSIGNED;")
+
+  execute("ALTER TABLE installation_notes MODIFY id INT UNSIGNED NOT NULL AUTO_INCREMENT;")
+  execute("ALTER TABLE installation_notes MODIFY submitted_by INT UNSIGNED;")
+  execute("ALTER TABLE installation_notes MODIFY mod_version_id INT UNSIGNED;")
+
+  execute("ALTER TABLE lover_infos MODIFY id INT UNSIGNED NOT NULL AUTO_INCREMENT;")
+  execute("ALTER TABLE lover_infos MODIFY mod_id INT UNSIGNED;")
+
+  execute("ALTER TABLE masters MODIFY id INT UNSIGNED NOT NULL AUTO_INCREMENT;")
+  execute("ALTER TABLE masters MODIFY plugin_id INT UNSIGNED;")
+  
+  execute("ALTER TABLE mod_asset_files MODIFY id INT UNSIGNED NOT NULL AUTO_INCREMENT;")
+
+  execute("ALTER TABLE mod_authors MODIFY mod_id INT UNSIGNED;")
+  execute("ALTER TABLE mod_authors MODIFY user_id INT UNSIGNED;")
+
+  execute("ALTER TABLE mod_list_compatibility_notes MODIFY mod_list_id INT UNSIGNED;")
+  execute("ALTER TABLE mod_list_compatibility_notes MODIFY compatibility_note_id INT UNSIGNED;")
+
+  execute("ALTER TABLE mod_list_custom_plugins MODIFY mod_list_id INT UNSIGNED;")
+
+  execute("ALTER TABLE mod_list_installation_notes MODIFY mod_list_id INT UNSIGNED;")
+  execute("ALTER TABLE mod_list_installation_notes MODIFY installation_note_id INT UNSIGNED;")
+
+  execute("ALTER TABLE mod_list_mods MODIFY mod_list_id INT UNSIGNED;")
+  execute("ALTER TABLE mod_list_mods MODIFY mod_id INT UNSIGNED;")
+
+  execute("ALTER TABLE mod_list_plugins MODIFY mod_list_id INT UNSIGNED;")
+  execute("ALTER TABLE mod_list_plugins MODIFY plugin_id INT UNSIGNED;")
+
+  execute("ALTER TABLE mod_list_stars MODIFY mod_list_id INT UNSIGNED;")
+  execute("ALTER TABLE mod_list_stars MODIFY user_id INT UNSIGNED;")
+
+  execute("ALTER TABLE mod_lists MODIFY id INT UNSIGNED NOT NULL AUTO_INCREMENT;")
+  execute("ALTER TABLE mod_lists MODIFY created_by INT UNSIGNED;")
+  execute("ALTER TABLE mod_lists MODIFY game_id INT UNSIGNED;")
+
+  execute("ALTER TABLE mod_stars MODIFY mod_id INT UNSIGNED;")
+  execute("ALTER TABLE mod_stars MODIFY user_id INT UNSIGNED;")
+
+  execute("ALTER TABLE mod_version_compatibility_notes MODIFY mod_version_id INT UNSIGNED;")
+  execute("ALTER TABLE mod_version_compatibility_notes MODIFY compatibility_note_id INT UNSIGNED;")
+
+  execute("ALTER TABLE mod_version_files MODIFY mod_version_id INT UNSIGNED;")
+  execute("ALTER TABLE mod_version_files MODIFY mod_asset_file_id INT UNSIGNED;")
+
+  execute("ALTER TABLE mod_versions MODIFY id INT UNSIGNED NOT NULL AUTO_INCREMENT;")
+  execute("ALTER TABLE mod_versions MODIFY mod_id INT UNSIGNED;")
+
+  execute("ALTER TABLE mods MODIFY id INT UNSIGNED NOT NULL AUTO_INCREMENT;")
+  execute("ALTER TABLE mods MODIFY game_id INT UNSIGNED;")
+  execute("ALTER TABLE mods MODIFY primary_category_id INT UNSIGNED;")
+  execute("ALTER TABLE mods MODIFY secondary_category_id INT UNSIGNED;")
+
+  execute("ALTER TABLE nexus_infos MODIFY id INT UNSIGNED NOT NULL AUTO_INCREMENT;")
+  execute("ALTER TABLE nexus_infos MODIFY mod_id INT UNSIGNED;")
+
+  execute("ALTER TABLE override_records MODIFY plugin_id INT UNSIGNED;")
+  execute("ALTER TABLE override_records MODIFY master_id INT UNSIGNED;")
+
+  execute("ALTER TABLE plugin_record_groups MODIFY plugin_id INT UNSIGNED;")
+
+  execute("ALTER TABLE plugins MODIFY id INT UNSIGNED NOT NULL AUTO_INCREMENT;")
+  execute("ALTER TABLE plugins MODIFY mod_version_id INT UNSIGNED;")
+
+  execute("ALTER TABLE record_groups MODIFY id INT UNSIGNED NOT NULL AUTO_INCREMENT;")
+  execute("ALTER TABLE record_groups MODIFY game_id INT UNSIGNED;")
+
+  execute("ALTER TABLE reputation_links MODIFY from_rep_id INT UNSIGNED;")
+  execute("ALTER TABLE reputation_links MODIFY to_rep_id INT UNSIGNED;")
+
+  execute("ALTER TABLE reviews MODIFY id INT UNSIGNED NOT NULL AUTO_INCREMENT;")
+  execute("ALTER TABLE reviews MODIFY submitted_by INT UNSIGNED;")
+  execute("ALTER TABLE reviews MODIFY mod_id INT UNSIGNED;")
+
+  execute("ALTER TABLE user_bios MODIFY id INT UNSIGNED NOT NULL AUTO_INCREMENT;")
+  execute("ALTER TABLE user_bios MODIFY user_id INT UNSIGNED;")
+
+  execute("ALTER TABLE user_reputations MODIFY id INT UNSIGNED NOT NULL AUTO_INCREMENT;")
+  execute("ALTER TABLE user_reputations MODIFY user_id INT UNSIGNED;")
+
+  execute("ALTER TABLE user_settings MODIFY id INT UNSIGNED NOT NULL AUTO_INCREMENT;")
+  execute("ALTER TABLE user_settings MODIFY user_id INT UNSIGNED;")
+
+  execute("ALTER TABLE users MODIFY id INT UNSIGNED NOT NULL AUTO_INCREMENT;")
+  execute("ALTER TABLE users MODIFY active_ml_id INT UNSIGNED;")
+  execute("ALTER TABLE users MODIFY active_mc_id INT UNSIGNED;")
+
+  execute("ALTER TABLE workshop_infos MODIFY id INT UNSIGNED NOT NULL AUTO_INCREMENT;")
+  execute("ALTER TABLE workshop_infos MODIFY mod_id INT UNSIGNED;")
+	
+  # foreign keys
   add_foreign_key "agreement_marks", "incorrect_notes", name: "agreement_marks_ibfk_1"
   add_foreign_key "agreement_marks", "users", column: "submitted_by", name: "agreement_marks_ibfk_2"
   add_foreign_key "categories", "categories", column: "parent_id"
@@ -422,9 +541,6 @@ ActiveRecord::Schema.define(version: 20160131215150) do
   add_foreign_key "compatibility_notes", "plugins", column: "compatibility_plugin_id", name: "compatibility_notes_ibfk_2"
   add_foreign_key "compatibility_notes", "users", column: "submitted_by", name: "compatibility_notes_ibfk_1"
   add_foreign_key "helpful_marks", "users", column: "submitted_by", name: "helpful_marks_ibfk_4"
-  add_foreign_key "incorrect_notes", "compatibility_notes", name: "incorrect_notes_ibfk_2"
-  add_foreign_key "incorrect_notes", "installation_notes", name: "incorrect_notes_ibfk_3"
-  add_foreign_key "incorrect_notes", "reviews", name: "incorrect_notes_ibfk_1"
   add_foreign_key "incorrect_notes", "users", column: "submitted_by", name: "incorrect_notes_ibfk_4"
   add_foreign_key "installation_notes", "mod_versions", name: "installation_notes_ibfk_2"
   add_foreign_key "installation_notes", "users", column: "submitted_by", name: "installation_notes_ibfk_1"
