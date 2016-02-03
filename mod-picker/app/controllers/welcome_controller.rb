734c7902f0f095023e208b3a38614ec5527848d7
Mator <why101@msn.com>
2016-02-02 16:19:00 -0800

    Created "under construction" page

--- mod-picker/app/controllers/welcome_controller.rb
+++ mod-picker/app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    redirect_to '/index.html'
   end
 end
---
+++ mod-picker/public/resources/js/script.js
@@ -0,0 +1,126 @@
+/* ==============================================
+   Countdown
+=============================================== */
+	// Create a countdown instance. Change the launchDay according to your needs.
+	// The month ranges from 0 to 11. I specify the month from 1 to 12 and manually subtract the 1.
+	// Thus the launchDay below denotes 7 May, 2014.
+	
+var newYear = new Date(); 
+newYear = new Date(2016, 4-1, 1);
+$('.defaultCountdown').countdown({until: newYear, format: 'DHMS'}); 
+
+
+
+
+/* ==============================================
+	For Tooltip.
+=====================================================================*/	
+
+$(function(){
+     $('[data-rel="tooltip"]').tooltip();
+});
+
+/* ==============================================
+	For WOW Animation.
+=====================================================================*/	
+$(document).ready( function() {
+ new WOW().init();
+
+/* ==============================================
+	For Smooth Scroll.
+=====================================================================*/	
+      var $stupid = $('<div></div>')
+        .height(1)
+        .hide()
+        .appendTo('body');
+
+      var mobileHack = function() {
+        $stupid.show();
+        setTimeout(function() {
+          $stupid.hide();
+        }, 10);
+      };
+
+      $('ul.mainnav a').smoothScroll({
+        afterScroll: mobileHack
+      });
+	
+	
+	
+/* ==============================================
+	For Fixed Menu.
+=====================================================================*/		
+var s = $("#stick_menu");
+    var pos = s.position();                                     
+    $(window).scroll(function() {
+        var windowpos = $(window).scrollTop();
+        if (windowpos >= pos.top) {
+            s.addClass("stick_menu");
+        } else {
+            s.removeClass("stick_menu");
+        }
+    });
+
+/* ==============================================
+	Remove Full Screen Image in Mobile view.
+=====================================================================*/			
+if ($(window).width() < 514) {
+     $('#head').removeClass('intro');
+	  $('#head').css('height', 'auto')
+} else {
+	$('#head').addClass('intro');
+   
+}	
+
+$(window).resize(function() {
+    if ($(window).width() < 1050) {
+     $('#head').removeClass('intro');
+	 $('#head').css('height', 'auto')
+    } else {
+	$('#head').addClass('intro');
+    }
+}).resize();
+	
+	
+/* ==============================================
+	For Full Screen Header Part.
+=====================================================================*/		
+	
+		"use strict";
+
+		var winHeight = $(window).height();
+		var winWidth = $(window).width();
+
+		if (winWidth > 979) {
+			$('.intro').css({
+				'height': winHeight,
+			});
+			} else{
+			$('.intro').css({
+				'height': '536px'
+			});
+		};
+
+		$(window).resize(function(){
+			var winHeight = $(window).height();
+			var winWidth = $(window).width();
+
+			if (winWidth > 979) {
+				$('.intro').css({
+					'height': winHeight
+				});
+				} else{
+				$('.intro').css({
+					'height': '536px'
+				});
+			};
+		});
+		
+/* ==============================================
+	For Customize Scroll Bar Part.
+=====================================================================*/				
+		
+	var nice = jQuery("html").niceScroll({scrollspeed:100,}).hide();;		
+		
+		
+	});    
