b2f163f353517206dc36f69a2982a98a3bdeb28f
Mator <why101@msn.com>
2016-02-07 13:39:04 -0800

    Under construction welcome page is now in the asset pipeline and working!

---
+++ mod-picker/app/assets/javascripts/angular.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Mator on 2/7/2016.
+ */
--- mod-picker/config/initializers/assets.rb
+++ mod-picker/config/initializers/assets.rb
@@ -4,8 +4,22 @@
 Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
-# Rails.application.config.assets.paths << Emoji.images_path
+# Rails.application.config.assets.paths << %w( )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w(
+animate.css
+bootstrap.min.css
+welcome.css
+style-responsive.css
+angular.min.js
+angular-route.min.js
+bootstrap.min.js
+heartcode-canvasloader-min.js
+jquery.countdown.min.js
+jquery.nicescroll.min.js
+jquery.smooth-scroll.js
+script.js
+wow.min.js
+)
