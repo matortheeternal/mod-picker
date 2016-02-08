0b9efc6521108e1e850b29b463911ae361f032da
Mator <why101@msn.com>
2016-02-08 00:04:30 -0800

    added some initial scraping code for nexus info records

--- mod-picker/app/models/nexus_info.rb
+++ mod-picker/app/models/nexus_info.rb
@@ -1,3 +1,27 @@
+require 'nokogiri'
+require 'open-uri'
+
 class NexusInfo < ActiveRecord::Base
   belongs_to :mod
+  belongs_to :game, :inverse_of => 'nexus_infos'
+
+  validates_presence_of :id, :game_id
+
+  def nexus_mods_url
+    "http://www.nexusmods.com/#{game.nexus_name}/mods/#{id}"
+  end
+
+  def scrape
+    doc = Nokogiri::HTML(open(nexus_mods_url))
+    modName = doc.at_css(".header-name").text
+    currentVersion = doc.at_css(".file_version").text
+    self.authors = doc.at_css(".header-author").text
+    self.endorsements = doc.at_css("#span_endors_number").text
+    self.unique_downloads = doc.at_css(".file-unique-dls").text
+    self.total_downloads = doc.at_css(".file-total-dls").text
+    self.views = doc.at_css(".file-total-views").text
+    self.uploaded_by = doc.at_css(".uploader").text
+    # TODO: Date added, date updated, nexus category, isUtility
+    # TODO: Create associated mod and mod version records
+  end
 end
