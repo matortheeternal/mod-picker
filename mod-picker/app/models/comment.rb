31bedcb3d93354ee4078664eabf842fd1142293d
Mator <why101@msn.com>
2016-02-14 14:20:15 -0800

    Comment filtering working

--- mod-picker/app/models/comment.rb
+++ mod-picker/app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ActiveRecord::Base
+  include Filterable
+
+  scope :type, -> (type) { where(commentable_type: type) }
+  scope :target, -> (id) { where(commentable_id: id) }
+
   belongs_to :user, :foreign_key => 'submitted_by', :inverse_of => 'comments'
   belongs_to :parent, :class_name => 'Comment', :foreign_key => 'parent_comment', :inverse_of => 'children'
   has_many :children, :class_name => 'Comment', :foreign_key => 'parent_comment', :inverse_of => 'parent'
