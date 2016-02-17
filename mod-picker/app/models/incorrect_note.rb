7ebe7eb1c3fe2429e14e04b1985809f381a12ffe
Mator <why101@msn.com>
2016-02-17 12:11:06 -0800

    added correctable counter cache

--- mod-picker/app/models/incorrect_note.rb
+++ mod-picker/app/models/incorrect_note.rb
@@ -5,5 +5,5 @@ class IncorrectNote < ActiveRecord::Base
 
   belongs_to :user, :foreign_key => 'submitted_by', :inverse_of => 'incorrect_notes'
   has_many :agreement_marks, :inverse_of => 'incorrect_note'
-  belongs_to :correctable, :polymorphic => true
+  belongs_to :correctable, :polymorphic => true, :counter_cache => true
 end
