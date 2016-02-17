6bbf490e6f662b8bbbae4ccd38d360c0544137d1
r79 <r@r79.io>
2016-02-17 02:03:37 +0100

    improved css - everything unselectable by default (except user generated content) - cooler implementation of the modlist

--- mod-picker/app/assets/javascripts/angularAssets/Directives/modList.js
+++ mod-picker/app/assets/javascripts/angularAssets/Directives/modList.js
@@ -6,8 +6,15 @@ app.directive('modList', function () {
     return {
         restrict: 'E',
         templateUrl: '/resources/directives/modList.html',
+        controller: 'modListController',
         scope: {
             data: '='
         }
+    }
+});
+
+app.controller('modListController', function ($scope) {
+    $scope.navigate = function (mod) {
+        window.location = '#/mod/' + mod.id;
     }
 });
