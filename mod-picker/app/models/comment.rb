200a9d4f922a73b682751c3e8ce911420328b268
Mator <why101@msn.com>
2016-02-17 12:01:22 -0800

    comments counter cache enabled

--- mod-picker/app/models/comment.rb
+++ mod-picker/app/models/comment.rb
@@ -8,5 +8,5 @@ class Comment < ActiveRecord::Base
   belongs_to :user, :foreign_key => 'submitted_by', :inverse_of => 'comments'
   belongs_to :parent, :class_name => 'Comment', :foreign_key => 'parent_comment', :inverse_of => 'children'
   has_many :children, :class_name => 'Comment', :foreign_key => 'parent_comment', :inverse_of => 'parent'
-  belongs_to :commentable, :polymorphic => true
+  belongs_to :commentable, :polymorphic => true, :counter_cache => true
 end
