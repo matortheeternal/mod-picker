df92b6bade5b715e91f5dfb30583450c9653ab54
Mator <why101@msn.com>
2016-02-21 00:27:38 -0800

    completed sliders on mods index page, including default hiding of extended filters

--- mod-picker/app/assets/javascripts/angularAssets/Views/mods.js
+++ mod-picker/app/assets/javascripts/angularAssets/Views/mods.js
@@ -10,6 +10,23 @@ app.config(['$routeProvider', function ($routeProvider) {
 app.controller('modsController', function ($scope, $q, backend) {
     $scope.loading = true;
 
+    /* visibility of extended filters */
+    $scope.nm_visible = false;
+    $scope.nm_toggle = function () {
+        $scope.nm_visible = !$scope.nm_visible;
+        if ($scope.nm_visbile) {
+            $scope.$broadcast('rzSliderForceRender');
+        }
+    };
+
+    $scope.mp_visible = false;
+    $scope.mp_toggle = function () {
+        $scope.mp_visible = !$scope.mp_visible;
+        if ($scope.mp_visbile) {
+            $scope.$broadcast('rzSliderForceRender');
+        }
+    };
+
     /* slider prototypes */
     var now = new Date();
     var start = new Date(2011,10,11);
@@ -134,6 +151,68 @@ app.controller('modsController', function ($scope, $q, backend) {
             hideLimitLabels: true
         }
     };
+
+    /* mod picker sliders */
+    $scope.slReputation = {
+        minValue: 0,
+        maxValue: 450,
+        options: {
+            floor: 0,
+            ceil: 450,
+            noSwitching: true,
+            hideLimitLabels: true
+        }
+    };
+    $scope.slRating = {
+        minValue: 0,
+        maxValue: 100,
+        options: {
+            floor: 0,
+            ceil: 100,
+            noSwitching: true,
+            hideLimitLabels: true
+        }
+    };
+    $scope.slReviews = {
+        minValue: 0,
+        maxValue: 200,
+        options: {
+            floor: 0,
+            ceil: 200,
+            noSwitching: true,
+            hideLimitLabels: true
+        }
+    };
+    $scope.slStars = {
+        minValue: 0,
+        maxValue: 130,
+        options: {
+            noSwitching: true,
+            hideLimitLabels: true,
+            stepsArray: generateSteps(1, 1000)
+        }
+    };
+    $scope.slCnotes = {
+        minValue: 0,
+        maxValue: 100,
+        options: {
+            floor: 0,
+            ceil: 100,
+            noSwitching: true,
+            hideLimitLabels: true
+        }
+    };
+    $scope.slInotes = {
+        minValue: 0,
+        maxValue: 100,
+        options: {
+            floor: 0,
+            ceil: 100,
+            noSwitching: true,
+            hideLimitLabels: true
+        }
+    };
+
 
 
     /* data */
