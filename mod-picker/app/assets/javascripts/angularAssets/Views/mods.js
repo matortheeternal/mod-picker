70322edfeb517003f91ec4ac2ca8bfbacf42905c
Mator <why101@msn.com>
2016-02-21 00:07:17 -0800

    added more sliders to the mods page

--- mod-picker/app/assets/javascripts/angularAssets/Views/mods.js
+++ mod-picker/app/assets/javascripts/angularAssets/Views/mods.js
@@ -9,6 +9,8 @@ app.config(['$routeProvider', function ($routeProvider) {
 
 app.controller('modsController', function ($scope, $q, backend) {
     $scope.loading = true;
+
+    /* slider prototypes */
     var now = new Date();
     var start = new Date(2011,10,11);
     $scope.dateSlider = {
@@ -17,6 +19,7 @@ app.controller('modsController', function ($scope, $q, backend) {
             ceil: now.getTime(),
             step: 86400000,
             hideLimitLabels: true,
+            noSwitching: true,
             translate: function(value, sliderId, label) {
                 var d;
                 switch (label) {
@@ -33,6 +36,8 @@ app.controller('modsController', function ($scope, $q, backend) {
             }
         }
     };
+
+    /* date sliders */
     $scope.slUpdated = {
         minValue: start.getTime(),
         maxValue: now.getTime()
@@ -41,8 +46,118 @@ app.controller('modsController', function ($scope, $q, backend) {
         minValue: start.getTime(),
         maxValue: now.getTime()
     };
+
+    /* nexus mods stat sliders */
+    $scope.slEndorsements = {
+        minValue: 0,
+        maxValue: 130,
+        options: {
+            noSwitching: true,
+            hideLimitLabels: true,
+            stepsArray: generateSteps(5, 500000)
+        }
+    };
+    $scope.slUniqueDownloads = {
+        minValue: 0,
+        maxValue: 140,
+        options: {
+            noSwitching: true,
+            hideLimitLabels: true,
+            stepsArray: generateSteps(50, 10000000)
+        }
+    };
+    $scope.slTotalDownloads = {
+        minValue: 0,
+        maxValue: 150,
+        options: {
+            noSwitching: true,
+            hideLimitLabels: true,
+            stepsArray: generateSteps(100, 50000000)
+        }
+    };
+    $scope.slViews = {
+        minValue: 0,
+        maxValue: 150,
+        options: {
+            noSwitching: true,
+            hideLimitLabels: true,
+            stepsArray: generateSteps(100, 50000000)
+        }
+    };
+    $scope.slPosts = {
+        minValue: 0,
+        maxValue: 100,
+        options: {
+            floor: 0,
+            ceil: 100,
+            noSwitching: true,
+            hideLimitLabels: true,
+            stepsArray: generateSteps(5, 50000)
+        }
+    };
+    $scope.slVideos = {
+        minValue: 0,
+        maxValue: 50,
+        options: {
+            floor: 0,
+            ceil: 50,
+            noSwitching: true,
+            hideLimitLabels: true
+        }
+    };
+    $scope.slImages = {
+        minValue: 0,
+        maxValue: 130,
+        options: {
+            noSwitching: true,
+            hideLimitLabels: true,
+            stepsArray: generateSteps(1, 1000)
+        }
+    };
+    $scope.slFiles = {
+        minValue: 0,
+        maxValue: 200,
+        options: {
+            floor: 0,
+            ceil: 200,
+            noSwitching: true,
+            hideLimitLabels: true
+        }
+    };
+    $scope.slArticles = {
+        minValue: 0,
+        maxValue: 50,
+        options: {
+            floor: 0,
+            ceil: 50,
+            noSwitching: true,
+            hideLimitLabels: true
+        }
+    };
+
+
+    /* data */
     backend.retrieveMods().then(function (data) {
         $scope.mods = data;
         $scope.loading = false;
     });
 });
+
+
+function generateSteps(initialSpacing, maxValue) {
+    var array = [0];
+    var c = 0;
+    var spacing = initialSpacing;
+    for (i = 1; c < maxValue; i++) {
+        c += spacing;
+        if ((c >= 100) && (i % 10 == 0)) {
+            if (Math.log10(spacing) % 1 != 0) {
+                spacing = c / 10;
+            } else {
+                spacing = (c * 1.5) / 10;
+            }
+        }
+        array.push(c);
+    }
+    return array;
+}
