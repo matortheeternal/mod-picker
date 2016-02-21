a4cede4d4b65470b0e29b555c12ef24a91318ad5
Mator <why101@msn.com>
2016-02-21 12:48:38 -0800

    added sliders to installation notes index

--- mod-picker/app/assets/javascripts/angularAssets/Views/inotes.js
+++ mod-picker/app/assets/javascripts/angularAssets/Views/inotes.js
@@ -9,6 +9,39 @@ app.config(['$routeProvider', function ($routeProvider) {
 
 app.controller('inotesController', function ($scope, $q, backend) {
     $scope.loading = true;
+
+    /* slider prototypes */
+    var start = new Date(2016,0,1);
+    $scope.dateSlider = {
+        options: {
+            hideLimitLabels: true,
+            noSwitching: true,
+            stepsArray: generateDateSteps(start)
+        }
+    };
+
+    /* date sliders */
+    $scope.slSubmitted = {
+        minValue: 0,
+        maxValue: 51
+    };
+    $scope.slEdited = {
+        minValue: 0,
+        maxValue: 51
+    };
+
+    /* helpfulness slider */
+    $scope.slHelpfulness = {
+        minValue: 0,
+        maxValue: 120,
+        options: {
+            hideLimitLabels: true,
+            noSwitching: true,
+            stepsArray: generateSteps(1, 500)
+        }
+    };
+
+    /* data */
     backend.retrieveInstallationNotes().then(function (data) {
         $scope.inotes = data;
         $scope.loading = false;
