0fb469bff59e0ddd385eb84d6093929311f41719
Mator <why101@msn.com>
2016-02-21 12:48:57 -0800

    added sliders to users index

--- mod-picker/app/assets/javascripts/angularAssets/Views/users.js
+++ mod-picker/app/assets/javascripts/angularAssets/Views/users.js
@@ -9,6 +9,96 @@ app.config(['$routeProvider', function ($routeProvider) {
 
 app.controller('usersController', function ($scope, $q, backend) {
     $scope.loading = true;
+
+    /* slider prototypes */
+    var start = new Date(2016,0,1);
+    $scope.dateSlider = {
+        options: {
+            hideLimitLabels: true,
+            noSwitching: true,
+            stepsArray: generateDateSteps(start)
+        }
+    };
+
+    /* date sliders */
+    $scope.slJoined = {
+        minValue: 0,
+        maxValue: 51
+    };
+    $scope.slLastSeen = {
+        minValue: 0,
+        maxValue: 51
+    };
+
+    /* stat sliders */
+    $scope.slReputation = {
+        minValue: 0,
+        maxValue: 70,
+        options: {
+            hideLimitLabels: true,
+            noSwitching: true,
+            stepsArray: generateSteps(5, 5000)
+        }
+    };
+    $scope.slMods = {
+        minValue: 0,
+        maxValue: 100,
+        options: {
+            floor: 0,
+            ceil: 100,
+            hideLimitLabels: true,
+            noSwitching: true
+        }
+    };
+    $scope.slCompatibilityNotes = {
+        minValue: 0,
+        maxValue: 60,
+        options: {
+            hideLimitLabels: true,
+            noSwitching: true,
+            stepsArray: generateSteps(1, 500)
+        }
+    };
+    $scope.slInstallationNotes = {
+        minValue: 0,
+        maxValue: 60,
+        options: {
+            hideLimitLabels: true,
+            noSwitching: true,
+            stepsArray: generateSteps(1, 500)
+        }
+    };
+    $scope.slIncorrectNotes = {
+        minValue: 0,
+        maxValue: 60,
+        options: {
+            hideLimitLabels: true,
+            noSwitching: true,
+            stepsArray: generateSteps(1, 500)
+        }
+    };
+    $scope.slComments = {
+        minValue: 0,
+        maxValue: 60,
+        options: {
+            hideLimitLabels: true,
+            noSwitching: true,
+            stepsArray: generateSteps(1, 500)
+        }
+    };
+    $scope.slModLists = {
+        minValue: 0,
+        maxValue: 100,
+        options: {
+            floor: 0,
+            ceil: 100,
+            hideLimitLabels: true,
+            noSwitching: true
+        }
+    };
+
+
+    /* data */
     backend.retrieveUsers().then(function (data) {
         $scope.users = data;
         $scope.loading = false;
