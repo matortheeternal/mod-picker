6c60ad5635f677e0800024ac66a353403a9dbcef
Mator <why101@msn.com>
2016-03-04 15:38:30 -0800

    submit mod form now scraping correctly

--- mod-picker/app/assets/javascripts/angularAssets/Views/submitMod.js
+++ mod-picker/app/assets/javascripts/angularAssets/Views/submitMod.js
@@ -1,6 +1,3 @@
-/**
- * Created by r79 on 2/11/2016.
- */
 app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/submitMod', {
             templateUrl: '/resources/partials/submitMod.html',
@@ -9,8 +6,24 @@ app.config(['$routeProvider', function ($routeProvider) {
     );
 }]);
 
-app.controller('submitModController', function ($scope, backend) {
+app.controller('submitModController', function ($scope, backend, submitService) {
     useTwoColumns(false);
+    $scope.nexusScraped = false;
+    var nexusUrlPattern = /(http[s]:\/\/{0,1}){0,1}www.nexusmods.com\/skyrim\/mods\/([0-9]{1,9})(\/\?){0,1}/i;
+
+    $scope.urlInvalid = function () {
+        return nexusUrlPattern.exec($scope.nexusUrl) == null
+    };
+
+    $scope.scrape = function () {
+        var match = nexusUrlPattern.exec($scope.nexusUrl);
+        var id = match[2];
+        submitService.scrapeNexus(3, id).then(function (data) {
+            $scope.nexusScraped = true;
+            $scope.nexus = data;
+        });
+    };
+
     $scope.submit = function () {
         if ($scope.submitModForm.$valid) {
             var primaryCategoryId = $scope.primaryCategory.subCategoryId || $scope.primaryCategory.mainCategoryId;
@@ -20,7 +33,7 @@ app.controller('submitModController', function ($scope, backend) {
                 '\nName: ' + $scope.name +
                 '\nPrimaryCategoryId: ' + primaryCategoryId +
                 (secondaryCategoryId ? '\nSecondaryCategoryId: ' + secondaryCategoryId : ''));
-            window.location = '#/browse';
+            window.location = '#/mods';
         }
     }
 });
