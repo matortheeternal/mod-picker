7166ff5ff11d25d112f678a369e31a609bb8975d
Mator <why101@msn.com>
2016-03-04 12:34:41 -0800

    updated scraping code to be a bit cleaner

--- mod-picker/app/models/nexus_info.rb
+++ mod-picker/app/models/nexus_info.rb
@@ -11,8 +11,19 @@ class NexusInfo < ActiveRecord::Base
     "http://www.nexusmods.com/#{game.nexus_name}/mods/#{id}"
   end
 
+  def scrape_rate
+    1.week.to_i
+  end
+
+  def nexus_date_format
+    '%d/%m/%Y - %I:%M%p'
+  end
+
   def scrape
+    # get the nexus mods page
     doc = Nokogiri::HTML(open(nexus_mods_url))
+
+    # scrape basic data
     self.last_scraped = DateTime.now
     self.modName = doc.at_css(".header-name").text
     self.currentVersion = doc.at_css(".file_version").text
@@ -22,15 +33,21 @@ class NexusInfo < ActiveRecord::Base
     self.total_downloads = doc.at_css(".file-total-dls").text
     self.views = doc.at_css(".file-total-views").text
     self.uploaded_by = doc.at_css(".uploader").text
+
+    # parse dates
     dates = doc.at_css(".header-dates").text
-    self.date_released = /Added:<\/strong> ([^<]*)/.match(dates).captures
-    self.date_updated = /Updated:<\/strong> ([^<]*)/.match(dates).captures
+    date_released_str = /Added:<\/strong> ([^<]*)/.match(dates).captures[0]
+    self.date_released = DateTime.parse(date_released_str, nexus_date_format)
+    date_updated_str = /Updated:<\/strong> ([^<]*)/.match(dates).captures[0]
+    self.date_updated = DateTime.parse(date_updated_str, nexus_date_format)
+
+    # parse nexus category
     cat = doc.at_css(".header-cat").text
-    self.nexus_category = /src_cat=([0-9]*)/.match(cat).captures
+    self.nexus_category = /src_cat=([0-9]*)/.match(cat).captures[0].to_i
   end
 
   def rescrape
-    if (DateTime.now - self.last_scraped) > 1.week.to_i
+    if (DateTime.now - self.last_scraped) > scrape_rate
       self.scrape
     end
   end
