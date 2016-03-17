59f79405e1a500f947b77317a88c0a45617d9666
Mator <why101@msn.com>
2016-03-16 23:20:04 -0700

    can now update allow comments and theme

--- mod-picker/app/controllers/user_settings_controller.rb
+++ mod-picker/app/controllers/user_settings_controller.rb
@@ -83,6 +83,6 @@ class UserSettingsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_setting_params
-      params.require(:user_setting).permit(:user_id, :show_notifications, :show_tooltips, :email_notifications, :email_public, :allow_adult_content, :allow_nexus_mods, :allow_lovers_lab, :allow_steam_workshop, :timezone, :udate_format, :utime_format)
+      params.require(:user_setting).permit(:user_id, :show_notifications, :show_tooltips, :email_notifications, :email_public, :allow_adult_content, :allow_nexus_mods, :allow_lovers_lab, :allow_steam_workshop, :timezone, :udate_format, :utime_format, :allow_comments, :theme)
     end
 end
