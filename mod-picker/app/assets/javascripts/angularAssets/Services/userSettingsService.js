be0022c47436c2983befdfd797bd11136b4aac13
Mator <why101@msn.com>
2016-03-16 21:51:41 -0700

    added update code for user settings submission

--- mod-picker/app/assets/javascripts/angularAssets/Services/userSettingsService.js
+++ mod-picker/app/assets/javascripts/angularAssets/Services/userSettingsService.js
@@ -9,16 +9,30 @@ app.service('userSettingsService', function (backend, $q) {
         return userSettings.promise;
     };
 
+    this.flash_errors = function(data) {
+        $scope.errors = data.errors;
+    };
+
     this.submit = function (user, user_settings) {
+        var usersSuccess = false;
+        var userSettingsSuccess = false;
 
     	backend.update('/users/' + user.id, user).then(function (data) {
-    		done = (data.status === "ok");
-
-    	};
+            usersSuccess = (data.status === "ok");
+            if (!usersSuccess) {
+                flash_errors(data);
+            } else if (userSettingsSuccess) {
+                $scope.success = true;
+            }
+    	});
     	backend.update('/user_settings/' + user_settings.id, user_settings).then(function (data) {
-
-	    };
-
+            userSettingsSuccess = (data.status === "ok");
+            if (!userSettingsSuccess) {
+                flash_errors(data);
+            } else if (usersSuccess) {
+                $scope.success = true;
+            }
+	    });
     };
 
 });
