c2d02a063c338d8990fcca147334dbed306af778
Mator <why101@msn.com>
2016-03-25 14:07:42 -0700

    created and ran update_user_reputations migration

--- mod-picker/db/schema.rb
+++ mod-picker/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160325201615) do
+ActiveRecord::Schema.define(version: 20160325210132) do
 
   create_table "agreement_marks", id: false, force: :cascade do |t|
     t.integer "incorrect_note_id", limit: 4
@@ -370,13 +370,15 @@ ActiveRecord::Schema.define(version: 20160325201615) do
   add_index "user_bios", ["user_id"], name: "user_id", using: :btree
 
   create_table "user_reputations", force: :cascade do |t|
-    t.float   "overall",            limit: 24
-    t.float   "offset",             limit: 24
-    t.float   "audiovisual_design", limit: 24
-    t.float   "plugin_design",      limit: 24
-    t.float   "utility_design",     limit: 24
-    t.float   "script_design",      limit: 24
-    t.integer "user_id",            limit: 4
+    t.float    "overall",          limit: 24
+    t.float    "offset",           limit: 24
+    t.integer  "user_id",          limit: 4
+    t.float    "site_rep",         limit: 24
+    t.float    "contribution_rep", limit: 24
+    t.float    "author_rep",       limit: 24
+    t.float    "given_rep",        limit: 24
+    t.datetime "last_computed"
+    t.boolean  "dont_compute"
   end
 
   add_index "user_reputations", ["user_id"], name: "user_id", using: :btree
