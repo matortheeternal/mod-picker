9484124b345db11e7e223175672165cf4adc2ada
ThreeTen22 <gtumen@gmail.com>
2016-03-26 06:16:03 -0500

    Revert "init commit"
    This reverts commit 50248cf245089824474238cebf2415d983b43ade.

--- mod-picker/app/assets/javascripts/angularAssets/Views/ModList.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by ThreeTen on 3/26/2016.
- */
-app.config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/modlist', {
-            templateUrl: '/resources/partials/modlist_template.html'
-        }
-    );
-}]);
