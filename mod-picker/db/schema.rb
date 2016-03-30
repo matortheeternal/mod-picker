5d490d07df2d24d365738d7d09aff5d8573fd4d5
Mator <why101@msn.com>
2016-03-30 16:46:36 -0700

    broke mod_list_notes into separate tables for compatibility, load order, and install order notes this will shave off 255 bytes per associated note

--- mod-picker/db/schema.rb
+++ mod-picker/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160330232704) do
+ActiveRecord::Schema.define(version: 20160330233241) do
 
   create_table "agreement_marks", id: false, force: :cascade do |t|
     t.integer "incorrect_note_id", limit: 4
@@ -181,6 +181,15 @@ ActiveRecord::Schema.define(version: 20160330232704) do
   add_index "mod_authors", ["mod_id"], name: "mod_id", using: :btree
   add_index "mod_authors", ["user_id"], name: "user_id", using: :btree
 
+  create_table "mod_list_compatibility_notes", id: false, force: :cascade do |t|
+    t.integer "mod_list_id",           limit: 4
+    t.integer "compatibility_note_id", limit: 4
+    t.enum    "status",                limit: ["Unresolved", "Resolved", "Ignored"], default: "Unresolved"
+  end
+
+  add_index "mod_list_compatibility_notes", ["compatibility_note_id"], name: "cn_id", using: :btree
+  add_index "mod_list_compatibility_notes", ["mod_list_id"], name: "ml_id", using: :btree
+
   create_table "mod_list_custom_plugins", id: false, force: :cascade do |t|
     t.integer "mod_list_id", limit: 4
     t.boolean "active"
@@ -191,14 +200,23 @@ ActiveRecord::Schema.define(version: 20160330232704) do
 
   add_index "mod_list_custom_plugins", ["mod_list_id"], name: "ml_id", using: :btree
 
-  create_table "mod_list_installation_notes", id: false, force: :cascade do |t|
-    t.integer "mod_list_id",          limit: 4
-    t.integer "installation_note_id", limit: 4
-    t.enum    "status",               limit: ["Resolved", "Ignored"]
+  create_table "mod_list_install_order_notes", id: false, force: :cascade do |t|
+    t.integer "mod_list_id",           limit: 4
+    t.integer "install_order_note_id", limit: 4
+    t.enum    "status",                limit: ["Unresolved", "Resolved", "Ignored"]
   end
 
-  add_index "mod_list_installation_notes", ["installation_note_id"], name: "in_id", using: :btree
-  add_index "mod_list_installation_notes", ["mod_list_id"], name: "ml_id", using: :btree
+  add_index "mod_list_install_order_notes", ["install_order_note_id"], name: "in_id", using: :btree
+  add_index "mod_list_install_order_notes", ["mod_list_id"], name: "ml_id", using: :btree
+
+  create_table "mod_list_load_order_notes", id: false, force: :cascade do |t|
+    t.integer "mod_list_id",        limit: 4,                                     null: false
+    t.integer "load_order_note_id", limit: 4,                                     null: false
+    t.enum    "status",             limit: ["Unresolved", "Resolved", "Ignored"]
+  end
+
+  add_index "mod_list_load_order_notes", ["load_order_note_id"], name: "index_mod_list_load_order_notes_on_load_order_note_id", using: :btree
+  add_index "mod_list_load_order_notes", ["mod_list_id"], name: "index_mod_list_load_order_notes_on_mod_list_id", using: :btree
 
   create_table "mod_list_mods", id: false, force: :cascade do |t|
     t.integer "mod_list_id", limit: 4
@@ -209,16 +227,6 @@ ActiveRecord::Schema.define(version: 20160330232704) do
 
   add_index "mod_list_mods", ["mod_id"], name: "mod_id", using: :btree
   add_index "mod_list_mods", ["mod_list_id"], name: "ml_id", using: :btree
-
-  create_table "mod_list_notes", id: false, force: :cascade do |t|
-    t.integer "mod_list_id", limit: 4
-    t.integer "note_id",     limit: 4
-    t.enum    "status",      limit: ["Unresolved", "Resolved", "Ignored"], default: "Unresolved"
-    t.string  "note_type",   limit: 255
-  end
-
-  add_index "mod_list_notes", ["mod_list_id"], name: "ml_id", using: :btree
-  add_index "mod_list_notes", ["note_id"], name: "cn_id", using: :btree
 
   create_table "mod_list_plugins", id: false, force: :cascade do |t|
     t.integer "mod_list_id", limit: 4
@@ -589,12 +597,15 @@ ActiveRecord::Schema.define(version: 20160330232704) do
   add_foreign_key "masters", "plugins", name: "masters_ibfk_1"
   add_foreign_key "mod_authors", "mods", name: "mod_authors_ibfk_1"
   add_foreign_key "mod_authors", "users", name: "mod_authors_ibfk_2"
+  add_foreign_key "mod_list_compatibility_notes", "compatibility_notes"
+  add_foreign_key "mod_list_compatibility_notes", "mod_lists", name: "mod_list_compatibility_notes_ibfk_1"
   add_foreign_key "mod_list_custom_plugins", "mod_lists", name: "mod_list_custom_plugins_ibfk_1"
-  add_foreign_key "mod_list_installation_notes", "installation_notes", name: "mod_list_installation_notes_ibfk_2"
-  add_foreign_key "mod_list_installation_notes", "mod_lists", name: "mod_list_installation_notes_ibfk_1"
+  add_foreign_key "mod_list_install_order_notes", "installation_notes", column: "install_order_note_id", name: "mod_list_install_order_notes_ibfk_2"
+  add_foreign_key "mod_list_install_order_notes", "mod_lists", name: "mod_list_install_order_notes_ibfk_1"
+  add_foreign_key "mod_list_load_order_notes", "load_order_notes"
+  add_foreign_key "mod_list_load_order_notes", "mod_lists"
   add_foreign_key "mod_list_mods", "mod_lists", name: "mod_list_mods_ibfk_1"
   add_foreign_key "mod_list_mods", "mods", name: "mod_list_mods_ibfk_2"
-  add_foreign_key "mod_list_notes", "mod_lists", name: "mod_list_notes_ibfk_1"
   add_foreign_key "mod_list_plugins", "mod_lists", name: "mod_list_plugins_ibfk_1"
   add_foreign_key "mod_list_plugins", "plugins", name: "mod_list_plugins_ibfk_2"
   add_foreign_key "mod_list_stars", "mod_lists", name: "mod_list_stars_ibfk_1"
