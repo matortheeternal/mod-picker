874728351f03663bffc5a8365dbef10e879d8339
BlueAccords <myoldemailname@gmail.com>
2016-03-31 14:39:31 -0700

    starting to add fixtures for mod_list_compatibility_notes

--- mod-picker/spec/models/mod_list_compatibility_note_spec.rb
+++ mod-picker/spec/models/mod_list_compatibility_note_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
 
+
 RSpec.describe ModListCompatibilityNote, :model, :wip do
+  fixtures :mod_list_compatibility_notes
+
   it "should have a valid factory" do
     expect(build(:mod_list_compatibility_note)).to be_valid
+  end
+
+  it "should have a valid fixture example" do
+    expect(mod_list_compatibility_notes(:resolved)).to be_valid 
   end
 end
