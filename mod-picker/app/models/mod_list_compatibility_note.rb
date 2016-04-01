010d72cb9f085a14c926306c0db37ab6eae3f195
Mator <why101@msn.com>
2016-04-01 11:54:43 -0700

    updated mod list note model associations

--- mod-picker/app/models/mod_list_compatibility_note.rb
+++ mod-picker/app/models/mod_list_compatibility_note.rb
@@ -1,4 +1,4 @@
 class ModListCompatibilityNote < ActiveRecord::Base
-  belongs_to :mod_list
-  belongs_to :compatibility_note
+  belongs_to :mod_list, :inverse_of => 'mod_list_compatibility_notes'
+  belongs_to :compatibility_note, :inverse_of => 'mod_list_compatibility_notes'
 end
--- mod-picker/app/models/mod_list_install_order_note.rb
+++ mod-picker/app/models/mod_list_install_order_note.rb
@@ -1,4 +1,4 @@
 class ModListInstallOrderNote < ActiveRecord::Base
-  belongs_to :install_order_note
-  belongs_to :mod_list
+  belongs_to :install_order_note, :inverse_of => 'mod_list_install_order_notes'
+  belongs_to :mod_list, :inverse_of => 'mod_list_install_order_notes'
 end
