7608f3c374860f5c56ba54711b7a22b429618f31
Mator <why101@msn.com>
2016-04-01 16:32:35 -0700

    deleted some old migrations that were already run

--- mod-picker/db/migrate/20160217200410_add_correctable_counter_cache_columns.rb
+++
@@ -1,7 +0,0 @@
-class AddCorrectableCounterCacheColumns < ActiveRecord::Migration
-  def change
-    add_column :compatibility_notes, :incorrect_notes_count, :integer
-    add_column :installation_notes, :incorrect_notes_count, :integer
-    add_column :reviews, :incorrect_notes_count, :integer
-  end
-end
