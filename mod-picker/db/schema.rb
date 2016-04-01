c077668d86f9c61438360fd59475b113541003c4
Mator <why101@msn.com>
2016-04-01 15:09:16 -0700

    created and ran migration to add note counter cache columns to mods

--- mod-picker/db/schema.rb
+++ mod-picker/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160401192259) do
+ActiveRecord::Schema.define(version: 20160401213639) do
 
   create_table "agreement_marks", id: false, force: :cascade do |t|
     t.integer "incorrect_note_id", limit: 4
@@ -404,16 +404,19 @@ ActiveRecord::Schema.define(version: 20160401192259) do
   add_index "mod_versions", ["mod_id"], name: "mod_id", using: :btree
 
   create_table "mods", force: :cascade do |t|
-    t.string  "name",                  limit: 128
-    t.string  "aliases",               limit: 128
+    t.string  "name",                      limit: 128
+    t.string  "aliases",                   limit: 128
     t.boolean "is_utility"
     t.boolean "has_adult_content"
-    t.integer "game_id",               limit: 4
-    t.integer "primary_category_id",   limit: 4
-    t.integer "secondary_category_id", limit: 4
-    t.integer "mod_stars_count",       limit: 4
-    t.integer "reviews_count",         limit: 4
-    t.integer "mod_versions_count",    limit: 4
+    t.integer "game_id",                   limit: 4
+    t.integer "primary_category_id",       limit: 4
+    t.integer "secondary_category_id",     limit: 4
+    t.integer "mod_stars_count",           limit: 4
+    t.integer "reviews_count",             limit: 4
+    t.integer "mod_versions_count",        limit: 4
+    t.integer "compatibility_notes_count", limit: 4
+    t.integer "install_order_notes_count", limit: 4
+    t.integer "load_order_notes_count",    limit: 4
   end
 
   add_index "mods", ["game_id"], name: "fk_rails_3ec448a848", using: :btree
