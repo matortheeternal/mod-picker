0872ace440d96e2f07d045093d09bde03835904a
Mator <why101@msn.com>
2016-04-01 13:14:18 -0700

    more association work on install order and load order notes

--- mod-picker/app/models/install_order_note.rb
+++ mod-picker/app/models/install_order_note.rb
@@ -5,14 +5,21 @@ class InstallOrderNote < ActiveRecord::Base
   scope :mod, -> (id) { joins(:mod_versions).where(:mod_versions => {mod_id: id}) }
   scope :mv, -> (id) { joins(:mod_versions).where(:mod_versions => {id: id}) }
 
-  belongs_to :user, :foreign_key => 'submitted_by', :inverse_of => 'InstallOrderNote'
+  belongs_to :user, :foreign_key => 'submitted_by', :inverse_of => 'install_order_notes'
 
+  # mods associatied with this install order note
+  belongs_to :install_first_mod, :foreign_key => 'install_first', :class_name => 'Mod', :inverse_of => 'install_before_notes'
+  belongs_to :install_second_mod, :foreign_key => 'install_second', :class_name => 'Mod', :inverse_of => 'install_after_notes'
+
+  # mod versions this install order note is associated with
   has_many :mod_list_install_order_notes, :inverse_of => 'install_order_note'
-  has_many :mod_lists, :through => 'mod_list_installation_notes', :inverse_of => 'install_order_notes'
+  has_many :mod_lists, :through => 'mod_list_install_order_notes', :inverse_of => 'install_order_notes'
 
+  # mod lists this install order note appears on
   has_many :mod_version_install_order_notes, :inverse_of => 'install_order_note'
   has_many :mod_versions, :through => 'mod_version_install_order_notes', :inverse_of => 'install_order_notes'
 
+  # community feedback on this install order note
   has_many :helpful_marks, :as => 'helpfulable'
   has_many :incorrect_notes, :as => 'correctable'
 
--- mod-picker/app/models/plugin.rb
+++ mod-picker/app/models/plugin.rb
@@ -8,4 +8,8 @@ class Plugin < ActiveRecord::Base
   has_many :mod_lists, :through => 'mod_list_plugins', :inverse_of => 'plugins'
 
   has_many :compatibility_note_plugins, :foreign_key => 'compatibility_plugin_id', :inverse_of => 'compatibility_plugin'
+
+  # load order notes
+  has_many :load_before_notes, :foreign_key => 'load_first', :class_name => 'LoadOrderNote', :inverse_of => 'load_second_plugin'
+  has_many :load_after_notes, :foreign_key => 'load_second', :class_name => 'LoadOrderNote', :inverse_of => 'load_second_plugin'
 end
