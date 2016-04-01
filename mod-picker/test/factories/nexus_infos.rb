a49c505349d0eee6f7c1e7150175c66a0facfe41
Mator <why101@msn.com>
2016-04-01 16:10:26 -0700

    created nexus_info factory

--- mod-picker/test/factories/nexus_infos.rb
+++ mod-picker/test/factories/nexus_infos.rb
@@ -1,20 +1,16 @@
 FactoryGirl.define do
   factory :nexus_info do
-    id 1
-mod_id 1
-uploaded_by "MyText"
-authors "MyText"
-endorsements 1
-total_downloads 1
-unique_downloads 1
-views 1
-posts_count 1
-videos_count 1
-images_count 1
-files_count 1
-articles_count 1
-nexus_category 1
-changelog "MyText"
+    uploaded_by { Faker::Internet.user_name }
+    authors { Faker::Internet.user_name }
+    total_downloads { Faker::Number.between(1000, 2000000) }
+    unique_downloads { Faker::Number.between(100, 1000000) }
+    endorsements { Faker::Number.between(50, 100000) }
+    views { Faker::Number.between(1000, 10000000) }
+    posts_count { Faker::Number.between(1, 50000) }
+    videos_count { Faker::Number.between(1, 10) }
+    images_count { Faker::Number.between(1, 100) }
+    files_count { Faker::Number.between(1, 40) }
+    articles_count { Faker::Number.between(0, 6) }
+    nexus_category { Faker::Number.between(1, 100) }
   end
-
 end
