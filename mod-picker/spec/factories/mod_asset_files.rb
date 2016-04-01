944f9a1432add9ab31e1d486c1cee0df8918c6d4
BlueAccords <myoldemailname@gmail.com>
2016-03-31 18:28:28 -0700

    cursory validations/tests for mod_asset_files done

--- mod-picker/spec/factories/mod_asset_files.rb
+++ mod-picker/spec/factories/mod_asset_files.rb
@@ -1,7 +1,12 @@
+# create_table "mod_asset_files", force: :cascade do |t|
+#     t.string "filepath", limit: 128, null: false
+#   end
+
+#   add_index "mod_asset_files", ["filepath"], name: "filepath", unique: true, using: :btree
+  
 FactoryGirl.define do
   factory :mod_asset_file do
-    id 1
-filepath "MyString"
+    filepath "filepath"
   end
 
 end
--- mod-picker/spec/models/mod_asset_file_spec.rb
+++ mod-picker/spec/models/mod_asset_file_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
+# create_table "mod_asset_files", force: :cascade do |t|
+#     t.string "filepath", limit: 128, null: false
+#   end
+
+#   add_index "mod_asset_files", ["filepath"], name: "filepath", unique: true, using: :btree
+
 RSpec.describe ModAssetFile, :model, :wip do
-  
+  it "should have a valid factory" do
+    expect(build(:mod_asset_file)).to be_valid
+  end
+
+  context "validations" do
+    describe "filepath" do
+      it "should be invalid if blank" do
+        mod = build(:mod_asset_file,
+                    filepath: "")
+        mod.valid?
+
+        mod2 = build(:mod_asset_file,
+                     filepath: nil)
+
+        expect(mod.errors[:filepath]).to include("can't be blank")
+        expect(mod.errors[:filepath]).to include("can't be blank")
+      end
+    end
+  end
 end
