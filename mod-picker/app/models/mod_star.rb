2903d60c5f64d2cf7e24ea6fb899246e10b3746a
Mator <why101@msn.com>
2016-04-01 19:39:30 -0700

    fixed error in mod_star model

--- mod-picker/app/models/mod_star.rb
+++ mod-picker/app/models/mod_star.rb
@@ -1,5 +1,5 @@
 class ModStar < ActiveRecord::Base
-  self.primary_keys = :mod_list_id, :user_id
+  self.primary_keys = :mod_id, :user_id
 
   belongs_to :user, :inverse_of => 'mod_stars', :counter_cache => true
   belongs_to :mod, :inverse_of => 'mod_stars', :counter_cache => true
