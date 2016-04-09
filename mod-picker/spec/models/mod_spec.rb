400fb031a526cf7e5ad6a3221d1c375175139efa
BlueAccords <myoldemailname@gmail.com>
2016-04-09 14:00:43 -0700

    uncommented out mod_Spec test

--- mod-picker/spec/models/mod_spec.rb
+++ mod-picker/spec/models/mod_spec.rb
@@ -8,62 +8,62 @@ RSpec.describe Mod, :model do
     expect(skyui.name).to eq('SkyUI')
   end
 
-  # describe "counter cache" do
-  #   describe "mod_stars_count" do
-  #     count_before = skyui.mod_stars_count
-  #     mod_star = skyui.mod_stars.create(user_id: 3)
+  describe "counter cache" do
+    describe "mod_stars_count" do
+      count_before = skyui.mod_stars_count
+      mod_star = skyui.mod_stars.create(user_id: 3)
 
-  #     it "should increment when we add a star" do
-  #       expect(skyui.mod_stars_count).to eq(count_before + 1)
-  #     end
+      it "should increment when we add a star" do
+        expect(skyui.mod_stars_count).to eq(count_before + 1)
+      end
 
-  #     it "should decrement when we remove a star" do
-  #       mod_star.destroy
-  #       expect(skyui.mod_stars_count).to eq(count_before)
-  #     end
-  #   end
+      it "should decrement when we remove a star" do
+        mod_star.destroy
+        expect(skyui.mod_stars_count).to eq(count_before)
+      end
+    end
 
-  #   describe "reviews_count" do
-  #     count_before = skyui.reviews_count
-  #     review = skyui.reviews.create(submitted_by: 3, rating1: 10)
+    describe "reviews_count" do
+      count_before = skyui.reviews_count
+      review = skyui.reviews.create(submitted_by: 3, rating1: 10)
 
-  #     it "should increment when we add a review" do
-  #       expect(skyui.reviews_count).to eq(count_before + 1)
-  #     end
+      it "should increment when we add a review" do
+        expect(skyui.reviews_count).to eq(count_before + 1)
+      end
 
-  #     it "should decrement when we remove a review" do
-  #       review.destroy
-  #       expect(skyui.reviews_count).to eq(count_before)
-  #     end
-  #   end
+      it "should decrement when we remove a review" do
+        review.destroy
+        expect(skyui.reviews_count).to eq(count_before)
+      end
+    end
 
-  #   describe "mod_versions_count" do
-  #     count_before = skyui.mod_versions_count
-  #     mod_version = skyui.mod_versions.create(version: '1.0')
+    describe "mod_versions_count" do
+      count_before = skyui.mod_versions_count
+      mod_version = skyui.mod_versions.create(version: '1.0')
 
-  #     it "should increment when we add a mod_version" do
-  #       expect(skyui.mod_versions_count).to eq(count_before + 1)
-  #     end
+      it "should increment when we add a mod_version" do
+        expect(skyui.mod_versions_count).to eq(count_before + 1)
+      end
 
-  #     it "should decrement when we remove a mod_version" do
-  #       mod_version.destroy
-  #       expect(skyui.mod_versions_count).to eq(count_before)
-  #     end
-  #   end
+      it "should decrement when we remove a mod_version" do
+        mod_version.destroy
+        expect(skyui.mod_versions_count).to eq(count_before)
+      end
+    end
 
-  #   describe "compatibility_notes_count" do
-  #     count_before = skyui.compatibility_notes_count
-  #     compatibility_note = skyui.mod_versions.first.compatibility_notes.create(submitted_by: 3, compatibility_type: "Incompatible")
+    describe "compatibility_notes_count" do
+      count_before = skyui.compatibility_notes_count
+      compatibility_note = skyui.mod_versions.first.compatibility_notes.create(submitted_by: 3, compatibility_type: "Incompatible")
 
-  #     it "should increment when we add a compatibility_note" do
-  #       expect(skyui.compatibility_notes_count).to eq(count_before + 1)
-  #     end
+      it "should increment when we add a compatibility_note" do
+        expect(skyui.compatibility_notes_count).to eq(count_before + 1)
+      end
 
-  #     it "should decrement when we remove a compatibility_note" do
-  #       compatibility_note.destroy
-  #       expect(skyui.compatibility_notes_count).to eq(count_before)
-  #     end
-  #   end
+      it "should decrement when we remove a compatibility_note" do
+        compatibility_note.destroy
+        expect(skyui.compatibility_notes_count).to eq(count_before)
+      end
+    end
 
-  # end
+  end
 end
