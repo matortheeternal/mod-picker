ee4637afad6341616f6f2323ecdb73e4235c07b2
Mator <why101@msn.com>
2016-04-11 12:44:58 -0700

    added new function to get sorted game titles to userTitleService

--- mod-picker/app/assets/javascripts/angularAssets/Services/userTitleService.js
+++ mod-picker/app/assets/javascripts/angularAssets/Services/userTitleService.js
@@ -9,11 +9,18 @@ app.service('userTitleService', function (backend, $q) {
         return titles.promise;
     };
 
-    this.getUserTitle = function (titles, reputation) {
-        var prevTitle = "";
+    this.getSortedGameTitles = function(titles) {
         var gameTitles = titles.filter(function(title) {
             return (title.game_id === window._current_game_id);
         });
+        gameTitles.sort(function(a, b) {
+            return a.rep_required - b.rep_required;
+        });
+        return gameTitles;
+    };
+
+    this.getUserTitle = function (gameTitles, reputation) {
+        var prevTitle = gameTitles[0];
         gameTitles.forEach(function(title) {
             if (reputation < title.rep_required) {
                 return prevTitle;
