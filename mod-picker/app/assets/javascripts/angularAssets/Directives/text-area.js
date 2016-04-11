f60e4d8964e25730836dd1966967321908e85093
Mator <why101@msn.com>
2016-04-10 19:10:28 -0700

    attempting to get simple-mde data binding working with angular, not working yet

--- mod-picker/app/assets/javascripts/angularAssets/Directives/text-area.js
+++ mod-picker/app/assets/javascripts/angularAssets/Directives/text-area.js
@@ -9,6 +9,17 @@ app.directive('textArea', function () {
         controller: 'textAreaController',
         scope: {
             data: '='
+        },
+        link: function(scope, element, attrs) {
+            // get text area element and turn it into a markdown editor
+            var data;
+            var textarea = element.find("textarea")[0];
+            var mde = new SimpleMDE({ element: textarea });
+
+            mde.codemirror.on("change", function(){
+                data = mde.value();
+                scope.data = data;
+            });
         }
     }
 });
