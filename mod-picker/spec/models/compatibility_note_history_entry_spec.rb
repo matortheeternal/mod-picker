1f3151833f38ff7c08d3c63dbeb1f53ae28043b1
BlueAccords <myoldemailname@gmail.com>
2016-04-13 18:26:41 -0700

    added valid fixtures for compatibility_note_history

--- mod-picker/spec/models/compatibility_note_history_entry_spec.rb
+++ mod-picker/spec/models/compatibility_note_history_entry_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe CompatibilityNoteHistoryEntry, :model, :wip do
   fixtures :compatibility_note_history_entries
 
   it "should have a valid fixture" do
-    expect(compatibility_note_history_entries(:history_note_alpha)).to be_valid
+    expect(compatibility_note_history_entries(:history_note_apocalypse)).to be_valid
   end
 end
