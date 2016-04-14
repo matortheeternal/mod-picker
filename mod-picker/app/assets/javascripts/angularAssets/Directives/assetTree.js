5eca1b98625f5ed960d90ee275a94cf0a1334b49
TheSiriusAdam <thesiriusadam@gmail.com>
2016-04-14 16:22:18 -0500

    created a placeholder directive for the asset tree used in the analysis tab

---
+++ mod-picker/app/assets/javascripts/angularAssets/Directives/assetTree.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Sirius on 3/25/2016.
+ */
+
+app.directive('assetTree', function () {
+    return {
+        restrict: 'E',
+        templateUrl: '/resources/directives/assetTree.html',
+        controller: 'assetTreeController',
+        scope: {
+            data: '='
+        }
+    };
+});
+
+app.controller('assetTreeController', function ($scope) {
+//leaving this here in case it is needed when the directive is actually made
+});
