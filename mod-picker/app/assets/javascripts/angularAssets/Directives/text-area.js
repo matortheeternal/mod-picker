b075952486389f1fb6cc67ac58759c0f43b14cd3
Mator <why101@msn.com>
2016-04-14 16:14:24 -0700

    returned simpleMDE js and css to application layout disabled spellchecker, made data binding a bit more picky

--- mod-picker/app/assets/javascripts/angularAssets/Directives/text-area.js
+++ mod-picker/app/assets/javascripts/angularAssets/Directives/text-area.js
@@ -13,11 +13,11 @@ app.directive('textArea', function () {
         link: function(scope, element, attrs) {
             // get text area element and turn it into a markdown editor
             var textarea = element.find("textarea")[0];
-            var mde = new SimpleMDE({ element: textarea });
+            var mde = new SimpleMDE({ element: textarea, spellChecker: false });
 
             // two-way data binding to and from mde
             scope.$watch('data', function(value){
-                if (mde.value() !== value) {
+                if ((typeof value === "string") && (mde.value() !== value)) {
                     mde.value(value);
                     scope.$apply();
                 }
