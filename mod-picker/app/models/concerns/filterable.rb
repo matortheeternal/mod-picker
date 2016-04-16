c3865b38fa38d7b08903cf6ab59471517d4bce79
Mator <why101@msn.com>
2016-04-16 12:06:16 -0700

    fixed range filtering to use .min and .max

--- mod-picker/app/models/concerns/filterable.rb
+++ mod-picker/app/models/concerns/filterable.rb
@@ -6,9 +6,8 @@ module Filterable
       results = self.where(nil)
       filtering_params.each do |key, value|
         if value.present?
-          if value.include? '..' # value is a range
-            range = value.split('..')
-            results = results.public_send(key, range[0], range[1])
+          if value.min.present? || value.max.present?
+            results = results.public_send(key, value.min, value.max)
           else
             results = results.public_send(key, value)
           end
