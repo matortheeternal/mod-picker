acc4428ebd9d7f63c42b8972c59922f28d243f35
r79 <r@r79.io>
2016-04-18 02:17:09 +0200

    removed unnecessary timeout on all requests (yup, the page is much faster now)

--- mod-picker/app/assets/javascripts/angularAssets/Services/submitService.js
+++ mod-picker/app/assets/javascripts/angularAssets/Services/submitService.js
@@ -1,13 +1,7 @@
 //TODO: change naming
 app.service('submitService', function (backend, $q) {
     this.scrapeNexus = function (gameId, nexusId) {
-        var nexusInfo = $q.defer();
-        backend.retrieve('/nexus_infos/' + nexusId, {game_id: gameId}).then(function (data) {
-            setTimeout(function () {
-                nexusInfo.resolve(data);
-            }, 1000);
-        });
-        return nexusInfo.promise;
+        return backend.retrieve('/nexus_infos/' + nexusId, {game_id: gameId});
     };
 
     this.verifyPlugins = function (plugins) {
--- mod-picker/app/assets/javascripts/angularAssets/Services/userService.js
+++ mod-picker/app/assets/javascripts/angularAssets/Services/userService.js
@@ -1,13 +1,6 @@
 app.service('userService', function (backend, $q) {
     this.retrieveUser = function (userId) {
-        var user = $q.defer();
-        backend.retrieve('/users/' + userId).then(function (data) {
-            //TODO: remove mocked delay
-            setTimeout(function () {
-                user.resolve(data);
-            }, 1000);
-        });
-        return user.promise;
+        return backend.retrieve('/users/' + userId);
     };
 
 });
