74d7d4aa95fc38d5d34634393e7ab11e50d57cc1
ThreeTen22 <gtumen@gmail.com>
2016-04-19 23:43:00 -0500

    modpage view init commit

--- mod-picker/app/assets/javascripts/angularAssets/Views/cnoteTemp.js
+++ mod-picker/app/assets/javascripts/angularAssets/Views/cnoteTemp.js
@@ -9,7 +9,7 @@ app.config(['$routeProvider', function ($routeProvider) {
 }]);
 
 app.controller('cnoteController', function ($scope) {
-    useTwoColumns(true);
+    //useTwoColumns(true);
     $scope.$watch('data', function (newValue) {
        // here we could have the cool part that connects to the server. Uncomment the next line to see some cool magic.
        // console.log(newValue);
