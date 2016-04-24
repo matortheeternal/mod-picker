eaf10fafa9d6270399fac1f80e53e4a14bb2bd78
TheSiriusAdam <thesiriusadam@gmail.com>
2016-04-24 15:38:51 -0500

    user route now loads correctly and the tabs point to the right places

--- mod-picker/app/assets/javascripts/angularAssets/Views/user.js
+++ mod-picker/app/assets/javascripts/angularAssets/Views/user.js
@@ -1,22 +1,22 @@
 app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/user/:userId', {
-            templateUrl: '/resources/partials/user.html',
+            templateUrl: '/resources/partials/showUser/user.html',
             controller: 'userController'
         }
     );
 }]);
 
 app.controller('userController', function ($scope, $q, $routeParams, userService) {
-    userService.retrieveUser($routeParams.userId).then(function (mod) {
+    userService.retrieveUser($routeParams.userId).then(function (user) {
         $scope.user = user;
     });
 
     //of the tab data
     $scope.tabs = [
-        { name: 'Compatibility notes', url: '/resources/partials/showMod/compatibility.html' },
-        { name: 'Installation', url: '/resources/partials/showMod/installation.html' },
-        { name: 'Reviews', url: '/resources/partials/showMod/reviews.html' },
-        { name: 'Analysis', url: '/resources/partials/showMod/analysis.html' }
+        { name: 'Social', url: '/resources/partials/showUser/social.html' },
+        { name: 'Mod Lists and Collections', url: '/resources/partials/showUser/lists.html' },
+        { name: 'Authored Mods', url: '/resources/partials/showUser/mods.html' },
+        { name: 'Contributions', url: '/resources/partials/showUser/contributions.html' }
     ];
 
     $scope.currentTab = $scope.tabs[0];
