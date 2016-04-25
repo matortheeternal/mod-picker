ffbeb7b317103e3670e763c23dd6f74fb2406f70
TheSiriusAdam <thesiriusadam@gmail.com>
2016-04-24 20:08:35 -0500

    fixed issues with directive names

---
+++ mod-picker/app/assets/javascripts/angularAssets/Directives/notePartials/lNote.js
@@ -0,0 +1,14 @@
+app.directive('lNote', function () {
+    return {
+        restrict: 'E',
+        templateUrl: '/resources/directives/notePartials/lNote.html',
+        controller: 'lNoteController',
+        scope: {
+            note: '='
+        }
+    };
+});
+
+app.controller('lNoteController', function ($scope) {
+//leaving this here in case it is needed when the directive is actually made
+});
