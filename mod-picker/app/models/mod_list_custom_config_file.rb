1ea0841a0bd301b5588193ea14ef35974aa66488
BlueAccords <myoldemailname@gmail.com>
2016-04-25 17:39:14 -0700

    Added validations to mod_list_custom_config_files

--- mod-picker/app/models/mod_list_custom_config_file.rb
+++ mod-picker/app/models/mod_list_custom_config_file.rb
@@ -1,3 +1,7 @@
 class ModListCustomConfigFile < ActiveRecord::Base
   belongs_to :mod_list, :inverse_of => 'custom_config_files'
+
+  # validations
+  validates :mod_list_id, :filename, :install_path, presence: true
+  validates :text_body, length: {maximum: 4096}
 end
