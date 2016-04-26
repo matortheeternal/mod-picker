e2cc4bf2642de13099a3e9d18f3c7ac74e9c92f0
BlueAccords <myoldemailname@gmail.com>
2016-04-25 18:51:09 -0700

    added validations to review_template

--- mod-picker/app/models/review_template.rb
+++ mod-picker/app/models/review_template.rb
@@ -1,4 +1,9 @@
 class ReviewTemplate < ActiveRecord::Base
   belongs_to :user, :foreign_key => 'submitted_by', :inverse_of => 'reviews'
   has_many :reviews, :inverse_of => 'review_template'
+
+  # Validations
+  validates :name, :section1, presence: true
+  validates :name, length: {in: 4..32}
+  validates :section1, :section2, :section3, :section4, :section5, length: {in: 2..32}
 end
