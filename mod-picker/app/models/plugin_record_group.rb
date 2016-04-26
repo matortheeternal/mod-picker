f2c4e985c32136c5b55a87e361b3f35ed3e5b8d3
BlueAccords <myoldemailname@gmail.com>
2016-04-25 18:29:48 -0700

    Added validations to plugin_record_group

--- mod-picker/app/models/plugin_record_group.rb
+++ mod-picker/app/models/plugin_record_group.rb
@@ -2,4 +2,8 @@ class PluginRecordGroup < ActiveRecord::Base
   self.primary_keys = :plugin_id, :sig
 
   belongs_to :plugin, :inverse_of => 'record_groups'
+
+  # Validations
+  validates :plugin_id, :signature, :new_records, :override_records, presence: true
+  validates :signature, length: {maximum: 4}
 end
