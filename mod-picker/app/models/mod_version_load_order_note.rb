7ea6b156a6526b7677862f4a3c09a29f0e6e5dd0
BlueAccords <myoldemailname@gmail.com>
2016-04-25 18:00:58 -0700

    Added validations to mod_version_load_order_note

--- mod-picker/app/models/mod_version_load_order_note.rb
+++ mod-picker/app/models/mod_version_load_order_note.rb
@@ -7,6 +7,7 @@ class ModVersionLoadOrderNote < ActiveRecord::Base
   belongs_to :mod_version, :inverse_of => 'mod_version_load_order_notes'
   belongs_to :load_order_note, :inverse_of => 'mod_version_load_order_notes'
 
+  validates :mod_version_id, :load_order_note_id, presence: true
   private
     def increment_counter_caches
       self.mod_version.mod.install_order_notes_count += 1
