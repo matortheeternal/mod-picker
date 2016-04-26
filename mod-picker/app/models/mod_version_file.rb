6239e2cd4d0a1db9bc091ae096d0e9436b78aab1
BlueAccords <myoldemailname@gmail.com>
2016-04-25 18:05:14 -0700

    Added validation to mod_version_file

--- mod-picker/app/models/mod_version_file.rb
+++ mod-picker/app/models/mod_version_file.rb
@@ -3,4 +3,8 @@ class ModVersionFile < ActiveRecord::Base
 
   belongs_to :mod_version, :inverse_of => 'ModVersionFile'
   belongs_to :mod_asset_file, :inverse_of => 'ModVersionFile'
+
+  # Validations
+  validates :mod_version_id, :mod_asset_file_id, presence: true
+  
 end
