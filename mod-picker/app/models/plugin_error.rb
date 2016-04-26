b77996281dcbf32fb97bed35f2b52ccb6350c3bf
BlueAccords <myoldemailname@gmail.com>
2016-04-25 18:32:59 -0700

    added validations to plugin_errors

--- mod-picker/app/models/plugin_error.rb
+++ mod-picker/app/models/plugin_error.rb
@@ -1,3 +1,7 @@
 class PluginError < ActiveRecord::Base
   belongs_to :plugin, :inverse_of => 'errors'
+
+  # Validations
+  validates :plugin_id, :signature, :form_id, :type, presence: true
+  validates :signature, length: {maximum: 4}
 end
