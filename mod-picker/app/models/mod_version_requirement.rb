1cf9d97f813f15b271ee8cda46410ec5e3089572
BlueAccords <myoldemailname@gmail.com>
2016-04-25 18:06:45 -0700

    added validatoins to mod_version_requirement

--- mod-picker/app/models/mod_version_requirement.rb
+++ mod-picker/app/models/mod_version_requirement.rb
@@ -3,4 +3,7 @@ class ModVersionRequirement < ActiveRecord::Base
 
   belongs_to :mod_version, :inverse_of => 'requires'
   belongs_to :required_mod_version, :class_name => 'ModVersion', :inverse_of => 'required_by', :foreign_key => 'required_id'
+
+  # Validations
+  validates :mod_version_id, :required_id, presence: true
 end
