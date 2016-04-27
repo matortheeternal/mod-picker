75f7dab153b0924dd4df31b8a51fdabc3909e273
Mator <why101@msn.com>
2016-04-27 13:15:55 -0700

    removed some unnecessary routes

--- mod-picker/config/routes.rb
+++ mod-picker/config/routes.rb
@@ -8,35 +8,15 @@ Rails.application.routes.draw do
     resources :install_order_notes
     resources :load_order_notes
     resources :compatibility_notes
-    resources :mod_version_compatibility_notes
-    resources :mod_list_compatibility_notes
-    resources :mod_list_install_order_notes
-    resources :mod_list_load_order_notes
-    resources :agreement_marks
     resources :incorrect_notes
-    resources :helpful_marks
-    resources :agreement_marks
     resources :reviews
     resources :mod_authors
-    resources :mod_list_stars
-    resources :mod_stars
-    resources :reputation_links
-    resources :user_reputations
     resources :user_settings
     resources :user_bios
-    resources :mod_list_mods
-    resources :mod_list_custom_plugins
-    resources :mod_list_plugins
     resources :mod_lists
-    resources :plugin_record_groups
-    resources :override_records
-    resources :masters
     resources :plugins
-    resources :mod_version_files
     resources :mod_asset_files
     resources :mod_versions
-    resources :lover_infos
-    resources :workshop_infos
     resources :nexus_infos
     resources :users
 
