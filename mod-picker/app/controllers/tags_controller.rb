f4f4aeec20bc73f2d97508840ec61cb6f24b0caa
Mator <why101@msn.com>
2016-04-27 13:17:34 -0700

    created tags controller

---
+++ mod-picker/app/controllers/tags_controller.rb
@@ -0,0 +1,31 @@
+class TagsController < ApplicationController
+  before_action :set_tag, only: [:destroy]
+
+  # GET /tags
+  def index
+    @tags = Tag.filter(filtering_params)
+
+    render :json => @tags
+  end
+
+  # DELETE /mods/1
+  def destroy
+    authorize! :destroy, @tag
+    if @tag.destroy
+      render json: {status: :ok}
+    else
+      render json: @tag.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_tag
+      @tag = Tag.find(params[:id])
+    end
+
+    # Params we allow filtering on
+    def filtering_params
+      params[:filters].slice(:game);
+    end
+end
