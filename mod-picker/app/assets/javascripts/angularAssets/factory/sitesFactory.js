004243f7aa0a8c136a5354f73a1286e35737eef1
Mator <why101@msn.com>
2016-04-27 16:46:35 -0700

    created sites factory for mod source submission

---
+++ mod-picker/app/assets/javascripts/angularAssets/factory/sitesFactory.js
@@ -0,0 +1,18 @@
+app.service('sitesFactory', function () {
+    this.sites = function() {
+        return [
+            {
+                label: "Nexus Mods",
+                expr: /(http[s]:\/\/?)?www.nexusmods.com\/skyrim\/mods\/([0-9]+)(\/\?)?/i
+            },
+            {
+                label: "Steam Workshop",
+                expr: /(http[s]:\/\/?)?steamcommunity.com\/sharedfiles\/filedetails\/\?id=([0-9]+)(\&)?.*/i
+            },
+            {
+                label: "Lovers Lab",
+                expr: /(http[s]:\/\/?)?www.loverslab.com\/files\/file\/([0-9a-z\-]+)(\/)?/i
+            }
+        ];
+    }
+});
