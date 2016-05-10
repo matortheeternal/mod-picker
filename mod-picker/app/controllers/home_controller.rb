f8322e22927ac9cbc0e922eec47c4c22f49719c5
Mator <why101@msn.com>
2016-05-09 21:49:42 -0700

    optimized queries and json for home#index

--- mod-picker/app/controllers/home_controller.rb
+++ mod-picker/app/controllers/home_controller.rb
@@ -32,23 +32,73 @@ class HomeController < ApplicationController
   def index
     # load recent data
     @articles = Article.order(:created_at => :DESC).limit(5)
-    @mod_lists = ModList.where("game_id = ? AND status = 3 AND hidden = false", params[:game]).order(:completed => :DESC).limit(10)
+    @mod_lists = ModList.where("game_id = ? AND status = 3 AND hidden = false", params[:game]).includes(:user => :reputation).order(:completed => :DESC).limit(10)
     @mods = Mod.where("game_id = ? AND hidden = false", params[:game]).order(:id => :ASC).limit(10)
-    @reviews = Review.where("game_id = ? AND hidden = false", params[:game]).order(:submitted => :DESC).limit(10)
-    @corrections = IncorrectNote.where("game_id = ? AND hidden = false", params[:game]).order(:created_at => :DESC).limit(10)
-    @compatibility_notes = CompatibilityNote.where("game_id = ? AND hidden = false", params[:game]).order(:submitted => :DESC).limit(10)
-    @install_order_notes = InstallOrderNote.where("game_id = ? AND hidden = false", params[:game]).order(:submitted => :DESC).limit(10)
-    @load_order_notes = LoadOrderNote.where("game_id = ? AND hidden = false", params[:game]).order(:submitted => :DESC).limit(10)
+    @reviews = Review.where("game_id = ? AND hidden = false", params[:game]).includes(:mod, :user => :reputation).order(:submitted => :DESC).limit(10)
+    @corrections = IncorrectNote.where("game_id = ? AND hidden = false", params[:game]).includes(:user => :reputation).order(:created_at => :DESC).limit(10)
+    @compatibility_notes = CompatibilityNote.where("game_id = ? AND hidden = false", params[:game]).includes(:first_mod, :second_mod, :user => :reputation).order(:submitted => :DESC).limit(10)
+    @install_order_notes = InstallOrderNote.where("game_id = ? AND hidden = false", params[:game]).includes(:first_mod, :second_mod, :user => :reputation).order(:submitted => :DESC).limit(10)
+    @load_order_notes = LoadOrderNote.where("game_id = ? AND hidden = false", params[:game]).includes(:first_plugin, :second_plugin, :user => :reputation).order(:submitted => :DESC).limit(10)
 
     render :json => {
         articles: @articles.as_json,
         recent: {
             mod_lists: @mod_lists.as_json,
-            mods: @mods.as_json,
-            reviews: @reviews.as_json,
+            mods: @mods.as_json({
+                :only => [:id, :name, :primary_category_id, :secondary_category_id, :mod_stars_count, :status, :reputation, :average_rating, :released]
+            }),
+            reviews: @reviews.as_json({
+                :only => [:id, :submitted, :edited, :incorrect_notes_count],
+                :include => {
+                    :user => {
+                        :only => [:id, :username, :role, :title],
+                        :include => {
+                            :reputation => {:only => [:overall]}
+                        },
+                        :methods => :avatar
+                    },
+                    :mod => {
+                        :only => [:id, :name]
+                    }
+                }
+            }),
             corrections: @corrections.as_json,
-            compatibility_notes: @compatibility_notes.as_json,
-            install_order_notes: @install_order_notes.as_json,
+            compatibility_notes: @compatibility_notes.as_json({
+                :only => [:id, :compatibility_type, :submitted, :edited, :text_body, :incorrect_notes_count],
+                :include => {
+                    :first_mod => {
+                        :only => [:id, :name]
+                    },
+                    :second_mod => {
+                        :only => [:id, :name]
+                    },
+                    :user => {
+                        :only => [:id, :username, :role, :title],
+                        :include => {
+                            :reputation => {:only => [:overall]}
+                        },
+                        :methods => :avatar
+                    }
+                }
+            }),
+            install_order_notes: @install_order_notes.as_json({
+                :only => [:id, :submitted, :edited, :text_body],
+                :include => {
+                    :first_mod => {
+                        :only => [:id, :name]
+                    },
+                    :second_mod => {
+                        :only => [:id, :name]
+                    },
+                    :user => {
+                        :only => [:id, :username, :role, :title],
+                        :include => {
+                            :reputation => {:only => [:overall]}
+                        },
+                        :methods => :avatar
+                    }
+                }
+            }),
             load_order_notes: @load_order_notes.as_json
         }
     }
