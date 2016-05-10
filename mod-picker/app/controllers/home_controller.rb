665c8e2338ab060ca9d5b3c175ef5d05c7e83b3f
Mator <why101@msn.com>
2016-05-09 20:39:41 -0700

    home controller now loads all data it needs - note that this code is untested

--- mod-picker/app/controllers/home_controller.rb
+++ mod-picker/app/controllers/home_controller.rb
@@ -30,19 +30,26 @@ class HomeController < ApplicationController
   end
 
   def index
+    # load recent data
     @articles = Article.order(:created_at => :DESC).limit(5)
-    @mod_lists = ModList.where("status = 3 AND hidden = false AND completed NOT NULL").order(:completed => :DESC).limit(10)
+    @mod_lists = ModList.where("game_id = ? AND status = 3 AND hidden = false", params[:game]).order(:completed => :DESC).limit(10)
+    @mods = Mod.where("game_id = ? AND hidden = false", params[:game]).order(:id => :ASC).limit(10)
+    @reviews = Review.where("game_id = ? AND hidden = false", params[:game]).order(:submitted => :DESC).limit(10)
+    @corrections = IncorrectNote.joins(:correctable).where(:correctable => { :game_id => params[:game], :hidden => false}).order(:submitted => :DESC).limit(10)
+    @compatibility_notes = CompatibilityNote.where("game_id = ? AND hidden = false", params[:game]).order(:submitted => :DESC).limit(10)
+    @install_order_notes = InstallOrderNote.where("game_id = ? AND hidden = false", params[:game]).order(:submitted => :DESC).limit(10)
+    @load_order_notes = LoadOrderNote.where("game_id = ? AND hidden = false", params[:game]).order(:submitted => :DESC).limit(10)
+
     render :json => {
         articles: @articles.as_json,
-        recent_stuff: {
+        recent: {
             mod_lists: @mod_lists.as_json,
-            mods: {},
-            comments: {},
-            reviews: {},
-            corrections: {},
-            compatibility_notes: {},
-            install_order_notes: {},
-            load_order_notes: {},
+            mods: @mods.as_json,
+            reviews: @reviews.as_json,
+            corrections: @corrections.as_json,
+            compatibility_notes: @compatibility_notes.as_json,
+            install_order_notes: @install_order_notes.as_json,
+            load_order_notes: @load_order_notes.as_json
         }
     }
   end
