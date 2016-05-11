8c6b60ad5739c7f3621ac5c1d008f07e1a91635c
TheSiriusAdam <thesiriusadam@gmail.com>
2016-05-11 01:03:32 -0500

    tweaked number of recent modlists, mods, and other contributions

--- mod-picker/app/controllers/home_controller.rb
+++ mod-picker/app/controllers/home_controller.rb
@@ -34,10 +34,10 @@ class HomeController < ApplicationController
     articles = Article.order(:created_at => :DESC).limit(5)
 
     # we include associated data we know we'll need because it increases the speed of the query
-    mod_lists = ModList.where("game_id = ? AND status = 3 AND hidden = false", params[:game]).includes(:user => :reputation).order(:completed => :DESC).limit(10)
-    mods = Mod.where("game_id = ? AND hidden = false", params[:game]).order(:id => :ASC).limit(10)
-    reviews = Review.where("game_id = ? AND hidden = false", params[:game]).includes(:mod, :user => :reputation).order(:submitted => :DESC).limit(10)
-    corrections = IncorrectNote.where("game_id = ? AND hidden = false", params[:game]).includes(:user => :reputation).order(:created_at => :DESC).limit(10)
+    mod_lists = ModList.where("game_id = ? AND status = 3 AND hidden = false", params[:game]).includes(:user => :reputation).order(:completed => :DESC).limit(5)
+    mods = Mod.where("game_id = ? AND hidden = false", params[:game]).order(:id => :ASC).limit(5)
+    reviews = Review.where("game_id = ? AND hidden = false", params[:game]).includes(:mod, :user => :reputation).order(:submitted => :DESC).limit(4)
+    corrections = IncorrectNote.where("game_id = ? AND hidden = false", params[:game]).includes(:user => :reputation).order(:created_at => :DESC).limit(4)
     compatibility_notes = CompatibilityNote.where("game_id = ? AND hidden = false", params[:game]).includes(:first_mod, :second_mod, :user => :reputation).order(:submitted => :DESC).limit(10)
     install_order_notes = InstallOrderNote.where("game_id = ? AND hidden = false", params[:game]).includes(:first_mod, :second_mod, :user => :reputation).order(:submitted => :DESC).limit(10)
     load_order_notes = LoadOrderNote.where("game_id = ? AND hidden = false", params[:game]).includes(:first_plugin, :second_plugin, :user => :reputation).order(:submitted => :DESC).limit(10)
