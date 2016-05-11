4ee57218e9e2f10090412c0f964939949581e3f1
TheSiriusAdam <thesiriusadam@gmail.com>
2016-05-11 01:00:42 -0500

    assuming skyrim to continue work on the page

--- mod-picker/app/assets/javascripts/angularAssets/BackendAPI/landingService.js
+++ mod-picker/app/assets/javascripts/angularAssets/BackendAPI/landingService.js
@@ -1,6 +1,5 @@
 app.service('landingService', function (backend, $q) {
     this.retrieveLanding = function () {
-        return backend.retrieve('/home');
+        return backend.retrieve('/home', {game: 1}); //TODO: this needs to be generalized and get the current game instead of assuming skyrim
     };
-
 });
