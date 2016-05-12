fef7b2b33145df12851b1c249a6739842c4d3cf5
Mator <why101@msn.com>
2016-05-12 10:28:43 -0700

    Revert "added authenticate! before_action to user_setting_controller and users_controller"
    This reverts commit 862e868e4f9feb6ced1994892af5c870ee776d4a.

--- mod-picker/app/controllers/users_controller.rb
+++ mod-picker/app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
-  before_action :authenticate_user!, only: [:link_account, :update, :destroy]
 
   # GET /users
   def index
