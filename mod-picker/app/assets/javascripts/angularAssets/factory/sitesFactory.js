edf8a36a9fd1c9109e589c90e013e8d297e6be5a
Mator <why101@msn.com>
2016-05-12 16:25:37 -0700

    added loginUrl to sitesFactory

--- mod-picker/app/assets/javascripts/angularAssets/factory/sitesFactory.js
+++ mod-picker/app/assets/javascripts/angularAssets/factory/sitesFactory.js
@@ -4,17 +4,20 @@ app.service('sitesFactory', function () {
             {
                 label: "Nexus Mods",
                 modUrlFormat: /(http[s]:\/\/?)?www.nexusmods.com\/skyrim\/mods\/([0-9]+)(\/\?)?/i,
-                userUrlFormat: /(http[s]:\/\/?)?forums.nexusmods.com\/index.php\?\/user\/([0-9]+)\-([A-Za-z0-9\-]+)(\/)?/i
+                userUrlFormat: /(http[s]:\/\/?)?forums.nexusmods.com\/index.php\?\/user\/([0-9]+)\-([A-Za-z0-9\-]+)(\/)?/i,
+                loginUrl: "https://forums.nexusmods.com/"
             },
             {
                 label: "Steam Workshop",
                 modUrlFormat: /(http[s]:\/\/?)?steamcommunity.com\/sharedfiles\/filedetails\/\?id=([0-9]+)(\&)?.*/i,
-                userUrlFormat: /(http[s]:\/\/?)?steamcommunity.com\/id\/([A-Za-z0-9\_]+)(\/)?/i
+                userUrlFormat: /(http[s]:\/\/?)?steamcommunity.com\/id\/([A-Za-z0-9\_]+)(\/)?/i,
+                loginUrl: "https://steamcommunity.com/login/"
             },
             {
                 label: "Lover's Lab",
                 modUrlFormat: /(http[s]:\/\/?)?www.loverslab.com\/files\/file\/([0-9]+)\-([0-9a-z\-]+)(\/)?/i,
-                userUrlFormat: /(http[s]:\/\/?)?www.loverslab.com\/index.php\?\/user\/([0-9]+)\-([A-Za-z0-9\-]+)(\/)?/i
+                userUrlFormat: /(http[s]:\/\/?)?www.loverslab.com\/index.php\?\/user\/([0-9]+)\-([A-Za-z0-9\-]+)(\/)?/i,
+                loginUrl: "https://www.loverslab.com/"
             }
         ];
     };
