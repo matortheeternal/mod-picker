66fd2950017fc34f44609f27d13ee0a2a87b0102
Mator <why101@msn.com>
2016-05-12 16:31:15 -0700

    added linkGenericAccount directive

---
+++ mod-picker/app/assets/javascripts/angularAssets/Directives/userSettings/linkGenericAccount.js
@@ -0,0 +1,60 @@
+app.directive('linkGenericAccount', function () {
+    return {
+        restrict: 'E',
+        templateUrl: '/resources/directives/userSettings/linkGenericAccount.html',
+        controller: 'linkGenericAccountController',
+        scope: {
+            siteLabel: '=',
+            siteUsername: '=',
+            sitePostsCount: '=',
+            siteDateJoined: '=',
+            siteVerificationToken: '='
+        }
+    }
+});
+
+app.controller('linkGenericAccountController', function ($scope, $timeout, userSettingsService, sitesFactory) {
+    $scope.showModal = false;
+    $scope.waiting = false;
+    $scope.verified = false;
+    $scope.site = sitesFactory.getSite(sitesFactory.sites(), $scope.siteLabel);
+
+    $scope.focusText = function($event) {
+        $event.target.select();
+    };
+
+    $scope.toggleModal = function() {
+        $scope.showModal = !$scope.showModal;
+    };
+
+    $scope.validateUrl = function() {
+        var match  = $scope.userUrl.match($scope.site.userUrlFormat);
+        $scope.urlValid = match !== null;
+    };
+
+    $scope.verifyUser = function() {
+        // exit if we're waiting for the next request or the user has been verified
+        if ($scope.waiting || $scope.verified) {
+            return;
+        }
+
+        // exit if the url is invalid
+        var match  = $scope.userUrl.match($scope.site.userUrlFormat);
+        if (!match) {
+            return;
+        }
+
+        $scope.errors = [];
+        $scope.waiting = true;
+        var user_path = match[2];
+        userSettingsService.verifyAccount($scope.siteLabel, user_path).then(function(data) {
+            $scope.verified = data.verified;
+            if (!data.verified) {
+                $scope.errors.push({ message: "Failed to verify account. You can try again in 30 seconds." });
+            }
+        });
+        $timeout(function() {
+            $scope.waiting = false;
+        }, 30000);
+    };
+});
