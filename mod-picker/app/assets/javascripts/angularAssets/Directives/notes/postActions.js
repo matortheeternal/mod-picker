8b43fd0fe1e9955820a68274c991cf7cec51b74a
Mator <why101@msn.com>
2016-05-13 13:23:49 -0700

    created generic postActions directive for the actions bar beneath reviews and notes

---
+++ mod-picker/app/assets/javascripts/angularAssets/Directives/notes/postActions.js
@@ -0,0 +1,28 @@
+app.directive('postActions', function () {
+    return {
+        restrict: 'E',
+        templateUrl: '/resources/directives/notes/postActions.html',
+        scope: {
+            target: '=',
+            route: '='
+        }
+    };
+});
+
+app.controller('postActionsController', function ($scope, modService) {
+    $scope.helpfulMark = function(helpful) {
+        if ($scope.target.helpful == helpful) {
+            modService.helpfulMark($scope.route, $scope.target.id).then(function (data) {
+                if (data.status == "ok") {
+                    delete $scope.target.helpful;
+                }
+            });
+        } else {
+            modService.helpfulMark($scope.route, $scope.target.id, helpful).then(function (data) {
+                if (data.status == "ok") {
+                    $scope.target.helpful = helpful;
+                }
+            });
+        }
+    };
+});
