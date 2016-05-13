06582acd3af6fdbfa25b5b2881de507358dd1bd6
Mator <why101@msn.com>
2016-05-13 10:58:19 -0700

    added compatibility verb logic to compatibilityNoteController - should probably move into a service

--- mod-picker/app/assets/javascripts/angularAssets/Directives/notes/compatibilityNote.js
+++ mod-picker/app/assets/javascripts/angularAssets/Directives/notes/compatibilityNote.js
@@ -15,6 +15,21 @@ app.directive('compatibilityNote', function () {
 });
 
 app.controller('compatibilityNoteController', function ($scope, modService) {
+    // TODO: Should probably be moved into some kind of service
+    function getVerb() {
+        switch ($scope.note.compatibility_type) {
+            case "incompatible":
+                return "with";
+            case "partially compatible":
+                return "with";
+            default:
+                return "for";
+        }
+    }
+
+    // set compatibility_verb
+    $scope.note.compatibility_verb = getVerb();
+
     $scope.helpfulMark = function(helpful) {
         if ($scope.note.helpful == helpful) {
             modService.helpfulMark("compatibility_notes", $scope.note.id).then(function (data) {
