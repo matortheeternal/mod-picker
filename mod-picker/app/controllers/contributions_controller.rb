fc86aa622531abd3adbb0ea54049ac1186944b81
Mator <why101@msn.com>
2016-05-13 18:09:58 -0700

    renamed helpfulable_controller to contributions_controller, added methods for hiding and approving contributions

---
+++ mod-picker/app/controllers/contributions_controller.rb
@@ -0,0 +1,45 @@
+class ContributionsController < ApplicationController
+  # POST /contribution/1/helpful
+  def helpful
+    helpful_mark = HelpfulMark.find_or_create_by(
+        submitted_by: current_user.id,
+        helpfulable_id: params[:id],
+        helpfulable_type: controller_name.classify)
+    if params.has_key?(:helpful)
+      helpful_mark.helpful = params[:helpful]
+      if helpful_mark.save
+        render json: {status: :ok}
+      else
+        render json: helpful_mark.errors, status: :unprocessable_entity
+      end
+    else
+      if helpful_mark.destroy
+        render json: {status: :ok}
+      else
+        render json: helpful_mark.errors, status: :unprocessable_entity
+      end
+    end
+  end
+
+  # POST /contribution/1/hide
+  def hide
+    authorize! :hide, @contribution
+    @contribution.hidden = params[:hidden]
+    if @contribution.save
+      render json: {status: ok}
+    else
+      render json: @contribution.errors, status: :unprocessable_entity
+    end
+  end
+
+  # POST /contribution/1/approve
+  def approve
+    authorize! :approve, @contribution
+    @contribution.approved = params[:approved]
+    if @contribution.save
+      render json: {status: ok}
+    else
+      render json: @contribution.errors, status: :unprocessable_entity
+    end
+  end
+end
