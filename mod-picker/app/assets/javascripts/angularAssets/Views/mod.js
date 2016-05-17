0185642f9b5f34dbf6faefa625583d8a1ddfbbca
Mator <why101@msn.com>
2016-05-16 20:13:16 -0700

    fixed mod analysis nestedAssets and asset tree

--- mod-picker/app/assets/javascripts/angularAssets/Views/mod.js
+++ mod-picker/app/assets/javascripts/angularAssets/Views/mod.js
@@ -235,8 +235,8 @@ app.controller('modController', function ($scope, $q, $stateParams, $timeout, mo
 
     $scope.retrieveAnalysis = function() {
         modService.retrieveAnalysis($stateParams.modId).then(function(analysis) {
-            analysis.nestedAssets = assetUtils.convertDataStringToNestedObject(analysis.assets);
             $scope.mod.assets = analysis.assets;
+            $scope.mod.nestedAssets = assetUtils.convertDataStringToNestedObject(analysis.assets);
             $scope.mod.plugins = analysis.plugins;
             if ($scope.mod.plugins.length > 0) {
                 $scope.currentPlugin = analysis.plugins[0];
