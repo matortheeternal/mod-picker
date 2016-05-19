22767b8f41260c7abb9db4be11128163040c981a
Mator <why101@msn.com>
2016-05-18 17:07:44 -0700

    plugins search now also returns mod_id for the purposes of the automatic requirement adding on the submit mod page

--- mod-picker/app/controllers/plugins_controller.rb
+++ mod-picker/app/controllers/plugins_controller.rb
@@ -13,7 +13,7 @@ class PluginsController < ApplicationController
     @plugins = Plugin.filter(search_params).sort({ column: "filename", direction: "ASC" }).limit(10)
 
     render :json => @plugins.as_json({
-        :only => [:id, :filename]
+        :only => [:mod_id, :id, :filename]
     })
   end
 
