8e4deb905e2c136f76ee3352bebb34b8245263f3
Mator <why101@msn.com>
2016-05-20 17:41:28 -0700

    Added set_dates callback to BaseReport BaseReport now inherits from EnhancedRecord::Base

--- mod-picker/app/models/base_report.rb
+++ mod-picker/app/models/base_report.rb
@@ -1,4 +1,16 @@
-class BaseReport < ActiveRecord::Base
+class BaseReport < EnhancedRecord::Base
   belongs_to :reportable, :polymorphic => true
   has_many :reports, :inverse_of => 'base_report'
+
+  # Callbacks
+  before_save :set_dates
+
+  private
+    def set_dates
+      if self.submitted.nil?
+        self.submitted = DateTime.now
+      else
+        self.edited = DateTime.now
+      end
+    end
 end
