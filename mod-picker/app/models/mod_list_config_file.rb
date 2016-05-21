f2259c08291cbac6c96abb9f9bbd21a3ce205dcc
Mator <why101@msn.com>
2016-05-20 18:29:45 -0700

    cleaned up ModListConfigFile associations

--- mod-picker/app/models/mod_list_config_file.rb
+++ mod-picker/app/models/mod_list_config_file.rb
@@ -1,7 +1,8 @@
 class ModListConfigFile < EnhancedRecord::Base
   self.primary_keys = :mod_list_id, :config_file_id
 
-  belongs_to :mod_list, :inverse_of => 'config_files'
+  belongs_to :mod_list, :inverse_of => 'mod_list_config_files'
+  belongs_to :config_file, :inverse_of => 'mod_list_config_files'
 
   # validations
   validates :mod_list_id, :config_file_id, presence: true
