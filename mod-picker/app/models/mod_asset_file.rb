5dbfee4359e1deb5e6fe9e939dacf557036475a8
Mator <why101@msn.com>
2016-05-20 17:52:18 -0700

    added mod_asset_files_count counter cache logic to ModAssetFile model

--- mod-picker/app/models/mod_asset_file.rb
+++ mod-picker/app/models/mod_asset_file.rb
@@ -3,4 +3,18 @@ class ModAssetFile < EnhancedRecord::Base
 
   belongs_to :mod, :inverse_of => 'mod_asset_files'
   belongs_to :asset_file, :inverse_of => 'mod_asset_files'
+
+  # Callbacks
+  after_create :increment_counters
+  before_destroy :decrement_counters
+
+  private
+    def increment_counters
+      self.asset_file.update_counter(:mod_asset_files_count, 1)
+    end
+
+    def decrement_counters
+      self.asset_file.update_counter(:mod_asset_files_count, -1)
+    end
+
 end
