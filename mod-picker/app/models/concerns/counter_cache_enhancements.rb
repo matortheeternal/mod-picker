0391a162fac6759d22d8c43565d57268f9c81616
Mator <why101@msn.com>
2016-05-21 15:47:49 -0700

    renamed CounterCacheEnhancements to RecordEnhancements

--- mod-picker/app/models/concerns/counter_cache_enhancements.rb
+++
@@ -1,9 +0,0 @@
-module CounterCacheEnhancements
-  extend ActiveSupport::Concern
-
-  # Custom counter updating code
-  def update_counter(column, offset)
-    self[column] += offset
-    self.save
-  end
-end
