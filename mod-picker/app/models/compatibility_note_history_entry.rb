36c6a3c6a651e963deba5fcf891352bba16f300c
Mator <why101@msn.com>
2016-05-20 18:20:06 -0700

    removed unneeded callbacks and validations from note history entries

--- mod-picker/app/models/compatibility_note_history_entry.rb
+++ mod-picker/app/models/compatibility_note_history_entry.rb
@@ -1,17 +1,6 @@
 class CompatibilityNoteHistoryEntry < EnhancedRecord::Base
-  after_initialize :init
-
   belongs_to :compatibility_note, :inverse_of => 'compatibility_note_history_entries'
   belongs_to :user, :foreign_key => 'submitted_by', :inverse_of => 'compatibility_note_history_entries'
 
   enum status: [ :incompatible, :"partially incompatible", :"compatibility mod", :"compatibility option", :"make custom patch" ]
-
-  # validations
-  validates :compatibility_note_id, :edit_summary, :submitted_by, :submitted, presence: true
-  validates :edit_summary, length: {in: 0..255}
-  validates :text_body, length: {in: 64..16384}
-
-  def init
-    self.submitted  ||= DateTime.now
-  end
 end
--- mod-picker/app/models/install_order_note_history_entry.rb
+++ mod-picker/app/models/install_order_note_history_entry.rb
@@ -1,16 +1,4 @@
 class InstallOrderNoteHistoryEntry < EnhancedRecord::Base
-  after_initialize :init
-
   belongs_to :install_order_note, :inverse_of => 'install_order_note_history_entries'
   belongs_to :user, :foreign_key => 'submitted_by', :inverse_of => 'install_order_note_history_entries'
-
-  # enum status: [ :incompatible, :"partially incompatible", :"compatibility mod", :"compatibility option", :"make custom patch" ]
-
-  # Validations
-  validates :edit_summary, :install_first, :install_second, :submitted, presence: true
-  validates :text_body, length: { in: 64..16384 }
-
-  def init 
-    self.submitted  ||= DateTime.now
-  end
 end
