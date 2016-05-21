eb6ea33fe40526eb74bc3ed13f550f3647ceab7e
Mator <why101@msn.com>
2016-05-20 19:12:20 -0700

    added counter cache logic for user.mod_tags_count

--- mod-picker/app/models/mod_list_tag.rb
+++ mod-picker/app/models/mod_list_tag.rb
@@ -16,10 +16,12 @@ class ModListTag < EnhancedRecord::Base
     def increment_counters
       self.mod_list.update_counter(:tags_count, 1)
       self.tag.update_counter(:mod_lists_count, 1)
+      self.user.update_counter(:mod_list_tags_count, 1)
     end
 
     def decrement_counters
       self.mod_list.update_counter(:tags_count, -1)
       self.tag.update_counter(:mod_lists_count, -1)
+      self.user.update_counter(:mod_list_tags_count, -1)
     end
 end
