a283d6a7584dba8987d6c17bd1e8bf980dda4d01
Mator <why101@msn.com>
2016-05-20 17:54:47 -0700

    refactored/fixed associations between UserReputations and ReputationLinks

--- mod-picker/app/models/reputation_link.rb
+++ mod-picker/app/models/reputation_link.rb
@@ -1,6 +1,6 @@
 class ReputationLink < EnhancedRecord::Base
   self.primary_keys = :from_rep_id, :to_rep_id
 
-  belongs_to :from_rep, :class_name => 'UserReputation', :inverse_of => 'reputation_given'
-  belongs_to :to_rep, :class_name => 'UserReputation', :inverse_of => 'reputation_recieved'
+  belongs_to :from_rep, :class_name => 'UserReputation', :inverse_of => 'from_links'
+  belongs_to :to_rep, :class_name => 'UserReputation', :inverse_of => 'to_links'
 end
--- mod-picker/app/models/user_reputation.rb
+++ mod-picker/app/models/user_reputation.rb
@@ -6,6 +6,8 @@ class UserReputation < EnhancedRecord::Base
   belongs_to :user
 
   scope :user, -> (id) { where(user_id: id) }
+  has_many :from_links, :class_name => 'ReputationLink', :inverse_of => 'from_rep'
+  has_many :to_links, :class_name => 'ReputationLink', :inverse_of => 'to_rep'
   
   # Validations
   validates :user_id, :overall, :offset, presence: true
