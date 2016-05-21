d4a9c7e5b065865ec94c1544fd04d6d157ab4fea
Mator <why101@msn.com>
2016-05-20 17:40:37 -0700

    made ModAssetFiles dependent on their parent AssetFile record AssetFile model now inherits from EnhancedRecord::Base

--- mod-picker/app/models/asset_file.rb
+++ mod-picker/app/models/asset_file.rb
@@ -1,9 +1,10 @@
-class AssetFile < ActiveRecord::Base
+class AssetFile < EnhancedRecord::Base
   belongs_to :game, :inverse_of => 'asset_files'
 
-  has_many :mod_asset_files, :inverse_of => 'asset_file'
+  has_many :mod_asset_files, :inverse_of => 'asset_file', :dependent => :destroy
   has_many :mods, :through => 'mod_asset_files', :inverse_of => 'asset_files'
 
+  # Validations
   validates :filepath, presence: true
 
   def self.as_json(options={})
