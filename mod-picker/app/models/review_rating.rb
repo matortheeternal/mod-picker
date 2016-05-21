878ee9a2d0a9cf77d8d196ac110ba5ff010f28ae
Mator <why101@msn.com>
2016-05-20 19:20:13 -0700

    added counter cache logic for review.ratings_count

--- mod-picker/app/models/review_rating.rb
+++ mod-picker/app/models/review_rating.rb
@@ -1,4 +1,18 @@
 class ReviewRating < EnhancedRecord::Base
   belongs_to :review, :inverse_of => 'review_ratings'
   belongs_to :review_section, :inverse_of => 'review_ratings'
+
+  # Callbacks
+  after_create :increment_counter_caches
+  before_destroy :decrement_counter_caches
+
+  # Private methods
+  private
+    def increment_counter_caches
+      self.review.update_counter(:ratings_count, 1)
+    end
+
+    def decrement_counter_caches
+      self.review.update_counter(:ratings_count, -1)
+    end
 end
