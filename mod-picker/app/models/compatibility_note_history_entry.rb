1c8e203a4a784c611caca195ecfa8a8479165f43
Mator <why101@msn.com>
2016-05-20 18:23:07 -0700

    added history_entries_count counter cache logic to history entry models

--- mod-picker/app/models/compatibility_note_history_entry.rb
+++ mod-picker/app/models/compatibility_note_history_entry.rb
@@ -3,4 +3,17 @@ class CompatibilityNoteHistoryEntry < EnhancedRecord::Base
   belongs_to :user, :foreign_key => 'submitted_by', :inverse_of => 'compatibility_note_history_entries'
 
   enum status: [ :incompatible, :"partially incompatible", :"compatibility mod", :"compatibility option", :"make custom patch" ]
+
+  # Callbacks
+  after_create :increment_counters
+  before_destroy :decrement_counters
+
+  private
+    def increment_counters
+      self.compatibility_note.update_counter(:history_entries_count, 1)
+    end
+
+    def decrement_counters
+      self.compatibility_note.update_counter(:history_entries_count, -1)
+    end
 end
--- mod-picker/app/models/install_order_note_history_entry.rb
+++ mod-picker/app/models/install_order_note_history_entry.rb
@@ -1,4 +1,17 @@
 class InstallOrderNoteHistoryEntry < EnhancedRecord::Base
   belongs_to :install_order_note, :inverse_of => 'install_order_note_history_entries'
   belongs_to :user, :foreign_key => 'submitted_by', :inverse_of => 'install_order_note_history_entries'
+
+  # Callbacks
+  after_create :increment_counters
+  before_destroy :decrement_counters
+
+  private
+    def increment_counters
+      self.install_order_note.update_counter(:history_entries_count, 1)
+    end
+
+    def decrement_counters
+      self.install_order_note.update_counter(:history_entries_count, -1)
+    end
 end
