7730c651d4a9f7060d2d2bbe5050c32e2271cb1c
Mator <why101@msn.com>
2016-05-20 17:38:46 -0700

    added counter cache logic for IncorrectNote.agree_count and IncorrectNote.disagree_count counter cache columns

--- mod-picker/app/models/agreement_mark.rb
+++ mod-picker/app/models/agreement_mark.rb
@@ -4,6 +4,28 @@ class AgreementMark < ActiveRecord::Base
   belongs_to :user, :foreign_key => 'submitted_by', :inverse_of => 'agreement_marks'
   belongs_to :incorrect_note, :inverse_of => 'agreement_marks'
 
+  # Validations
   validates :incorrect_note_id, :submitted_by, presence: true
   validates :agree, inclusion: [true, false]
+
+  # Callbacks
+  after_create :increment_counters
+  before_destroy :decrement_counters
+
+  private
+    def decrement_counters
+      if self.agree
+        self.incorrect_note.update_counter(:agree_count, -1)
+      else
+        self.incorrect_note.update_counter(:disagree_count, -1)
+      end
+    end
+
+    def increment_counters
+      if self.agree
+        self.incorrect_note.update_counter(:agree_count, 1)
+      else
+        self.incorrect_note.update_counter(:disagree_count, 1)
+      end
+    end
 end
