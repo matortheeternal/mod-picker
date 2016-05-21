4e2f7b1a3f0dfc0dbbe2cbfa2d2de20cc5451321
Mator <why101@msn.com>
2016-05-20 18:35:13 -0700

    added update_counters method to Game model

--- mod-picker/app/models/game.rb
+++ mod-picker/app/models/game.rb
@@ -12,5 +12,18 @@ class Game < EnhancedRecord::Base
   has_many :reviews, :inverse_of => 'game'
   has_many :plugins, :inverse_of => 'game'
 
-  # Validations not needed for static data table
+  def update_counters
+    self.mods_count = Mod.where(game_id: self.id).count
+    self.nexus_infos_count = NexusInfo.where(game_id: self.id).count
+    self.lover_infos_count = LoverInfo.where(game_id: self.id).count
+    self.mod_lists_count = ModList.where(game_id: self.id).count
+    self.config_files_count = ConfigFile.where(game_id: self.id).count
+    self.asset_files_count = AssetFile.where(game_id: self.id).count
+    self.compatibility_notes_count = CompatibilityNote.where(game_id: self.id).count
+    self.incorrect_notes_count = IncorrectNote.where(game_id: self.id).count
+    self.install_order_notes_count = InstallOrderNote.where(game_id: self.id).count
+    self.load_order_notes_count = LoadOrderNote.where(game_id: self.id).count
+    self.reviews_count = Review.where(game_id: self.id).count
+    self.plugins_count = Plugin.where(game_id: self.id).count
+  end
 end
