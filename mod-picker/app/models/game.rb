2eee17690d634d70f9df8f81eac4185d94e60a71
Mator <why101@msn.com>
2016-05-20 20:32:47 -0700

    cleaned up game.update_lazy_counters to use associations instead of SQL queries

--- mod-picker/app/models/game.rb
+++ mod-picker/app/models/game.rb
@@ -14,17 +14,18 @@ class Game < ActiveRecord::Base
   has_many :plugins, :inverse_of => 'game'
 
   def update_lazy_counters
-    self.mods_count = Mod.where(game_id: self.id).count
-    self.nexus_infos_count = NexusInfo.where(game_id: self.id).count
-    self.lover_infos_count = LoverInfo.where(game_id: self.id).count
-    self.mod_lists_count = ModList.where(game_id: self.id).count
-    self.config_files_count = ConfigFile.where(game_id: self.id).count
-    self.asset_files_count = AssetFile.where(game_id: self.id).count
-    self.compatibility_notes_count = CompatibilityNote.where(game_id: self.id).count
-    self.incorrect_notes_count = IncorrectNote.where(game_id: self.id).count
-    self.install_order_notes_count = InstallOrderNote.where(game_id: self.id).count
-    self.load_order_notes_count = LoadOrderNote.where(game_id: self.id).count
-    self.reviews_count = Review.where(game_id: self.id).count
-    self.plugins_count = Plugin.where(game_id: self.id).count
+    self.mods_count = self.mods.count
+    self.nexus_infos_count = self.nexus_infos.count
+    self.lover_infos_count = self.lover_infos.count
+    self.workshop_infos_count = self.workshop_infos.count
+    self.mod_lists_count = self.mod_lists.count
+    self.config_files_count = self.config_files.count
+    self.asset_files_count = self.asset_files.count
+    self.compatibility_notes_count = self.compatibility_notes.count
+    #self.incorrect_notes_count = self.incorrect_notes.count
+    self.install_order_notes_count = self.install_order_notes.count
+    self.load_order_notes_count = self.load_order_notes.count
+    self.reviews_count = self.reviews.count
+    self.plugins_count = self.plugins.count
   end
 end
