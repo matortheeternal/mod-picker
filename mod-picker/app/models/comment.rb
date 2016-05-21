72cd4b6f3715a859360c3b9ce059dff3aa205d76
Mator <why101@msn.com>
2016-05-20 17:49:02 -0700

    cleaned up comment validations and callbacks added set_dates callback

--- mod-picker/app/models/comment.rb
+++ mod-picker/app/models/comment.rb
@@ -1,58 +1,40 @@
 class Comment < EnhancedRecord::Base
   include Filterable
 
-  after_initialize :init
-  
-  after_create :increment_counter_caches
-  before_destroy :decrement_counter_caches
-
   scope :type, -> (type) { where(commentable_type: type) }
   scope :target, -> (id) { where(commentable_id: id) }
   scope :by, -> (id) { where(submitted_by: id) }
 
   belongs_to :user, :foreign_key => 'submitted_by', :inverse_of => 'comments'
   belongs_to :commentable, :polymorphic => true
-  
+
   has_one :base_report, :as => 'reportable'
 
   # parent/child comment association
   belongs_to :parent, :class_name => 'Comment', :foreign_key => 'parent_comment', :inverse_of => 'children'
   has_many :children, :class_name => 'Comment', :foreign_key => 'parent_comment', :inverse_of => 'parent'
 
-  # validation
+  # Validations
   validates :submitted_by, :commentable_type, :commentable_id, presence: true
   validates :hidden, inclusion: [true, false]
   validates :commentable_type, inclusion: ["User", "ModList"]
+  validates :text_body, length: {in: 1..8192}
 
-  validate :validate_text_body_length
-
-  def init
-    self.submitted  ||= Date.today
-    self.hidden     ||= false
-  end
-
-  # validate text_body lengths, depending on commentable_type
-  # TODO: refactor comment model method so it short circuits when blank
-  def validate_text_body_length
-    case self.commentable_type
-      when "User"
-        if text_body.blank?
-          errors.add(:text_body, "body can't be empty")
-        elsif !self.text_body.length.between?(1, 16384)
-          errors.add(:text_body, "length must be less than 16384 characters")
-        end
-      when "ModList"
-        if text_body.blank?
-          errors.add(:text_body, "body can't be empty")
-        elsif !self.text_body.length.between?(1, 4096)
-          errors.add(:text_body, "length must be less than 4096 characters")
-        end
-    end
-  end
+  # Callbacks
+  before_save :set_dates
+  after_create :increment_counter_caches
+  before_destroy :decrement_counter_caches
 
   # Private methods
   private
-    # counter caches
+    def set_dates
+      if self.submitted.nil?
+        self.submitted = DateTime.now
+      else
+        self.edited = DateTime.now
+      end
+    end
+
     def increment_counter_caches
       self.commentable.update_counter(:comments_count, 1)
     end
