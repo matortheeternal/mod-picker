2f39ea147e05330b8bdc7f4c1c435deceb8042bb
Mator <why101@msn.com>
2016-05-21 13:22:00 -0700

    added updateContribution method to contributionService

--- mod-picker/app/assets/javascripts/angularAssets/BackendAPI/contributionService.js
+++ mod-picker/app/assets/javascripts/angularAssets/BackendAPI/contributionService.js
@@ -31,4 +31,12 @@ app.service('contributionService', function (backend, $q) {
         });
         return action.promise;
     };
+
+    this.updateContribution = function(type, contribution) {
+        var action = $q.defer();
+        backend.update('/' + type + '/' + contribution.id, contribution).then(function (data) {
+            action.resolve(data);
+        });
+        return action.promise;
+    };
 });
