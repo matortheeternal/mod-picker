729b46672d9cb4ffb9a5b14a67c3b9dc5af7d4a7
Mator <why101@msn.com>
2016-05-21 23:34:27 -0700

    made slider have two possible data bindings (directly to an object or inside of a filters object) so we can do our ng-repeat sliders and still have working data binding sliders also will delete/not save data if they are at both their default positions

--- mod-picker/app/assets/javascripts/angularAssets/Directives/slider.js
+++ mod-picker/app/assets/javascripts/angularAssets/Directives/slider.js
@@ -6,6 +6,7 @@ app.directive('slider', function () {
         templateUrl: '/resources/directives/slider.html',
         controller: 'sliderController',
         scope: {
+            filters: '=?',
             data: '=',
             type: '@',
             attr: '@'
@@ -17,19 +18,49 @@ app.controller('sliderController', function ($scope, sliderOptionsFactory, $time
     if (typeof sliderOptionsFactory[$scope.type]=== "function") {
         $scope.slider = sliderOptionsFactory[$scope.type]($scope.attr);
     }
-    $scope.data = {};
     $scope.rawData = {
         min: $scope.slider.min || 0,
         max: $scope.slider.max
     };
 
-    $scope.$watch('rawData', function(rawData) {
-        if ($scope.slider.options.stepsArray) {
-            $scope.data.min = $scope.slider.options.stepsArray[rawData.min];
-            $scope.data.max = $scope.slider.options.stepsArray[rawData.max];
+    $scope.deleteData = function() {
+        if ($scope.filters) {
+            delete $scope.filters[$scope.data];
         } else {
-            $scope.data.min = rawData.min;
-            $scope.data.max = rawData.max;
+            delete $scope.data;
+        }
+    };
+
+    $scope.setData = function(min, max) {
+        if ($scope.filters) {
+            $scope.filters[$scope.data] = {
+                min: min,
+                max: max
+            };
+        } else {
+            $scope.data = {
+                min: min,
+                max: max
+            };
+        }
+    };
+
+    $scope.$watch('rawData', function(rawData) {
+        var stepsArray = $scope.slider.options.stepsArray;
+        var atMin = rawData.min == 0;
+        var atMax = rawData.max == parseInt($scope.attr) ||
+            stepsArray && rawData.max == stepsArray.length - 1;
+        // delete data and return if we're at default values
+        if (atMin && atMax) {
+            $scope.deleteData();
+            return;
+        }
+
+        // set data using steps array if available, else use raw data
+        if (stepsArray) {
+            $scope.setData(stepsArray[rawData.min], stepsArray[rawData.max]);
+        } else {
+            $scope.setData(rawData.min, rawData.max);
         }
     }, true);
 
