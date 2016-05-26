0fd2a8a48896edc388f97062ab4a9f4b508aac72
Mator <why101@msn.com>
2016-05-26 13:16:38 -0700

    corrected routes for updating tags

--- mod-picker/config/routes.rb
+++ mod-picker/config/routes.rb
@@ -28,8 +28,8 @@ Rails.application.routes.draw do
     # tags
     match '/tags', to: 'tags#index', via: 'post'
     match '/tags/:id', to: 'tags#destroy', via: 'delete'
-    match '/mods/:id/tag', to: 'mods#tag', via: 'post'
-    match '/mod_lists/:id/tag', to: 'mod_lists#tag', via: 'post'
+    match '/mods/:id/tags', to: 'mods#update_tags', via: 'put'
+    match '/mod_lists/:id/tags', to: 'mod_lists#update_tags', via: 'put'
 
     # mods
     resources :mods, only: [:show, :update, :destroy]
