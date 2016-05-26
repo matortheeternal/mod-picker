9f37bcc3bd9aaa7b4397f9dad778da6b9fef2038
TheSiriusAdam <thesiriusadam@gmail.com>
2016-05-26 12:19:26 -0500

    refactored reviewSectionService to be similar to the userTitleService and helpful marks services (associateSections)

--- mod-picker/app/assets/javascripts/angularAssets/BackendAPI/reviewSectionService.js
+++ mod-picker/app/assets/javascripts/angularAssets/BackendAPI/reviewSectionService.js
@@ -1,4 +1,6 @@
 app.service('reviewSectionService', function (backend, $q) {
+    var service = this;
+
     this.retrieveReviewSections = function () {
         var reviewSections = $q.defer();
 
@@ -9,15 +11,32 @@ app.service('reviewSectionService', function (backend, $q) {
         return reviewSections.promise;
     };
 
-    this.getSectionsForCategory = function(reviewSections, category) {
-        return reviewSections.filter(function(section) {
-            return (section.category_id == category.id) || (section.category_id == category.parent_id);
+    var allReviewSections = this.retrieveReviewSections();
+
+    this.getSectionsForCategory = function(category) {
+        var output = $q.defer();
+        allReviewSections.then(function(sections) {
+          output.resolve(sections.filter(function(section, category) {
+              return (section.category_id === category.id) || (section.category_id == category.parent_id);
+          }));
+        });
+        return output.promise;
+    };
+
+    this.getSectionById = function(sections, id) {
+        return sections.find(function(section) {
+            return (section.id === id);
         });
     };
 
-    this.getSectionById = function(reviewSections, id) {
-        return reviewSections.find(function(section) {
-            return (section.id == id);
+    this.associateReviewSections = function(reviews) {
+        allReviewSections.then(function(sections) {
+          reviews.forEach(function(review) {
+            //set the category using the categoryId
+            review.review_ratings.forEach(function(rating) {
+                rating.section = service.getSectionById(sections, rating.review_section_id);
+            });
+          });
         });
     };
 });
