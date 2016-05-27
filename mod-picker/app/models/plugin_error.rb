67401c79752e8f1fdb04f2c487f21df26404793c
Mator <why101@msn.com>
2016-05-27 16:38:46 -0700

    removed plugin_id presence validations

--- mod-picker/app/models/plugin_error.rb
+++ mod-picker/app/models/plugin_error.rb
@@ -2,7 +2,7 @@ class PluginError < ActiveRecord::Base
   belongs_to :plugin, :inverse_of => 'errors'
 
   # Validations
-  validates :plugin_id, :signature, :form_id, :group, presence: true
+  validates :signature, :form_id, :group, presence: true
   validates :signature, length: {maximum: 4}
 
   belongs_to :plugin, :inverse_of => 'plugin_errors'
