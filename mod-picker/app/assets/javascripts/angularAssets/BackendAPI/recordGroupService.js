04347ee8699176049dc8706a7c5cbe8287ddc355
TheSiriusAdam <thesiriusadam@gmail.com>
2016-05-30 22:34:12 -0500

    fixed typo in record group association

--- mod-picker/app/assets/javascripts/angularAssets/BackendAPI/recordGroupService.js
+++ mod-picker/app/assets/javascripts/angularAssets/BackendAPI/recordGroupService.js
@@ -1,5 +1,5 @@
 app.service('recordGroupService', function (backend, $q) {
-    var service = this;
+    var thisService = this;
     this.retrieveRecordGroups = function (game_id) {
         return backend.retrieve('/record_groups', {game_id: game_id});
     };
@@ -11,11 +11,11 @@ app.service('recordGroupService', function (backend, $q) {
     };
 
     this.associateGroups = function(plugins, game_id) {
-        this.retrieveRecordGroups(game_id).then(function(groups) {
+        thisService.retrieveRecordGroups(game_id).then(function(allGroups) {
             plugins.forEach(function(plugin) {
                 if (plugin.plugin_record_groups) {
                     plugin.plugin_record_groups.forEach(function(group) {
-                        var record_group = service.getGroupFromSignature(groups, plugin.sig);
+                        var record_group = thisService.getGroupFromSignature(allGroups, group.sig);
                         plugin.name = record_group.name;
                         plugin.child_group = record_group.child_group;
                     });
