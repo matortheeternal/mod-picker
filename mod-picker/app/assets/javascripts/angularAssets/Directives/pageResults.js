1d852cd8f737357ca1149120f1f98798a6279083
Mator <why101@msn.com>
2016-06-02 12:30:48 -0700

    fixed up pageResults directive logic - page range was only set once through ng-init, now using a watch - background-highlight color now applied to page items, not the page-results-container

--- mod-picker/app/assets/javascripts/angularAssets/Directives/pageResults.js
+++ mod-picker/app/assets/javascripts/angularAssets/Directives/pageResults.js
@@ -9,17 +9,20 @@ app.directive('pageResults', function () {
 
 app.controller('pageResultsController', function($scope) {
     var pageDistance = 3;
+    $scope.range = [];
 
     $scope.pageRange = function() {
         if (!$scope.pages.max) {
-            return [];
+            return;
         }
-        var a = [];
+        $scope.range = [];
         for (var i = 1; i <= $scope.pages.max; i++) {
             if ((i === 1) || (i == $scope.pages.max) ||
                 (Math.abs($scope.pages.current - i) < pageDistance))
-                a.push(i);
+                $scope.range.push(i);
         }
-        return a;
-    }
+    };
+
+    $scope.$watch('pages.max', $scope.pageRange);
+    $scope.$watch('pages.current', $scope.pageRange);
 });
