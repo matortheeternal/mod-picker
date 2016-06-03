f2f7b7892eb0ca1c3adf5b2f28af0616c3fbef87
TheSiriusAdam <thesiriusadam@gmail.com>
2016-06-03 12:16:04 -0500

    temporary commenting out of modlist splitting logic because current_user.mod_lists isn't being included

--- mod-picker/app/assets/javascripts/angularAssets/BackendAPI/userService.js
+++ mod-picker/app/assets/javascripts/angularAssets/BackendAPI/userService.js
@@ -7,13 +7,14 @@ app.service('userService', function (backend, $q, userSettingsService, userTitle
     this.retrieveCurrentUser = function () {
         var output = $q.defer();
         backend.retrieve('/current_user').then(function (user) {
-            //moving collections into a separate array
-            user.collections = [];
-            for (var i = user.mod_lists.length - 1; i >= 0; i--) {
-                if (user.mod_lists[i].is_collection) {
-                    user.collections.push(user.mod_lists.splice(i, 1)[0]);
-                }
-            }
+            //TODO:uncomment this when mod_lists are served correctly
+            // //moving collections into a separate array
+            // user.collections = [];
+            // for (var i = user.mod_lists.length - 1; i >= 0; i--) {
+            //     if (user.mod_lists[i].is_collection) {
+            //         user.collections.push(user.mod_lists.splice(i, 1)[0]);
+            //     }
+            // }
 
             //get user title if it's not custom
             if (!user.title) {
