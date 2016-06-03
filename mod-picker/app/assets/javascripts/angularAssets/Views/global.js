cb14ca412f9bd5efe42b81db6a8734fcd73384af
Mator <why101@msn.com>
2016-06-03 12:47:25 -0700

    added a listener in the global controller to reload the current user from the backend

--- mod-picker/app/assets/javascripts/angularAssets/Views/global.js
+++ mod-picker/app/assets/javascripts/angularAssets/Views/global.js
@@ -21,4 +21,10 @@ app.controller('globalController', function($scope, $rootScope, userService, the
     $rootScope.$on('themeChanged', function (event, newTheme) {
         $scope.currentTheme = newTheme;
     });
+
+    $scope.$on('reloadCurrentUser', function() {
+        userService.retrieveCurrentUser().then(function (user) {
+            $scope.currentUser = user;
+        });
+    });
 });
