d00a95f02c22bd727978d90a471f3a3198e72db1
Mator <why101@msn.com>
2016-06-02 19:43:45 -0700

    added permissions at global context

--- mod-picker/app/assets/javascripts/angularAssets/Views/global.js
+++ mod-picker/app/assets/javascripts/angularAssets/Views/global.js
@@ -1,8 +1,20 @@
 app.controller('globalController', function($scope, $rootScope, userService, themesService, gameService) {
+    $scope.permissions = {
+        isAdmin: false,
+        isModerator: false,
+        canSubmitMod: false
+    };
 
-    userService.retrieveCurrentUser().then(function(data) {
-        $scope.currentUser = data;
-        themesService.changeTheme(data.settings.theme);
+    userService.retrieveCurrentUser().then(function(user) {
+        $scope.currentUser = user;
+        themesService.changeTheme(user.settings.theme);
+        // figure out user permissions
+        var permissions = $scope.permissions;
+        permissions.isAdmin = user.role === 'admin';
+        permissions.isModerator = user.role === 'moderator';
+        // TODO: Remove this when beta is over
+        permissions.canSubmitMod = true;
+        //permissions.canSubmitMod = permissions.isAdmin || permissions.isModerator || user.reputation.overall > 160;
     });
 
     gameService.retrieveGames().then(function(data) {
