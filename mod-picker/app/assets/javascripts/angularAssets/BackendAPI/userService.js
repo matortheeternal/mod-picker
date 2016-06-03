f765897a48c404a75bb425b850147a73f81e32a6
TheSiriusAdam <thesiriusadam@gmail.com>
2016-06-03 09:56:26 -0500

    title setting and  collection and modlist splitting are done in the userService now

--- mod-picker/app/assets/javascripts/angularAssets/BackendAPI/userService.js
+++ mod-picker/app/assets/javascripts/angularAssets/BackendAPI/userService.js
@@ -1,17 +1,24 @@
-app.service('userService', function (backend, $q, userSettingsService) {
+app.service('userService', function (backend, $q, userSettingsService, userTitleService) {
     this.retrieveUser = function (userId) {
-        return backend.retrieve('/users/' + userId);
-    };
+        var output = $q.defer();
+        backend.retrieve('/users/' + userId).then(function(user) {
+            //moving collections into a separate array
+            user.collections = [];
+            for (var i = user.mod_lists.length - 1; i >= 0; i--) {
+                if (user.mod_lists[i].is_collection) {
+                    user.collections.push(user.mod_lists.splice(i, 1)[0]);
+                }
+            }
 
-    //TODO: there has to be a better way to do this right? -Sirius
-    this.retrieveThisUser = function () {
-        var user = $q.defer();
-    	userSettingsService.retrieveUserSettings().then(function (user_settings) {
-	    	backend.retrieve('/users/' + user_settings.user_id).then(function (data) {
-	            user.resolve(data);
-	        });
+            //get user title if it's not custom
+            if (!user.title) {
+                userTitleService.getUserTitle(user.reputation.overall).then(function(title) {
+                    user.title = title;
+                });
+            }
+
+            output.resolve(user);
         });
-        return user.promise;
+        return output.promise;
     };
-
 });
