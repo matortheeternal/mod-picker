55120cf19ee0aea633c97b0fb9e1c576e8e10639
Mator <why101@msn.com>
2016-06-04 15:05:56 -0700

    fixed bugs with home_controller after database refactor

--- mod-picker/app/controllers/home_controller.rb
+++ mod-picker/app/controllers/home_controller.rb
@@ -35,9 +35,9 @@ class HomeController < ApplicationController
 
     # we include associated data we know we'll need because it increases the speed of the query
     mod_lists = ModList.where("game_id = ? AND status = 3 AND hidden = false", params[:game]).includes(:user => :reputation).order(:edited => :DESC).limit(4)
-    mods = Mod.where("game_id = ? AND hidden = false", params[:game]).order(:id => :ASC).limit(4)
+    mods = Mod.game(params[:game]).where("hidden = false").order(:id => :ASC).limit(4)
     reviews = Review.where("game_id = ? AND hidden = false", params[:game]).includes(:mod, :user => :reputation).order(:submitted => :DESC).limit(4)
-    corrections = Correction.where("game_id = ? AND hidden = false", params[:game]).includes(:user => :reputation).order(:created_at => :DESC).limit(4)
+    corrections = Correction.where("game_id = ? AND hidden = false", params[:game]).includes(:user => :reputation).order(:submitted => :DESC).limit(4)
     compatibility_notes = CompatibilityNote.where("game_id = ? AND hidden = false", params[:game]).includes(:first_mod, :second_mod, :user => :reputation).order(:submitted => :DESC).limit(4)
     install_order_notes = InstallOrderNote.where("game_id = ? AND hidden = false", params[:game]).includes(:first_mod, :second_mod, :user => :reputation).order(:submitted => :DESC).limit(4)
     load_order_notes = LoadOrderNote.where("game_id = ? AND hidden = false", params[:game]).includes(:first_plugin, :second_plugin, :user => :reputation).order(:submitted => :DESC).limit(4)
@@ -47,9 +47,9 @@ class HomeController < ApplicationController
         recent: {
             mod_lists: mod_lists.as_json,
             mods: mods.as_json({
-                :only => [:id, :name, :primary_category_id, :secondary_category_id, :mod_stars_count, :status, :reputation, :average_rating, :released, :reviews_count],
-                :include => {:authors => {:only => [:username]}},
-                :methods => [:image, :mod_lists_count]
+                :only => [:id, :name, :primary_category_id, :secondary_category_id, :mod_stars_count, :status, :reputation, :average_rating, :released, :reviews_count, :mod_lists_count],
+                :include => {:author_users => {:only => [:username]}},
+                :methods => [:image]
             }),
             reviews: reviews.as_json({
                 :only => [:id, :submitted, :edited, :corrections_count, :text_body],
