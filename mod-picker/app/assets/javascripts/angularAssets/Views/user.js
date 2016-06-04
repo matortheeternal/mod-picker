256c8382439e8c3427bc95c01255a057edc49175
TheSiriusAdam <thesiriusadam@gmail.com>
2016-06-04 03:23:47 -0500

    user page now using ui router tabs

--- mod-picker/app/assets/javascripts/angularAssets/Views/user.js
+++ mod-picker/app/assets/javascripts/angularAssets/Views/user.js
@@ -2,40 +2,49 @@ app.config(['$stateProvider', function ($stateProvider) {
     $stateProvider.state('base.user', {
             templateUrl: '/resources/partials/showUser/user.html',
             controller: 'userController',
-            url: '/profile/:userId'
-        }
-    );
+            url: '/profile/:userId',
+            redirectTo: 'base.user.Social',
+            resolve: {
+                user: function(userService, $stateParams) {
+                    return userService.retrieveUser($stateParams.userId);
+                }
+            }
+        }).state('base.user.Social', {
+            templateUrl: '/resources/partials/showUser/social.html',
+        }).state('base.user.Mod Lists', {
+            templateUrl: '/resources/partials/showUser/lists.html',
+        }).state('base.user.Mods', {
+            templateUrl: '/resources/partials/showUser/mods.html',
+        }).state('base.user.Contributions', {
+            templateUrl: '/resources/partials/showUser/contributions.html',
+        });
 }]);
 
-app.controller('userController', function ($scope, $q, $stateParams, userService) {
-    userService.retrieveUser($stateParams.userId).then(function (user) {
-        $scope.user = user;
-        $scope.roleClass = "user-role-" + $scope.user.role;
-        //formatting the role displayed on the site
-        switch($scope.user.role) {
-            case "admin":
-                $scope.user.role = "Administrator";
-                break;
-            case "moderator":
-                $scope.user.role = "Moderator";
-                break;
-            case "author":
-                $scope.user.role = "Mod Author";
-                break;
-            default:
-                //shows nothing if they have no notable role
-                $scope.user.role = "";
-                break;
-        }
-    });
+app.controller('userController', function ($scope, user) {
+    $scope.user = user;
+    $scope.roleClass = "user-role-" + $scope.user.role;
+    //formatting the role displayed on the site
+    switch($scope.user.role) {
+        case "admin":
+            $scope.user.role = "Administrator";
+            break;
+        case "moderator":
+            $scope.user.role = "Moderator";
+            break;
+        case "author":
+            $scope.user.role = "Mod Author";
+            break;
+        default:
+            //shows nothing if they have no notable role
+            $scope.user.role = "";
+            break;
+    }
 
     //of the tab data
     $scope.tabs = [
-        { name: 'Social', url: '/resources/partials/showUser/social.html' },
-        { name: 'Mod Lists', url: '/resources/partials/showUser/lists.html' },
-        { name: 'Mods', url: '/resources/partials/showUser/mods.html' },
-        { name: 'Contributions', url: '/resources/partials/showUser/contributions.html' }
+        { name: 'Social'},
+        { name: 'Mod Lists'},
+        { name: 'Mods'},
+        { name: 'Contributions'}
     ];
-
-    $scope.currentTab = $scope.tabs[0];
 });
