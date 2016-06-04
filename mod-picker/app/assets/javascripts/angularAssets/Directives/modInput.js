5cc78652c1eaa20c6d930e63570994e94f99dee0
Mator <why101@msn.com>
2016-06-04 13:27:57 -0700

    added disabled scope attribute to modInput directive

--- mod-picker/app/assets/javascripts/angularAssets/Directives/modInput.js
+++ mod-picker/app/assets/javascripts/angularAssets/Directives/modInput.js
@@ -8,7 +8,8 @@ app.directive('modInput', function () {
             searchText: '=?',
             onChange: '=?',
             excludedId: '=?',
-            searchPlugins: '=?'
+            searchPlugins: '=?',
+            disabled: '=?'
         }
     }
 });
