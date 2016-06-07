a8e25504fff896b9dbb11d3b503ab84364bd97f8
Mator <why101@msn.com>
2016-06-07 00:10:46 -0700

    corrected some settings for the production environment until I can figure out how to get uglifier to work properly

--- mod-picker/config/environments/production.rb
+++ mod-picker/config/environments/production.rb
@@ -22,10 +22,10 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.serve_static_files = true
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = ConditionalUglifier.new
+  # config.assets.js_compressor = ConditionalUglifier.new
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
