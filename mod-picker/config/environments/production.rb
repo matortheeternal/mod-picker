a16c5d92e888a84726542abb4d36a6775794c993
Mator <why101@msn.com>
2016-06-07 12:12:48 -0700

    enabled force_ssl for production environment

--- mod-picker/config/environments/production.rb
+++ mod-picker/config/environments/production.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
