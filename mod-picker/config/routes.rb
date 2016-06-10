f8d2c77e008509152943b5815f19cb3974b688e6
Mator <why101@msn.com>
2016-06-10 14:25:41 -0700

    refactor http verbs on routes to be arrays of symbols instead of literal strings refactored index routes to be /resource/index instead of /resource so we don't conflict with the create route refactored route organization added routes for history and corrections to notes

--- mod-picker/config/routes.rb
+++ mod-picker/config/routes.rb
@@ -5,20 +5,15 @@ Rails.application.routes.draw do
   # require authentication before allowing user to access any resources
   authenticate :user do
     resources :comments
-    resources :install_order_notes
-    resources :load_order_notes
-    resources :compatibility_notes
-    resources :corrections
-    resources :reviews
     resources :mod_authors
     resources :user_bios
     resources :mod_asset_files
 
     # users and user settings
     resources :users, only: [:index, :show, :update, :destroy]
-    match '/current_user', to: 'users#current', via: 'get'
+    match '/current_user', to: 'users#current', via: [:get]
     resources :user_settings, only: [:index, :update]
-    match '/link_account', to: 'users#link_account', via: 'get'
+    match '/link_account', to: 'users#link_account', via: [:get]
 
     # scraping
     resources :nexus_infos, only: [:show, :destroy]
@@ -26,21 +21,20 @@ Rails.application.routes.draw do
     resources :lover_infos, only: [:show, :destroy]
 
     # tags
-    match '/tags', to: 'tags#index', via: 'post'
-    match '/tags/:id', to: 'tags#destroy', via: 'delete'
+    match '/tags', to: 'tags#index', via: [:post]
+    match '/tags/:id', to: 'tags#destroy', via: [:delete]
     match '/mods/:id/tags', to: 'mods#update_tags', via: [:patch, :put]
     match '/mod_lists/:id/tags', to: 'mod_lists#update_tags', via: [:patch, :put]
 
     # mods
-    resources :mods, only: [:show, :update, :destroy]
-    match '/mods/submit', to: 'mods#create', via: 'post'
-    match '/mods', to: 'mods#index', via: 'post'
-    match '/mods/search', to: 'mods#search', via: 'post'
+    resources :mods, only: [:show, :create, :update, :destroy]
+    match '/mods/index', to: 'mods#index', via: [:get, :post]
+    match '/mods/search', to: 'mods#search', via: [:post]
 
     # plugins
     resources :plugins, only: [:show, :destroy]
-    match '/plugins', to: 'plugins#index', via: 'post'
-    match '/plugins/search', to: 'plugins#search', via: 'post'
+    match '/plugins', to: 'plugins#index', via: [:post]
+    match '/plugins/search', to: 'plugins#search', via: [:post]
 
     # content associated with mods
     match '/mods/:id/corrections', to: 'mods#corrections', via: [:get, :post]
@@ -50,40 +44,60 @@ Rails.application.routes.draw do
     match '/mods/:id/load_order_notes', to: 'mods#load_order_notes', via: [:get, :post]
     match '/mods/:id/analysis', to: 'mods#analysis', via: [:get, :post]
 
+    # reviews
+    resources :reviews, only: [:show, :create, :update, :destroy]
+    match '/reviews/index', to: 'reviews#index', via: [:get, :post]
+    match '/reviews/:id/approve', to: 'reviews#approve', via: [:post]
+    match '/reviews/:id/hide', to: 'reviews#hide', via: [:post]
+
+    # compatibility notes
+    resources :compatibility_notes, only: [:show, :create, :update, :destroy]
+    match '/compatibility_notes/index', to: 'compatibility_notes#index', via: [:get, :post]
+    match '/compatibility_notes/:id/approve', to: 'compatibility_notes#approve', via: [:post]
+    match '/compatibility_notes/:id/hide', to: 'compatibility_notes#hide', via: [:post]
+    match '/compatibility_notes/:id/corrections', to: 'compatibility_notes#corrections', via: [:get]
+    match '/compatibility_notes/:id/history', to: 'compatibility_notes#history', via: [:get]
+
+    # install order notes
+    resources :install_order_notes, only: [:show, :create, :update, :destroy]
+    match '/install_order_notes/index', to: 'install_order_notes#index', via: [:get, :post]
+    match '/install_order_notes/:id/hide', to: 'install_order_notes#hide', via: [:post]
+    match '/install_order_notes/:id/corrections', to: 'install_order_notes#corrections', via: [:get]
+    match '/install_order_notes/:id/history', to: 'install_order_notes#history', via: [:get]
+
+    # load order notes
+    resources :load_order_notes, only: [:show, :create, :update, :destroy]
+    match '/load_order_notes/index', to: 'load_order_notes#index', via: [:get, :post]
+    match '/load_order_notes/:id/hide', to: 'load_order_notes#hide', via: [:post]
+    match '/install_order_notes/:id/approve', to: 'install_order_notes#approve', via: [:post]
+    match '/load_order_notes/:id/corrections', to: 'load_order_notes#corrections', via: [:get]
+    match '/load_order_notes/:id/history', to: 'load_order_notes#history', via: [:get]
+
+    # corrections
+    resources :corrections, only: [:show, :create, :update, :destroy]
+    match '/corrections/index', to: 'corrections#index', via: [:get, :post]
+    match '/corrections/:id/hide', to: 'corrections#hide', via: [:post]
+    match '/load_order_notes/:id/approve', to: 'load_order_notes#approve', via: [:post]
+    match '/corrections/:id/agreement', to: 'corrections#agreement', via: [:post]
+
     # helpful marks
-    match '/reviews/:id/helpful', to: 'reviews#helpful', via: 'post'
-    match '/compatibility_notes/:id/helpful', to: 'compatibility_notes#helpful', via: 'post'
-    match '/install_order_notes/:id/helpful', to: 'install_order_notes#helpful', via: 'post'
-    match '/load_order_notes/:id/helpful', to: 'load_order_notes#helpful', via: 'post'
-
-    # hiding content
-    match '/corrections/:id/hide', to: 'corrections#hide', via: 'post'
-    match '/reviews/:id/hide', to: 'reviews#hide', via: 'post'
-    match '/compatibility_notes/:id/hide', to: 'compatibility_notes#hide', via: 'post'
-    match '/install_order_notes/:id/hide', to: 'install_order_notes#hide', via: 'post'
-    match '/load_order_notes/:id/hide', to: 'load_order_notes#hide', via: 'post'
-
-    # approving content
-    match '/reviews/:id/approve', to: 'reviews#approve', via: 'post'
-    match '/compatibility_notes/:id/approve', to: 'compatibility_notes#approve', via: 'post'
-    match '/install_order_notes/:id/approve', to: 'install_order_notes#approve', via: 'post'
-    match '/load_order_notes/:id/approve', to: 'load_order_notes#approve', via: 'post'
-
-    # agreement marks
-    match '/corrections/:id/agreement', to: 'corrections#agreement', via: 'post'
+    match '/reviews/:id/helpful', to: 'reviews#helpful', via: [:post]
+    match '/compatibility_notes/:id/helpful', to: 'compatibility_notes#helpful', via: [:post]
+    match '/install_order_notes/:id/helpful', to: 'install_order_notes#helpful', via: [:post]
+    match '/load_order_notes/:id/helpful', to: 'load_order_notes#helpful', via: [:post]
 
     # mod lists
     resources :mod_lists, only: [:index, :show, :create, :update, :destroy]
-    match '/active_mod_list', to: 'mod_lists#active', via: 'get'
+    match '/active_mod_list', to: 'mod_lists#active', via: [:get]
 
     # mod and mod list stars
-    match '/mod_lists/:id/star', to: 'mod_lists#create_star', via: 'post'
-    match '/mod_lists/:id/star', to: 'mod_lists#destroy_star', via: 'delete'
-    match '/mods/:id/star', to: 'mods#create_star', via: 'post'
-    match '/mods/:id/star', to: 'mods#destroy_star', via: 'delete'
+    match '/mod_lists/:id/star', to: 'mod_lists#create_star', via: [:post]
+    match '/mod_lists/:id/star', to: 'mod_lists#destroy_star', via: [:delete]
+    match '/mods/:id/star', to: 'mods#create_star', via: [:post]
+    match '/mods/:id/star', to: 'mods#destroy_star', via: [:delete]
 
     # avatars
-    match '/avatar', to: 'avatars#create', via: 'post'
+    match '/avatar', to: 'avatars#create', via: [:post]
 
     # static data
     resources :categories, only: [:index]
@@ -95,15 +109,15 @@ Rails.application.routes.draw do
     resources :user_titles, only: [:index]
 
     # home page
-    match '/skyrim', to: 'home#skyrim', via: 'get'
-    match '/fallout4', to: 'home#fallout4', via: 'get'
+    match '/skyrim', to: 'home#skyrim', via: [:get]
+    match '/fallout4', to: 'home#fallout4', via: [:get]
   end
 
   # welcome page
   resources :welcome, only: [:index]
 
   # contact us and subscribe
-  match '/contacts', to: 'contacts#new', via: 'get'
+  match '/contacts', to: 'contacts#new', via: [:get]
   resources :contacts, only: [:new, :create]
 
   # You can have the root of your site routed with "root"
