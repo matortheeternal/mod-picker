c56059358d63d05ea96d0e07a257fb1e7ace839e
Mator <why101@msn.com>
2016-06-19 14:38:29 -0700

    added separate decays for success and error messages

--- mod-picker/app/assets/javascripts/angularAssets/Directives/userSettings/messages.js
+++ mod-picker/app/assets/javascripts/angularAssets/Directives/userSettings/messages.js
@@ -4,22 +4,28 @@ app.directive('messages', function () {
         templateUrl: '/resources/directives/userSettings/messages.html',
         controller: 'messagesController',
         scope: {
-            decayTime: '='
+            errorDecay: '=?',
+            successDecay: '=?'
         }
     }
 });
 
 //TODO: empty controller is probably unnecessary :P
 app.controller('messagesController', function ($scope, $timeout) {
-    var decay = $scope.decayTime || 5000;
+    // default decay values
+    $scope.errorDecay = $scope.errorDecay || 15000;
+    $scope.successDecay = $scope.successDecay || 7500;
+
+    // the messages array
     $scope.messages = [];
 
     $scope.$on('message', function(event, message) {
+        var decay = message.type === 'error' ? $scope.errorDecay : $scope.successDecay;
         $scope.messages.push(message);
         $timeout(function() {
             var index = $scope.messages.indexOf(message);
             // TODO: We should set display hidden before splicing the message so the transition works
             $scope.messages.splice(index, 1);
-        }, decay);
+        }, message.decay || decay);
     });
 });
