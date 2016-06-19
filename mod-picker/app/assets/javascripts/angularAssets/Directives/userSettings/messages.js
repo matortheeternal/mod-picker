255dc8a4c19ea7db78e7872dc2cad0c446dfdaf4
Mator <why101@msn.com>
2016-06-19 14:38:14 -0700

    messages directive has been dramatically improved (more generic)

--- mod-picker/app/assets/javascripts/angularAssets/Directives/userSettings/messages.js
+++ mod-picker/app/assets/javascripts/angularAssets/Directives/userSettings/messages.js
@@ -1,17 +1,10 @@
-/**
- * Created by Sirius on 3/25/2016.
- */
-
 app.directive('messages', function () {
     return {
         restrict: 'E',
         templateUrl: '/resources/directives/userSettings/messages.html',
         controller: 'messagesController',
         scope: {
-            decayTime: '=',
-            successMessage: '@',
-            errors: '=',
-            successBool: '='
+            decayTime: '='
         }
     }
 });
@@ -19,12 +12,14 @@ app.directive('messages', function () {
 //TODO: empty controller is probably unnecessary :P
 app.controller('messagesController', function ($scope, $timeout) {
     var decay = $scope.decayTime || 5000;
+    $scope.messages = [];
 
-    $scope.$watch('successBool', function(newValue) {
-        if(newValue) {
-            $timeout(function() {
-                $scope.successBool = false;
-            }, decay);
-        }
+    $scope.$on('message', function(event, message) {
+        $scope.messages.push(message);
+        $timeout(function() {
+            var index = $scope.messages.indexOf(message);
+            // TODO: We should set display hidden before splicing the message so the transition works
+            $scope.messages.splice(index, 1);
+        }, decay);
     });
 });
