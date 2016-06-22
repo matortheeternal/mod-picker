6fdc20672379cdeb9f0ff8ce1b4fdf1188d5dbe0
Mator <why101@msn.com>
2016-06-21 20:24:22 -0700

    added getErrorQuote method to quoteService

--- mod-picker/app/assets/javascripts/angularAssets/BackendAPI/quoteService.js
+++ mod-picker/app/assets/javascripts/angularAssets/BackendAPI/quoteService.js
@@ -23,4 +23,54 @@ app.service('quoteService', function (backend, $q) {
         });
         return output.promise;
     };
+
+    this.getErrorQuote = function(errorCode) {
+        var errorQuotes = [{
+            codes: [401, 403, 550],
+            quotes: [
+                "Bit late to be wandering around, isn't it?",
+                "Hmm. I still don't like it, but I guess I'll overlook it. This time.",
+                "Wait... I know you.",
+                "My cousin is out fighting dragons. And what do I get? Guard duty.",
+                "Let me guess, someone stole your sweetroll?"
+            ],
+            class: "guard-quote"
+        }, {
+            codes: [404, 410],
+            quotes: [
+                "I'm fairly certain you've wandered into the wrong building, friend.",
+                "Move along citizen!",
+                "Citizen, you must be lost, the tavern is down the road a piece.",
+                "Ha!  Found you!"
+            ],
+            class: "penitus-quote"
+        }, {
+            codes: [503],
+            quotes: [
+                "I can't carry any more.",
+                "I am sworn to carry your burdens.",
+                "There's something... wrong here."
+            ],
+            class: "lydia-quote"
+        }, {
+            codes: [500],
+            quotes: [
+                "The butler did it! Or is it the advisor? Whoever that man behind the throne was.",
+                "Hmmmm... 'Fixed' is such a subjective term. I think 'treated' is far more appropriate, don't you? Like one does to a rash, or an arrow in the face.",
+                "Cheese! For everyone!"
+            ],
+            class: "sheogorath-quote"
+        }];
+
+        // get the object matching the input errorCode OR the last quote object in the array
+        var obj = errorQuotes.find(function(quote) {
+            return quote.codes.indexOf(errorCode) > -1;
+        }) || errorQuotes[errorQuotes.length - 1];
+
+        // return a random quote from the object
+        return {
+            text: randomElement(obj.quotes),
+            class: obj.class
+        }
+    };
 });
