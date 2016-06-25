4893058057f78de0fc6a3523c018ba318b58d184
Mator <why101@msn.com>
2016-06-24 23:50:23 -0700

    refactored errorService.errorMessage to create multiple messages when we receive a json response

--- mod-picker/app/assets/javascripts/angularAssets/Services/errorService.js
+++ mod-picker/app/assets/javascripts/angularAssets/Services/errorService.js
@@ -7,10 +7,35 @@ app.service('errorService', function ($rootScope, $location) {
         };
     };
 
-    this.errorMessage = function (label, errorResponse) {
-        return {
+    this.errorMessages = function (label, errorResponse) {
+        var errors = [];
+
+        // parse json errors into error messages if the response is json
+        if (typeof errorResponse.data == "object") {
+            // loop through response
+            var data = errorResponse.data;
+            for (var property in data) {
+                if (data.hasOwnProperty(property)) {
+                    // loop through errors array
+                    for (var i = 0; i < data[property].length; i++) {
+                        var item = data[property][i];
+                        errors.push({
+                            type: "error",
+                            text: label + ", " + property + ": " + item
+                        })
+                    }
+                }
+            }
+            // return errors array if we found any in the response json
+            if (errors.length) {
+                return errors;
+            }
+        }
+
+        // default return
+        return [{
             type: "error",
             text: label + ", " + errorResponse.status + ": " + errorResponse.statusText
-        }
+        }]
     };
 });
