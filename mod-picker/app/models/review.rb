72e38475f7bd2d24d66be48c46331d87f5fd084e
Mator <why101@msn.com>
2016-06-24 23:36:00 -0700

    customized message response on the unique review validation

--- mod-picker/app/models/review.rb
+++ mod-picker/app/models/review.rb
@@ -31,7 +31,7 @@ class Review < ActiveRecord::Base
   validates :mod_id, :text_body, presence: true
   validates :text_body, length: {in: 512..32768}
   # only one review per mod per user
-  validates :mod_id, uniqueness: { scope: :submitted_by }
+  validates :mod_id, uniqueness: { scope: :submitted_by, :message => "You've already submitted a review for this mod." }
 
   # Callbacks
   after_create :increment_counters
