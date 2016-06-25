6f5c3a6f5d1a135d6a76cfcf7f34abe81dba31f7
Mator <why101@msn.com>
2016-06-24 18:33:02 -0700

    created showHistory function for usage by the historyModal

--- mod-picker/app/assets/javascripts/angularAssets/Directives/contributions/historyModal.js
+++ mod-picker/app/assets/javascripts/angularAssets/Directives/contributions/historyModal.js
@@ -2,6 +2,22 @@ app.directive('historyModal', function () {
     return {
         restrict: 'E',
         templateUrl: '/resources/directives/contributions/historyModal.html',
+        controller: 'historyModalController',
         scope: false
     };
+});
+
+app.controller('historyModalController', function ($scope) {
+    $scope.showHistory = function(entry) {
+        $scope.activeHistoryEntry = angular.copy($scope.target);
+        if (entry) {
+            for (var property in entry) {
+                if (entry.hasOwnProperty(property)) {
+                    $scope.activeHistoryEntry[property] = entry[property];
+                }
+            }
+        }
+    };
+
+    $scope.showHistory();
 });
