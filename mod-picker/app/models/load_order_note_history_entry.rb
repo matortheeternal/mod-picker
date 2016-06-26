f1a4324f5229dd42f37df586a060327aeffab0b6
Mator <why101@msn.com>
2016-06-25 22:02:28 -0700

    added validations to load order note history entries

--- mod-picker/app/models/load_order_note_history_entry.rb
+++ mod-picker/app/models/load_order_note_history_entry.rb
@@ -2,6 +2,9 @@ class LoadOrderNoteHistoryEntry < ActiveRecord::Base
   belongs_to :load_order_note, :inverse_of => 'history_entries', :foreign_key => 'load_order_note_id'
   belongs_to :editor, :class_name => 'User', :foreign_key => 'edited_by', :inverse_of => 'load_order_note_history_entries'
 
+  # Validations
+  validates :load_order_note_id, :edited_by, :text_body, :edit_summary, presence: true
+
   # Callbacks
   after_create :increment_counters
   before_destroy :decrement_counters
