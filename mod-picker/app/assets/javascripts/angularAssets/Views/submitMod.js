f65a910a50c8f5dcb1ddb17878f7af5b9e64bd7e
Mator <why101@msn.com>
2016-06-28 14:35:45 -0700

    refactored submitModController.modInvalid to modValid, adding/fixing code as necessary for custom sources

--- mod-picker/app/assets/javascripts/angularAssets/Views/submitMod.js
+++ mod-picker/app/assets/javascripts/angularAssets/Views/submitMod.js
@@ -276,18 +276,46 @@ app.controller('submitModController', function ($scope, backend, submitService, 
     };
 
     /* mod submission */
-    $scope.modInvalid = function () {
-        // submission isn't allowed until the user has scraped a nexus page,
-        // provided a mod analysis, and provided at least one category
+
+    // submission isn't allowed until the user has provided at least one valid source
+    // provided a mod analysis, and provided at least one category
+    $scope.modValid = function () {
+        // main source validation
         var sourcesValid = true;
         $scope.sources.forEach(function(source) {
             sourcesValid = sourcesValid && source.valid;
         });
-        return (!sourcesValid || $scope.analysis == null || $scope.mod.categories == null ||
-            $scope.mod.categories.length == 0 || $scope.mod.categories.length > 2)
+
+        // custom source validation
+        if ($scope.customSources.length) {
+            $scope.customSources.forEach(function(source) {
+                sourcesValid = sourcesValid && source.valid;
+            });
+            // if we are only submitting custom soruces, we need to verify
+            // we have all general info
+            if (!$scope.sources.length) {
+                sourcesValid = sourcesValid && $scope.mod.name && $scope.mod.authors &&
+                    $scope.mod.released;
+            }
+        }
+        else {
+            // if we don't have any custom sources we should verify we have
+            // the scraped data for at least one official source
+            sourcesValid = sourcesValid && ($scope.nexus || $scope.workshop || $scope.lab);
+        }
+
+        // return true if any
+        var categoriesValid = $scope.mod.categories && $scope.mod.categories.length &&
+            $scope.mod.categories.length <= 2;
+        return (sourcesValid && categoriesValid && $scope.mod.analysis)
     };
 
     $scope.submit = function () {
+        // return if mod is invalid
+        if (!$scope.modValid()) {
+            return;
+        }
+
         var sources = {
             nexus: $scope.nexus,
             workshop: $scope.workshop,
@@ -295,7 +323,7 @@ app.controller('submitModController', function ($scope, backend, submitService, 
         };
         $scope.submitting = true;
         $scope.submittingStatus = "Submitting Mod...";
-        submitService.submitMod($scope.mod, $scope.analysis, sources, $scope.requirements).then(function() {
+        submitService.submitMod($scope.mod, sources, $scope.customSources).then(function() {
             $scope.submittingStatus = "Mod Submitted Successfully!";
             $scope.success = true;
         }, function(response) {
