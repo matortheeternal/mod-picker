8c99b3b706f9bd2bc0e97215f9608258d96b6645
Mator <why101@msn.com>
2016-07-03 14:42:46 -0700

    now rendering submitter association in default article json

--- mod-picker/app/models/article.rb
+++ mod-picker/app/models/article.rb
@@ -4,6 +4,22 @@ class Article < ActiveRecord::Base
   # Callbacks
   before_save :set_dates
 
+  def as_json(options={})
+    if JsonHelpers.json_options_empty(options)
+      default_options = {
+          :include => {
+              :submitter => {
+                  :only => [:id, :username, :role, :title],
+                  :methods => :avatar
+              }
+          }
+      }
+      super(options.merge(default_options))
+    else
+      super(options)
+    end
+  end
+
   private
     def set_dates
       if self.submitted.nil?
