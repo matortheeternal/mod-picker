6e161ef2df9d2293b4ef562bc17960e75ac944c1
Mator <why101@msn.com>
2016-07-04 15:16:29 -0700

    reordered some callbacks

--- mod-picker/app/models/mod.rb
+++ mod-picker/app/models/mod.rb
@@ -210,7 +210,7 @@ class Mod < ActiveRecord::Base
   validates :name, :aliases, length: {maximum: 128}
 
   # callbacks
-  after_create :increment_counters, :create_associations, :update_metrics
+  after_create :create_associations, :update_metrics, :increment_counters
   after_update :create_associations, :update_metrics
   before_destroy :decrement_counters
 
