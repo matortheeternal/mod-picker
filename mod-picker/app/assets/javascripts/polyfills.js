84de47e64fcd03a256178c49b22836ac2c2cd2ac
Mator <why101@msn.com>
2016-07-04 18:06:43 -0700

    added getDifferentProperties helper function to polyfills

--- mod-picker/app/assets/javascripts/polyfills.js
+++ mod-picker/app/assets/javascripts/polyfills.js
@@ -1,3 +1,23 @@
 String.prototype.capitalize = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
 };
+
+function getDifferentProperties(obj, otherObj) {
+    var result = {};
+    for (var property in obj) {
+        if (obj.hasOwnProperty(property)) {
+            // if we pass absolute equality we skip the property
+            var p1 = obj[property];
+            var p2 = otherObj[property];
+            if (p1 === p2) {
+                continue;
+            } else if (!!p1 && !!p2) {
+                if (p1.toString() === p2.toString()) {
+                    continue;
+                }
+            }
+            result[property] = obj[property];
+        }
+    }
+    return result;
+}
