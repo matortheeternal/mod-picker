c3730c0e65f0d427c61011e60227f7eacd8548ae
Sirius <thesiriusadam@gmail.com>
2016-07-06 15:58:19 -0500

    added a new variable to all filter objects called param that names the params

--- mod-picker/app/assets/javascripts/angularAssets/factory/filtersFactory.js
+++ mod-picker/app/assets/javascripts/angularAssets/factory/filtersFactory.js
@@ -1,75 +1,84 @@
-app.service('filtersFactory', function () {
+app.service("filtersFactory", function() {
     this.modStatisticFilters = function() {
-        return [
-            {
+        return [{
                 label: "Endorsements",
                 common: true,
-                sites: {nexus: true},
+                sites: { nexus: true },
                 data: "endorsements",
-                max: 500000
+                max: 500000,
+                param: "end"
             },
             {
                 label: "Subscribers",
                 common: true,
-                sites: {workshop: true},
+                sites: { workshop: true },
                 data: "subscribers",
-                max: 1250000
+                max: 1250000,
+                param: "su"
             },
             {
                 label: "Unique Downloads",
                 common: true,
-                sites: {nexus: true},
+                sites: { nexus: true },
                 data: "unique_downloads",
-                max: 10000000
+                max: 10000000,
+                param: "ud"
             },
             {
                 label: "Favorites",
                 common: true,
-                sites: {workshop: true, lab: true},
+                sites: { workshop: true, lab: true },
                 data: "favorites",
-                max: 20000
+                max: 20000,
+                param: "fa"
             },
             {
                 label: "Downloads",
                 common: true,
-                sites: {nexus: true, lab: true},
+                sites: { nexus: true, lab: true },
                 data: "downloads",
-                max: 50000000
+                max: 50000000,
+                param: "dl"
             },
             {
                 label: "Views",
                 common: true,
-                sites: {nexus: true, workshop: true, lab: true},
+                sites: { nexus: true, workshop: true, lab: true },
                 data: "views",
-                max: 50000000
+                max: 50000000,
+                param: "vw"
             },
             {
                 label: "Posts Count",
                 common: false,
-                sites: {nexus: true, workshop: true},
+                sites: { nexus: true, workshop: true },
                 data: "posts",
-                max: 500000
+                max: 500000,
+                param: "pc"
             },
             {
                 label: "Images Count",
                 common: false,
-                sites: {nexus: true, workshop: true},
-                data: "endorsements",
-                max: 500000
+                sites: { nexus: true, workshop: true },
+                data: "images",
+                max: 200,
+                param: "ic"
             },
             {
                 label: "Videos Count",
                 common: false,
-                sites: {nexus: true, workshop: true},
+                sites: { nexus: true, workshop: true },
                 data: "videos",
-                max: 50
+                max: 50,
+                param: "vc"
             },
             {
                 label: "Files Count",
                 common: false,
-                sites: {nexus: true},
+                sites: { nexus: true },
                 data: "files",
-                max: 200
+                max: 200,
+                param: "fc"
             },
             /*{ TODO: Unimplemented on backend
                 label: "Bugs Count",
@@ -81,77 +90,86 @@ app.service('filtersFactory', function () {
             {
                 label: "Discussions Count",
                 common: false,
-                sites: {/*nexus: true, TODO: Unimplemented on backend */ workshop: true},
+                sites: { /*nexus: true, TODO: Unimplemented on backend */ workshop: true },
                 data: "discussions",
-                max: 50
+                max: 50,
+                param: "dc"
             },
             {
                 label: "Articles Count",
                 common: false,
-                sites: {nexus: true},
+                sites: { nexus: true },
                 data: "articles",
-                max: 50
+                max: 50,
+                param: "ac"
             }
         ];
     };
 
     this.modPickerFilters = function() {
-        return [
-            {
+        return [{
                 label: "Reputation",
                 common: true,
                 data: "reputation",
-                max: 450
+                max: 450,
+                param: "rep"
             },
             {
                 label: "Average Rating",
                 common: true,
                 data: "rating",
-                max: 100
+                max: 100,
+                param: "avg"
             },
             {
                 label: "Reviews Count",
                 common: true,
                 data: "reviews",
-                max: 200
+                max: 200,
+                param: "rev"
             },
             {
                 label: "Number of Stars",
                 common: false,
                 data: "stars",
-                max: 1000
+                max: 1000,
+                param: "str"
             },
             {
                 label: "Compatibility Notes Count",
                 common: false,
                 data: "compatibility_notes",
-                max: 100
+                max: 100,
+                param: "cnc"
             },
             {
                 label: "Install Order Notes Count",
                 common: false,
                 data: "install_order_notes",
-                max: 100
+                max: 100,
+                param: "ioc"
             },
             {
                 label: "Load Order Notes Count",
                 common: false,
                 data: "load_order_notes",
-                max: 100
+                max: 100,
+                param: "loc"
             }
-        ]
+        ];
     };
 
     this.modDateFilters = function() {
-        return [
-            {
+        return [{
                 label: "Date Updated",
-                data: "updated"
+                data: "updated",
+                param: "du"
             },
             {
                 label: "Date Released",
-                data: "released"
+                data: "released",
+                param: "dr"
             }
-        ]
-    }
+        ];
+    };
 });
