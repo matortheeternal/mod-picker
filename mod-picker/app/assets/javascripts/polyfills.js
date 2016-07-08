001cbe01fb0abccfd86aceae059ed9871827aedd
Mator <why101@msn.com>
2016-07-08 15:28:05 -0700

    added String.prototype.titleCase polyfill

--- mod-picker/app/assets/javascripts/polyfills.js
+++ mod-picker/app/assets/javascripts/polyfills.js
@@ -2,6 +2,14 @@ String.prototype.capitalize = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
 };
 
+String.prototype.titleCase = function() {
+    return this.replace(/[^-'\s]+/g, function(word) {
+        return word.replace(/^./, function(first) {
+            return first.toUpperCase();
+        });
+    });
+};
+
 function getDifferentProperties(obj, otherObj) {
     var result = {};
     for (var property in obj) {
