e29fad3b67dd373de28b495bc96076fb265b250e
Mator <why101@msn.com>
2016-07-09 13:58:41 -0700

    made comments and users indexes use the new indexFactory.buildIndex function :boom: :zap: :boom: :scream: :triumph:

--- mod-picker/app/assets/javascripts/angularAssets/Views/browse/comments.js
+++ mod-picker/app/assets/javascripts/angularAssets/Views/browse/comments.js
@@ -22,63 +22,23 @@ app.run(function($futureState, indexService, filtersFactory) {
     $futureState.futureState(state);
 });
 
-app.controller('commentsIndexController', function ($scope, $q, $stateParams, $state, contributionService, sliderFactory, columnsFactory, filtersFactory, currentUser, indexService) {
+app.controller('commentsIndexController', function ($scope, $stateParams, $state, currentUser, contributionService, columnsFactory, filtersFactory, indexService, indexFactory) {
     // get parent variables
     $scope.currentUser = currentUser;
     $scope.globalPermissions = angular.copy(currentUser.permissions);
 
-    // initialize local variables
-    $scope.availableColumnData = [];
-    $scope.actions = [];
-    $scope.extendedFilterVisibility = {};
-    $scope.pages = {};
+    // columns for view
     $scope.columns = columnsFactory.commentColumns();
     $scope.columnGroups = columnsFactory.commentColumnGroups();
 
-    // load sort values from url parameters
-    $scope.sort = {};
-    indexService.setSortFromParams($scope.sort, $stateParams);
-
-    // initialize filters
+    // filters for view
     $scope.filterPrototypes = filtersFactory.commentFilters();
     $scope.dateFilters = filtersFactory.contributionDateFilters();
     $scope.statFilters = filtersFactory.commentStatisticFilters();
-    $scope.filters = {};
-    //load filter values from url parameters
-    indexService.setFiltersFromParams($scope.filters, $scope.filterPrototypes, $stateParams);
 
-    /* data fetching functions */
-    var firstGet = false;
-    $scope.getComments = function(page) {
-        delete $scope.comments;
-        var options = {
-            filters: $scope.filters,
-            sort: $scope.sort,
-            page: page || 1
-        };
-        contributionService.retrieveContributions('comments', options, $scope.pages).then(function(data) {
-            $scope.comments = data.comments;
-            firstGet = true;
-        });
-    };
-
-    // fetch comments when we load the page
-    $scope.getComments();
-
-    // fetch comments again when filters or sort changes
-    var getCommentsTimeout;
-    $scope.$watch('[filters, sort]', function() {
-        // get users
-        if ($scope.filters && firstGet) {
-            clearTimeout(getCommentsTimeout);
-            $scope.pages.current = 1;
-            getCommentsTimeout = setTimeout($scope.getComments, 700);
-        }
-
-        // set url parameters
-        if ($scope.filters && firstGet) {
-            var params = indexService.getParamsFromFilters($scope.filters, $scope.filterPrototypes);
-            $state.transitionTo('base.comments', params, { notify: false });
-        }
-    }, true);
+    // build generic controller stuff
+    $scope.route = 'comments';
+    $scope.contributions = true;
+    $scope.retrieve = contributionService.retrieveContributions;
+    indexFactory.buildIndex($scope, $stateParams, $state, indexService);
 });
--- mod-picker/app/assets/javascripts/angularAssets/Views/browse/users.js
+++ mod-picker/app/assets/javascripts/angularAssets/Views/browse/users.js
@@ -22,71 +22,22 @@ app.run(function($futureState, indexService, filtersFactory) {
     $futureState.futureState(state);
 });
 
-app.controller('usersController', function ($scope, $q, $stateParams, $state, userService, sliderFactory, columnsFactory, filtersFactory, currentUser, indexService) {
+app.controller('usersController', function ($scope, $stateParams, $state, currentUser, userService, columnsFactory, filtersFactory, indexService, indexFactory) {
     // get parent variables
     $scope.currentUser = currentUser;
     $scope.globalPermissions = angular.copy(currentUser.permissions);
 
-    // initialize local variables
-    $scope.availableColumnData = [];
-    $scope.actions = [];
-    $scope.extendedFilterVisibility = {};
-    $scope.pages = {};
+    // columns for view
     $scope.columns = columnsFactory.userColumns();
     $scope.columnGroups = columnsFactory.userColumnGroups();
-
-    // load sort values from url parameters
-    $scope.sort = {};
-    indexService.setSortFromParams($scope.sort, $stateParams);
 
     // initialize filters
     $scope.filterPrototypes = filtersFactory.userFilters();
     $scope.dateFilters = filtersFactory.userDateFilters();
     $scope.statFilters = filtersFactory.userStatisticFilters();
-    $scope.filters = {};
-    //load filter values from url parameters
-    indexService.setFiltersFromParams($scope.filters, $scope.filterPrototypes, $stateParams);
 
-    /* filter helper functions */
-    $scope.toggleExtendedFilterVisibility = function(filterId) {
-        var extendedFilter = $scope.extendedFilterVisibility[filterId] = !$scope.extendedFilterVisibility[filterId];
-        if (extendedFilter) {
-            $scope.$broadcast('rerenderSliders');
-        }
-    };
-
-    /* data fetching functions */
-    var firstGet = false;
-    $scope.getUsers = function(page) {
-        delete $scope.users;
-        var options = {
-            filters: $scope.filters,
-            sort: $scope.sort,
-            page: page || 1
-        };
-        userService.retrieveUsers(options, $scope.pages).then(function(data) {
-            $scope.users = data.users;
-            firstGet = true;
-        });
-    };
-
-    // fetch users when we load the page
-    $scope.getUsers();
-
-    // fetch users again when filters or sort changes
-    var getUsersTimeout;
-    $scope.$watch('[filters, sort]', function() {
-        // get users
-        if ($scope.filters && firstGet) {
-            clearTimeout(getUsersTimeout);
-            $scope.pages.current = 1;
-            getUsersTimeout = setTimeout($scope.getUsers, 700);
-        }
-
-        // set url parameters
-        if ($scope.filters && firstGet) {
-            var params = indexService.getParamsFromFilters($scope.filters, $scope.filterPrototypes);
-            $state.transitionTo('base.users', params, { notify: false });
-        }
-    }, true);
+    // build generic controller stuff
+    $scope.route = 'users';
+    $scope.retrieve = userService.retrieveUsers;
+    indexFactory.buildIndex($scope, $stateParams, $state, indexService);
 });
