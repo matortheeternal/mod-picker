50124a7731f128a81d082aa6f98b11d091fda275
Mator <why101@msn.com>
2016-07-08 21:31:13 -0700

    comments#index method fleshed out (json formatting, pagination, and sorting)

--- mod-picker/app/controllers/comments_controller.rb
+++ mod-picker/app/controllers/comments_controller.rb
@@ -3,9 +3,26 @@ class CommentsController < ApplicationController
 
   # GET /comments
   def index
-    @comments = Comment.accessible_by(current_ability).filter(filtering_params)
+    @comments = Comment.includes(:submitter => :reputation).accessible_by(current_ability).filter(filtering_params).sort(params[:sort]).paginate(:page => params[:page])
+    count = Comment.accessible_by(current_ability).filter(filtering_params).count
 
-    render :json => @comments
+    render :json => {
+        comments: @comments.as_json({
+            :except => [:submitted_by],
+            :include => {
+                :submitter => {
+                    :only => [:id, :username, :role, :title],
+                    :include => {
+                        :reputation => {:only => [:overall]}
+                    },
+                    :methods => :avatar
+                }
+            },
+            :methods => :commentable_link
+        }),
+        max_entries: count,
+        entries_per_page: Comment.per_page
+    }
   end
 
   # GET /comments/1
