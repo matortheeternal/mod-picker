ff4d87046ead0ab03c2205f52d09801190b8ff34
Mator <why101@msn.com>
2016-07-09 12:46:21 -0700

    made filter/params functions in indexService more generic

--- mod-picker/app/assets/javascripts/angularAssets/Services/indexService.js
+++ mod-picker/app/assets/javascripts/angularAssets/Services/indexService.js
@@ -36,67 +36,84 @@ app.service('indexService', function() {
         }
     };
 
-    this.setParamFromFilter = function(filters, key, params, paramKey) {
-        if (filters.hasOwnProperty(key)) {
-            params[paramKey] = filters[key];
+    this.setSliderFilterFromParam = function(filters, filter, paramValue) {
+        var filterVals = paramValue.split('-');
+
+        // special date filter handling
+        if (filter.subtype === 'Date') {
+            filters[filter.data] = {
+                min: filterVals[0].replace(/\./g, "/"),
+                max: filterVals[1].replace(/\./g, "/")
+            };
+        } else {
+            filters[filter.data] = {
+                min: parseInt(filterVals[0]),
+                max: parseInt(filterVals[1])
+            };
         }
     };
 
-    this.setListParamFromFilter = function(filters, key, params, paramKey) {
-        if (filters.hasOwnProperty(key)) {
-            params[paramKey] = filters[key].join(',');
-        }
-    };
-
-    this.setSliderFiltersFromParams = function(filters, filterPrototypes, stateParams) {
+    this.setDefaultParamsFromFilters = function(params, filterPrototypes) {
         filterPrototypes.forEach(function(filter) {
-            if (!stateParams[filter.param]) {
-                return;
-            }
-            var filterVals = stateParams[filter.param].split('-');
-
-            // special date filter handling
-            if (filter.type === 'Date') {
-                filters[filter.data] = {
-                    min: filterVals[0].replace(/\./g, "/"),
-                    max: filterVals[1].replace(/\./g, "/")
-                };
+            if (filter.type === 'Boolean') {
+                params[filter.param] = filter.default;
             } else {
-                filters[filter.data] = {
-                    min: parseInt(filterVals[0]),
-                    max: parseInt(filterVals[1])
-                };
+                params[filter.param] = '';
             }
         });
     };
 
-    this.getParamsFromSliderFilters = function(filters, filterPrototypes) {
+    this.setFiltersFromParams = function(filters, filterPrototypes, stateParams) {
+        filterPrototypes.forEach(function(filter) {
+            // skip filters which we don't have a param set for
+            if (!stateParams[filter.param]) {
+                return;
+            }
+
+            // handle range filters (sliders)
+            if (filter.type === 'Range') {
+                service.setSliderFilterFromParam(filters, filter, stateParams[filter.param]);
+            }
+            // handle list filters
+            else if (filter.type === 'List') {
+                service.setListFilterFromParam(filters, filter.data, stateParams[filter.param]);
+            }
+            // handle normal filters
+            else {
+                service.setFilterFromParam(filters, filter.data, stateParams[filter.param]);
+            }
+        });
+    };
+
+    this.getParamsFromFilters = function(filters, filterPrototypes) {
         var params = {};
-        var setParams = function(protoFilter) {
+        filterPrototypes.forEach(function(filter) {
             // this is the filter values stored on the scope of the view
-            var filter = filters[protoFilter.data];
-            // if the filter has been set to a non-default value it will be defined
-            if (filter) {
+            var filterValue = filters[filter.data];
+
+            // return if the filter has not been set
+            if (!filterValue) {
+                return;
+            }
+
+            // if the filter is a range filter, it will have a min and max value
+            if (filter.type === 'Range') {
                 // special handling for date slider filters
-                if (protoFilter.type === 'Date') {
-                    params[protoFilter.param] = (filter.min + "-" + filter.max).replace(/\//g, ".");
-                }
-                // if the filter is a range filter, it will have a min and max value
-                else if (filter.hasOwnProperty('min') && filter.hasOwnProperty('max')) {
-                    params[protoFilter.param] = filter.min + "-" + filter.max;
-                }
-                // else if the filter is an array, make it into a comma separate list
-                else if (filter.constructor === Array) {
-                    params[protoFilter.param] = filter.join(',');
-                }
-                // else the filter should be just a value
-                else {
-                    params[protoFilter.param] = filter.toString();
+                if (filter.subtype === 'Date') {
+                    params[filter.param] = (filterValue.min + "-" + filterValue.max).replace(/\//g, ".");
+                } else {
+                    params[filter.param] = filterValue.min + "-" + filterValue.max;
                 }
             }
-        };
-
-        filterPrototypes.forEach(setParams);
+            // else if the filter is a list filter, generate param as comma separated list
+            else if (filter.type === 'List') {
+                params[filter.param] = filterValue.join(',');
+            }
+            // else the filter should be just a value
+            else {
+                params[filter.param] = filterValue.toString();
+            }
+        });
 
         return params;
     };
