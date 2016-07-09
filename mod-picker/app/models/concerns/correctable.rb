5800a4472aeab02acb35c32c8d51936c7d3f513d
Mator <why101@msn.com>
2016-07-09 15:23:55 -0700

    added correctable module with standing enum, corrections association, and compute_standing helper method

---
+++ mod-picker/app/models/concerns/correctable.rb
@@ -0,0 +1,18 @@
+module Sortable
+  extend ActiveSupport::Concern
+
+  included do
+    enum standing: [ :good, :unknown, :bad ]
+    has_many :corrections, :as => 'correctable'
+  end
+
+  def compute_standing
+    if self.corrections.where(status: 1).count > 0
+      self.standing = :bad
+    elsif self.corrections.where(status: 0).count > 0
+      self.standing = :unknown
+    else
+      self.standing = :good
+    end
+  end
+end
