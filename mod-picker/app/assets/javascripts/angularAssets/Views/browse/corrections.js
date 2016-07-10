0c8cb7359a0097ee542f60089c3222c8c62b0abc
Mator <why101@msn.com>
2016-07-10 16:37:58 -0700

    added state and controller for corrections index

---
+++ mod-picker/app/assets/javascripts/angularAssets/Views/browse/corrections.js
@@ -0,0 +1,23 @@
+app.run(function($futureState, indexFactory, filtersFactory) {
+    // dynamically construct and apply state
+    var filterPrototypes = filtersFactory.correctionFilters();
+    var state = indexFactory.buildState('id', 'asc', 'corrections', filterPrototypes);
+    $futureState.futureState(state);
+});
+
+app.controller('correctionsController', function ($scope, $stateParams, $state, currentUser, contributionService, indexService,  filtersFactory, indexFactory) {
+    // get parent variables
+    $scope.currentUser = currentUser;
+    $scope.globalPermissions = angular.copy(currentUser.permissions);
+
+    // filters for view
+    $scope.filterPrototypes = filtersFactory.correctionFilters();
+    $scope.dateFilters = filtersFactory.contributionDateFilters();
+    $scope.statFilters = filtersFactory.correctionStatisticFilters();
+
+    // build generic controller stuff
+    $scope.route = 'corrections';
+    $scope.contributions = true;
+    $scope.retrieve = contributionService.retrieveContributions;
+    indexFactory.buildIndex($scope, $stateParams, $state, indexService);
+});
