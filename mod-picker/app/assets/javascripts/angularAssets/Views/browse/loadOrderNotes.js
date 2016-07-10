bcde75084390d2494e66fc4607a996b308efa5d8
Mator <why101@msn.com>
2016-07-10 15:31:04 -0700

    added load order notes index page state and controller

---
+++ mod-picker/app/assets/javascripts/angularAssets/Views/browse/loadOrderNotes.js
@@ -0,0 +1,23 @@
+app.run(function($futureState, indexFactory, filtersFactory) {
+    // dynamically construct and apply state
+    var filterPrototypes = filtersFactory.loadOrderNoteFilters();
+    var state = indexFactory.buildState('reputation', 'asc', 'loadOrderNotes', filterPrototypes);
+    $futureState.futureState(state);
+});
+
+app.controller('loadOrderNotesController', function ($scope, $stateParams, $state, currentUser, contributionService, indexService,  filtersFactory, indexFactory) {
+    // get parent variables
+    $scope.currentUser = currentUser;
+    $scope.globalPermissions = angular.copy(currentUser.permissions);
+
+    // filters for view
+    $scope.filterPrototypes = filtersFactory.loadOrderNoteFilters();
+    $scope.dateFilters = filtersFactory.contributionDateFilters();
+    $scope.statFilters = filtersFactory.noteStatisticFilters();
+
+    // build generic controller stuff
+    $scope.route = 'load_order_notes';
+    $scope.contributions = true;
+    $scope.retrieve = contributionService.retrieveContributions;
+    indexFactory.buildIndex($scope, $stateParams, $state, indexService);
+});
