2a0a064de4c8c6b6f84e522f99a90b26cbf42a2a
Mator <why101@msn.com>
2016-07-10 15:29:27 -0700

    added scopes to correctable.rb

--- mod-picker/app/models/concerns/correctable.rb
+++ mod-picker/app/models/concerns/correctable.rb
@@ -3,6 +3,23 @@ module Correctable
 
   included do
     enum standing: [ :good, :unknown, :bad ]
+
+    scope :editor, -> (username) { joins(:editors).where(:users => {:username => username}) }
+    scope :standing, -> (standing_hash) {
+      # build commentables array
+      standings = []
+      standing_hash.each_with_index do |(key,value),index|
+        if standing_hash[key]
+          standings.push(index)
+        end
+      end
+
+      # return query
+      where(standing: standings)
+    }
+    scope :corrections_count, -> (range) { where(corrections_count: range[:min]..range[:max]) }
+    scope :history_entries_count, -> (range) { where(history_entries_count: range[:min]..range[:max]) }
+
     has_many :corrections, :as => 'correctable'
   end
 
