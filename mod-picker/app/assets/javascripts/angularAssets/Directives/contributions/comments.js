7e11f64735fa45aadb7e1a1c82522c877147b29f
Mator <why101@msn.com>
2016-07-11 16:25:43 -0700

    re-added context links to comments index

--- mod-picker/app/assets/javascripts/angularAssets/Directives/contributions/comments.js
+++ mod-picker/app/assets/javascripts/angularAssets/Directives/contributions/comments.js
@@ -8,7 +8,8 @@ app.directive('comments', function () {
             currentUser: '=',
             modelName: '=?',
             target: '=?',
-            eventPrefix: '=?'
+            eventPrefix: '=?',
+            showContext: "=?"
         }
     };
 });
