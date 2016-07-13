53fb8ea4828562e715fd4079d80dc558f32a13ba
Mator <why101@msn.com>
2016-07-13 12:06:44 -0700

    cleaned up mod list routes

--- mod-picker/config/routes.rb
+++ mod-picker/config/routes.rb
@@ -98,17 +98,9 @@ Rails.application.routes.draw do
     # mod lists
     resources :mod_lists, only: [:index, :show, :create, :update, :destroy]
     match '/active_mod_list', to: 'mod_lists#active', via: [:get]
-
-    # mod list mods
     match '/mod_lists/:id/mods', to: 'mod_lists#mods', via: 'get'
-
-    # mod list tools
     match '/mod_lists/:id/tools', to: 'mod_lists#tools', via: 'get'
-
-    # mod list plugins
     match '/mod_lists/:id/plugins', to: 'mod_lists#plugins', via: 'get'
-
-    # mod list config files
     match '/mod_lists/:id/config', to: 'mod_lists#configs', via: 'get'
 
     # mod and mod list stars
