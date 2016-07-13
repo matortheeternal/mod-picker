ac0b8507c257bd584f45b110b6285b5f505b0e09
Mator <why101@msn.com>
2016-07-13 10:40:25 -0700

    added updateModList function to modListService

--- mod-picker/app/assets/javascripts/angularAssets/BackendAPI/modListService.js
+++ mod-picker/app/assets/javascripts/angularAssets/BackendAPI/modListService.js
@@ -1,35 +1,11 @@
 app.service('modListService', function (backend, $q) {
-    this.retrieveModList = function (modListId) {
+    this.retrieveModList = function(modListId) {
         return backend.retrieve('/mod_lists/' + modListId);
     };
 
-    // copypasted from the modService
-    // this.retrieveMods = function (filters, sort, newPage) {
-    //     var mods = $q.defer();
-
-    //     if(newPage && newPage > pages.max) {
-    //         mods.reject();
-    //         return mods.promise;
-    //     }
-
-    //     pages.current = newPage || pages.current;
-
-    //     var postData =  {
-    //         filters: filters,
-    //         sort: sort,
-    //         page: pages.current
-    //     };
-
-    //     backend.post('/mods', postData).then(function (data) {
-    //         pages.max = Math.ceil(data.max_entries / data.entries_per_page);
-    //         mods.resolve({
-    //             mods: data.mods,
-    //             pageInformation: pages
-    //         });
-    //     });
-
-    //     return mods.promise;
-    // };
+    this.updateModList = function(modlist) {
+        return backend.update('/mod_lists/' + modlist.id, { mod_list: modlist });
+    };
 
     this.starModList = function(modId, starred) {
         var star = $q.defer();
