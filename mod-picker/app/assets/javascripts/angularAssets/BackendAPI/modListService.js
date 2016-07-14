fc5c07d0f3875332e3ebbb66922567673b09bbaa
Mator <why101@msn.com>
2016-07-13 19:08:28 -0700

    added retrieveModListTools method to modListService

--- mod-picker/app/assets/javascripts/angularAssets/BackendAPI/modListService.js
+++ mod-picker/app/assets/javascripts/angularAssets/BackendAPI/modListService.js
@@ -3,6 +3,18 @@ app.service('modListService', function (backend, objectUtils) {
         return backend.retrieve('/mod_lists/' + modListId);
     };
 
+    this.starModList = function(modListId, starred) {
+        if (starred) {
+            return backend.delete('/mod_lists/' + modListId + '/star');
+        } else {
+            return backend.post('/mod_lists/' + modListId + '/star', {});
+        }
+    };
+
+    this.retrieveModListTools = function(modListId) {
+        return backend.retrieve('/mod_lists/' + modListId + '/tools');
+    };
+
     this.updateModList = function(modList) {
         var modListData = {
             mod_list: {
@@ -17,13 +29,5 @@ app.service('modListService', function (backend, objectUtils) {
         objectUtils.deleteEmptyProperties(modListData, 1);
 
         return backend.update('/mod_lists/' + modlist.id, modListData);
-    };
-
-    this.starModList = function(modListId, starred) {
-        if (starred) {
-            return backend.delete('/mod_lists/' + modListId + '/star');
-        } else {
-            return backend.post('/mod_lists/' + modListId + '/star', {});
-        }
     };
 });
