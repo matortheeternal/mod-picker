f4493d332238c8c19f4b331fd7a4226a9c7b78d6
Mator <why101@msn.com>
2016-07-14 11:20:08 -0700

    created and ran migration to add tools_count column to mod_lists table

--- mod-picker/db/schema.rb
+++ mod-picker/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160710151363) do
+ActiveRecord::Schema.define(version: 20160714181748) do
 
   create_table "agreement_marks", id: false, force: :cascade do |t|
     t.integer "correction_id", limit: 4,                null: false
@@ -446,6 +446,7 @@ ActiveRecord::Schema.define(version: 20160710151363) do
     t.boolean  "is_collection",                           default: false, null: false
     t.string   "name",                      limit: 255,                   null: false
     t.text     "description",               limit: 65535
+    t.integer  "tools_count",               limit: 4,     default: 0,     null: false
     t.integer  "mods_count",                limit: 4,     default: 0,     null: false
     t.integer  "plugins_count",             limit: 4,     default: 0,     null: false
     t.integer  "active_plugins_count",      limit: 4,     default: 0,     null: false
