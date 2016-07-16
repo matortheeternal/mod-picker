c40f3ef309a27bb0296af79d90e5b51b198df6f3
Mator <why101@msn.com>
2016-07-15 17:11:30 -0700

    moved agreement mark json into AgreementMark as_json default_options

--- mod-picker/app/controllers/mods_controller.rb
+++ mod-picker/app/controllers/mods_controller.rb
@@ -166,7 +166,7 @@ class ModsController < ApplicationController
     agreement_marks = AgreementMark.where(submitted_by: current_user.id, correction_id: corrections.ids)
     render :json => {
         corrections: corrections,
-        agreement_marks: agreement_marks.as_json({:only => [:correction_id, :agree]})
+        agreement_marks: agreement_marks
     }
   end
 
