2cbd81481eff35ff59f756a76d5df7cd7a7bda9a
Sirius <thesiriusadam@gmail.com>
2016-07-16 16:04:22 -0500

    removed variables that are no longer used
    sorting and filtering is stored in the tab params now

--- mod-picker/app/assets/javascripts/angularAssets/Views/mod/mod.js
+++ mod-picker/app/assets/javascripts/angularAssets/Views/mod/mod.js
@@ -110,23 +110,6 @@ app.controller('modController', function($scope, $q, $stateParams, $state, $time
     $scope.tags = [];
     $scope.newTags = [];
     $scope.statusModal = {};
-    $scope.sort = {
-        reviews: {},
-        compatibility_notes: {},
-        install_order_notes: {},
-        load_order_notes: {}
-    };
-    $scope.filters = {
-        compatibility_notes: {
-            mod_list: true
-        },
-        install_order_notes: {
-            mod_list: true
-        },
-        load_order_notes: {
-            mod_list: true
-        }
-    };
     $scope.retrieving = {};
     // error handling
     $scope.errors = {};
