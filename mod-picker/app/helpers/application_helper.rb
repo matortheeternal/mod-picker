1888a9bd80e4204380172458ae8f9fb7d256a5d1
BlueAccords <myoldemailname@gmail.com>
2016-07-16 13:01:32 -0700

    added toc to redcarpet renderer

--- mod-picker/app/helpers/application_helper.rb
+++ mod-picker/app/helpers/application_helper.rb
@@ -15,7 +15,12 @@ module ApplicationHelper
     renderer = Redcarpet::Render::HTML.new(render_options)
     # instance vars used to prevent unnecessary reintiializing
     @markdown ||= Redcarpet::Markdown.new(renderer, extensions)
-
-    @markdown.render(text).html_safe
+    # generate table of contents as well
+    @markdown_toc ||= Redcarpet::Markdown.new(
+      Redcarpet::Render::HTML_TOC.new(nesting_level: 3)
+    )
+    
+    # render table of contents then page content
+    @markdown_toc.render(text).html_safe + @markdown.render(text).html_safe
   end
 end
