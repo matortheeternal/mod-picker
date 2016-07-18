fea85531f7f33870c29efc9e2ff8104395a7aa6d
Mator <why101@msn.com>
2016-07-18 11:05:30 -0700

    added error rendering to mod_list_mods#new method

--- mod-picker/app/controllers/mod_list_mods_controller.rb
+++ mod-picker/app/controllers/mod_list_mods_controller.rb
@@ -2,10 +2,14 @@ class ModListModsController < ApplicationController
   # GET /mod_list_mods/new
   # Renders the JSON for a new mod list mod
   def new
-    @mod = Mod.find(params[:mod_id])
-    authorize! :read, @mod
+    if params.has_key?(:mod_id)
+      @mod = Mod.find(params[:mod_id])
+      authorize! :read, @mod
 
-    @mod_list_mod = ModListMod.new(mod_id: @mod.id, index: 0)
-    render json: @mod_list_mod
+      @mod_list_mod = ModListMod.new(mod_id: @mod.id, index: 0)
+      render json: @mod_list_mod
+    else
+      render json: {error: "You must specify a mod_id to create a new ModListMod."}, status: 400
+    end
   end
 end
