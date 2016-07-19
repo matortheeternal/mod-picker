8f1849b40a99733341754c8ffeb3d7f445b15fe6
Mator <why101@msn.com>
2016-07-19 13:44:26 -0700

    added associations/json formatting for mod list groups

--- mod-picker/app/models/mod_list_custom_plugin.rb
+++ mod-picker/app/models/mod_list_custom_plugin.rb
@@ -10,6 +10,18 @@ class ModListCustomPlugin < ActiveRecord::Base
   after_create :increment_counters
   before_destroy :decrement_counters
 
+  def as_json(options={})
+    if JsonHelpers.json_options_empty(options)
+      # TODO: Revise this as necessary
+      default_options = {
+          :only => [:index, :group_id, :filename, :active, :description]
+      }
+      super(options.merge(default_options))
+    else
+      super(options)
+    end
+  end
+
   private
     def increment_counters
       self.mod_list.update_counter(:custom_plugins_count, 1)
