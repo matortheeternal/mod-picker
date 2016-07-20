f0f87c5fa82b1aa1a428641f4b8da0a083925f84
Mator <why101@msn.com>
2016-07-20 00:28:17 -0700

    updated modListToolsController to build a view model with group/tool item nesting so we can handle it properly using the dnd-lists directive

--- mod-picker/app/assets/javascripts/angularAssets/Views/modList/modListTools.js
+++ mod-picker/app/assets/javascripts/angularAssets/Views/modList/modListTools.js
@@ -1,12 +1,33 @@
 app.controller('modListToolsController', function($scope, $state, $stateParams, modListService) {
+    $scope.buildToolsModel = function(tools, groups) {
+        $scope.model.tools = [];
+        groups.forEach(function(group) {
+            if (group.tab !== 'tools') {
+                return;
+            }
+            $scope.model.tools.push(group);
+            group.children = tools.filter(function(tool) {
+                return tool.group_id == group.id;
+            });
+        });
+        tools.forEach(function(tool) {
+            if (!tool.group_id) {
+                $scope.model.tools.push(tool);
+            }
+        });
+    };
+
     $scope.retrieveTools = function() {
         $scope.retrieving.tools = true;
         modListService.retrieveModListTools($scope.mod_list.id).then(function(data) {
-            $scope.mod_list.tools = data;
-            $scope.retrieving.tools = false;
+            $scope.buildToolsModel(data.tools, data.groups);
+            $scope.mod_list.tools = data.tools;
+            $scope.mod_list.groups = Array.prototype.concat($scope.mod_list.groups || [], data.groups);
+            $scope.originalModList.tools = angular.copy($scope.mod_list.tools);
+            $scope.originalModList.groups = angular.copy($scope.mod_list.groups);
             // TODO: Retrieve this from the backend
             $scope.mod_list.missing_tools = [];
-            $scope.originalModList.tools = angular.copy($scope.mod_list.tools);
+            $scope.retrieving.tools = false;
         }, function(response) {
             $scope.errors.tools = response;
         });
