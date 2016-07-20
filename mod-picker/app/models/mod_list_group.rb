06bd3abbc3620c4cbf4f40319c30dcc8541ebcce
Mator <why101@msn.com>
2016-07-20 01:02:01 -0700

    set up backend to accept attr_writing for mod list group children

--- mod-picker/app/models/mod_list_group.rb
+++ mod-picker/app/models/mod_list_group.rb
@@ -1,10 +1,26 @@
 class ModListGroup < ActiveRecord::Base
   enum tab: [:tools, :mods, :plugins]
 
+  attr_writer :children
+
   belongs_to :mod_list, :inverse_of => 'mod_list_groups'
 
   # Validations
   validates :mod_list_id, :name, presence: true
+
+  # Callbacks
+  after_create :link_children
+
+  def link_children
+    if @children
+      children_ids = @children.map { |c| c[:id] }
+      if self.tab == :tools || self.tab == :mods
+        ModListMod.where(id: children_ids).update_all(group_id: self.id)
+      elsif self.tab == :plugins
+        ModListPlugin.where(id: children_ids).update_all(group_id: self.id)
+      end
+    end
+  end
 
   def as_json(options={})
     if JsonHelpers.json_options_empty(options)
