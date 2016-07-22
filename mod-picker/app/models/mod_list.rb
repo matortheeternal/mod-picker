efa453934b724b2be986266becadc8fa14b8e3f0
Mator <why101@msn.com>
2016-07-22 16:35:47 -0700

    now not rendering compatibility, install order, or load order notes for mod lists which are hidden or unapproved

--- mod-picker/app/models/mod_list.rb
+++ mod-picker/app/models/mod_list.rb
@@ -107,21 +107,21 @@ class ModList < ActiveRecord::Base
     mod_ids = self.mod_list_mod_ids
     return [] if mod_ids.empty?
 
-    CompatibilityNote.where(first_mod_id: mod_ids, second_mod_id: mod_ids)
+    CompatibilityNote.visible.where(first_mod_id: mod_ids, second_mod_id: mod_ids)
   end
 
   def install_order_notes
     mod_ids = self.mod_list_mod_ids
     return [] if mod_ids.empty?
 
-    InstallOrderNote.where(first_mod_id: mod_ids, second_mod_id: mod_ids)
+    InstallOrderNote.visible.where(first_mod_id: mod_ids, second_mod_id: mod_ids)
   end
 
   def load_order_notes
     plugin_ids = self.mod_list_plugin_ids
     return [] if plugin_ids.empty?
 
-    LoadOrderNote.where(first_plugin_id: plugin_ids, second_plugin_id: plugin_ids)
+    LoadOrderNote.visible.where(first_plugin_id: plugin_ids, second_plugin_id: plugin_ids)
   end
 
   def required_tools
