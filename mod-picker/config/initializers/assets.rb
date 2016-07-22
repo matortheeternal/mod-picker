f6a5f29ae47c0fdf04c5e47ef2482f447ec0e738
Mator <why101@msn.com>
2016-07-21 18:31:56 -0700

    added sortable vendor asset

--- mod-picker/config/initializers/assets.rb
+++ mod-picker/config/initializers/assets.rb
@@ -37,6 +37,8 @@ Rails.application.config.assets.precompile += [
 'sticky.min.js',
 'bootstrap.min.js',
 'rzslider.min.js',
+'sortable.min.js',
+'ng-sortable.min.js',
 'heartcode-canvasloader-min.js',
 'jquery.countdown.min.js',
 'jquery.nicescroll.min.js',
