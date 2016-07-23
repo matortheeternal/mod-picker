253b75f7d16300de8445ffff1b7f9299c7916f5a
Mator <why101@msn.com>
2016-07-23 14:59:26 -0700

    added resolution helper functions to installOrderNoteController

--- mod-picker/app/assets/javascripts/angularAssets/Directives/contributions/installOrderNote.js
+++ mod-picker/app/assets/javascripts/angularAssets/Directives/contributions/installOrderNote.js
@@ -20,4 +20,23 @@ app.controller('installOrderNoteController', function ($scope) {
     // set defaults
     $scope.showUserColumn = angular.isDefined($scope.showUserColumn) ? $scope.showUserColumn : true;
     $scope.showActions = angular.isDefined($scope.showActions) ? $scope.showActions : true;
+
+    $scope.getResolutionStatus = function() {
+        if ($scope.ignored) {
+            return "Ignored";
+        } else if ($scope.resolved) {
+            return "Resolved";
+        } else {
+            return "Unresolved";
+        }
+    };
+
+    $scope.resolve = function(action, index) {
+        var options = {
+            note: $scope.note,
+            action: action,
+            index: index
+        };
+        $scope.$emit('resolveInstallOrderNote', options);
+    };
 });
