f9f10a1d2e7de1525f90dc6ed1a2db8069733fc4
Mator <why101@msn.com>
2016-07-24 17:20:44 -0700

    created a new polyfill Array.prototype.unite which combines two arrays and stores the result in the first array

--- mod-picker/app/assets/javascripts/polyfills.js
+++ mod-picker/app/assets/javascripts/polyfills.js
@@ -15,3 +15,12 @@ String.prototype.toDashFormat = function() {
         return "-" + uppercaseLetter.toLowerCase();
     })
 };
+
+// concatenates array1 with array2 and assigns the result to a1
+// does nothing if either array1 or array2 are undefined
+Array.prototype.unite = function(array1, array2) {
+    if (array1 && array2) {
+        //noinspection JSUnusedAssignment
+        array1 = Array.prototype.concat(array1, array2);
+    }
+};
