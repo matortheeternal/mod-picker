9bbbfb4585a66238050a3f4f32a6f7509effe71f
Sirius <thesiriusadam@gmail.com>
2016-07-24 23:59:03 -0500

    replaced ui-state with functions in the tabs directive
    this is so it can check to see if there is a sticky state that matches the tab, and automatically populate a parameter object for that state

--- mod-picker/app/assets/javascripts/angularAssets/Directives/tabs.js
+++ mod-picker/app/assets/javascripts/angularAssets/Directives/tabs.js
@@ -6,8 +6,48 @@ app.directive('tabs', function() {
     return {
         restrict: 'E',
         templateUrl: '/resources/directives/tabs.html',
+        controller: 'tabsController',
         scope: {
             tabs: '='
+        }
+    };
+});
+app.controller('tabsController', function($scope, $state, $stickyState) {
+    $scope.isCurrentTab = function(tabName) {
+        if ($state.includes('**.' + tabName)) {
+            return 'selected-tab';
+        } else {
+            return '';
+        }
+    };
+
+    $scope.selectTab = function(tabName) {
+        if ($state.includes('**.' + tabName)) {
+            return;
+        }
+
+        var loadedTabState = $stickyState.getInactiveStates().find(function(state) {
+            //an array of every state on the tree leading to this one, in order of parentage
+            var stateArray = state.toString().split(".");
+            return stateArray.find(function(stateName) {
+                return tabName === stateName;
+            });
+        });
+
+        if (loadedTabState) {
+            var params = {};
+            var setParams = loadedTabState.ownParams;
+            for (var paramName in setParams) {
+                if (!paramName.startsWith('$$') && !paramName.startsWith('_')) {
+                    var value = setParams[paramName].value();
+                    if (value) {
+                        params[paramName] = value;
+                    }
+                }
+            }
+            $state.go('^.' + tabName, params);
+        } else {
+            $state.go('^.' + tabName);
         }
     };
 });
