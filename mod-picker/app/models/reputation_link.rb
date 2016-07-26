10a378c0c3368fa9fbd18a2336b5a629d8e40382
Sirius <thesiriusadam@gmail.com>
2016-07-26 14:01:41 -0500

    from_links and to_links were swapped
    from_links should be reputation_links that are directed at that user, so ReputationLink.to_rep is the inverse of UserReputation.from_links

--- mod-picker/app/models/reputation_link.rb
+++ mod-picker/app/models/reputation_link.rb
@@ -1,8 +1,8 @@
 class ReputationLink < ActiveRecord::Base
   self.primary_keys = :from_rep_id, :to_rep_id
 
-  belongs_to :from_rep, :class_name => 'UserReputation', :inverse_of => 'from_links'
-  belongs_to :to_rep, :class_name => 'UserReputation', :inverse_of => 'to_links'
+  belongs_to :to_rep, :class_name => 'UserReputation', :inverse_of => 'from_links'
+  belongs_to :from_rep, :class_name => 'UserReputation', :inverse_of => 'to_links'
 
   # Validations
   validates :from_rep_id, :to_rep_id, presence: true
--- mod-picker/app/models/user_reputation.rb
+++ mod-picker/app/models/user_reputation.rb
@@ -5,8 +5,8 @@ class UserReputation < ActiveRecord::Base
 
   belongs_to :user
 
-  has_many :from_links, :class_name => 'ReputationLink', :inverse_of => 'from_rep'
-  has_many :to_links, :class_name => 'ReputationLink', :inverse_of => 'to_rep'
+  has_many :from_links, :class_name => 'ReputationLink', :inverse_of => 'to_rep', :dependent => :destroy
+  has_many :to_links, :class_name => 'ReputationLink', :inverse_of => 'from_rep', :dependent => :destroy
 
   # Validations
   validates :user_id, presence: true
