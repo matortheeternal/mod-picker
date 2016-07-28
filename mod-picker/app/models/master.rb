1a7494e772cd8573c031b0e51aac1947bfba55b6
Mator <why101@msn.com>
2016-07-27 23:26:28 -0700

    added plugins scope to Master model

--- mod-picker/app/models/master.rb
+++ mod-picker/app/models/master.rb
@@ -1,6 +1,8 @@
 class Master < ActiveRecord::Base
   self.primary_keys = :plugin_id, :master_plugin_id
 
+  scope :plugins, -> (plugin_ids) { where(plugin_id: plugin_ids) }
+
   belongs_to :plugin, :inverse_of => 'masters'
   belongs_to :master_plugin, :class_name => 'Plugin', :foreign_key => 'master_plugin_id', :inverse_of => 'children'
 
