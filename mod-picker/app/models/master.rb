3e877341f5f27762d5baf105b6272af55bfd5d04
Mator <why101@msn.com>
2016-07-27 23:26:40 -0700

    added default json formatting to Master model

--- mod-picker/app/models/master.rb
+++ mod-picker/app/models/master.rb
@@ -8,4 +8,23 @@ class Master < ActiveRecord::Base
 
   # Validations
   validates :plugin_id, :master_plugin_id, :index, presence: true
+
+  def as_json(options={})
+    if JsonHelpers.json_options_empty(options)
+      default_options = {
+          :only => [],
+          :include => {
+              :plugin => {
+                  :only => [:id, :mod_id, :filename]
+              },
+              :master_plugin => {
+                  :only => [:id, :mod_id, :filename]
+              }
+          }
+      }
+      super(options.merge(default_options))
+    else
+      super(options)
+    end
+  end
 end
