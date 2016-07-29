a9fb7fe9b87db3f2954a0d463c74e4ff70b86700
Sirius <thesiriusadam@gmail.com>
2016-07-29 15:03:51 -0500

    Revert "added generic retrieval of comments"
    This reverts commit 2a1c86fd9e3cedcb77adfa90f46697f3a1e1da59.

--- mod-picker/app/assets/javascripts/angularAssets/Directives/contributions/comments/comments.js
+++ mod-picker/app/assets/javascripts/angularAssets/Directives/contributions/comments/comments.js
@@ -6,6 +6,7 @@ app.directive('comments', function () {
         scope: {
             comments: '=',
             pageData: '=',
+            retrievalCallback: '=',
             currentUser: '=',
             objectType: '=',
             object: '=',
@@ -13,24 +14,7 @@ app.directive('comments', function () {
     };
 });
 
-app.controller('commentsController', function ($scope, commentService) {
-    $scope.retrievalCallback = function(page) {
-        // TODO: Make options dynamic
-        var options = {
-            sort: {
-                column: 'submitted',
-                direction: 'desc'
-            },
-            page: page || 1
-        };
-        commentService.retrieveComments($scope.object.id, $scope.objectType.toLowerCase(), options, $scope.pageData).then(function(data) {
-            $scope.comments = data;
-        }, function(response) {
-            var params = { label: 'Error retrieving comments', response: response };
-            $scope.$emit('errorMessage', params);
-        });
-    };
-
+app.controller('commentsController', function ($scope, contributionService) {
     $scope.startNewComment = function() {
         $scope.$broadcast('startNewComment');
     };
