a653ea393ca36f094792ae03279207a2b68b62ea
Mator <why101@msn.com>
2016-07-30 14:58:51 -0700

    massively cleaned up the modListPluginsController - unneeded functions removed, now using $scope.$broadcast to communicate with child modules

--- mod-picker/app/assets/javascripts/angularAssets/Views/modList/modListPlugins.js
+++ mod-picker/app/assets/javascripts/angularAssets/Views/modList/modListPlugins.js
@@ -31,97 +31,6 @@ app.controller('modListPluginsController', function($scope, modListService, colu
         });
     };
 
-    $scope.buildUnresolvedPluginCompatibility = function() {
-        $scope.notes.unresolved_plugin_compatibility = [];
-        $scope.notes.ignored_plugin_compatibility = [];
-        $scope.notes.plugin_compatibility.forEach(function(note) {
-            // skip destroyed or ignored notes
-            if (note._destroy) {
-                return;
-            } else if (note.ignored) {
-                $scope.notes.ignored_plugin_compatibility.push(note);
-                return;
-            }
-            switch (note.status) {
-                case 'compatibility plugin':
-                    // unresolved if the compatibility plugin is not present and both mods are present
-                    if (!$scope.findPlugin(note.compatibility_plugin_id, true) &&
-                        $scope.findMod(note.mods[0].id, true) && $scope.findMod(note.mods[1].id, true)) {
-                        note.resolved = false;
-                        $scope.notes.unresolved_plugin_compatibility.push(note);
-                    } else {
-                        note.resolved = true;
-                    }
-                    break;
-                case 'make custom patch':
-                    // unresolved if the custom plugin is not present and both mods are present
-                    if (!$scope.findCustomPlugin(note.id) &&
-                        $scope.findMod(note.mods[0].id, true) && $scope.findMod(note.mods[1].id, true)) {
-                        note.resolved = false;
-                        $scope.notes.unresolved_plugin_compatibility.push(note);
-                    } else {
-                        note.resolved = true;
-                    }
-                    break;
-            }
-        });
-    };
-
-    $scope.buildUnresolvedLoadOrder = function() {
-        $scope.notes.unresolved_load_order = [];
-        $scope.notes.ignored_load_order = [];
-        $scope.notes.load_order.forEach(function(note) {
-            // skip destroyed or ignored notes
-            if (note._destroy) {
-                return;
-            } else if (note.ignored) {
-                $scope.notes.ignored_load_order.push(note);
-                return;
-            }
-            var first_plugin = $scope.findPlugin(note.plugins[0].id, true);
-            var second_plugin = $scope.findPlugin(note.plugins[1].id, true);
-            // unresolved if the both mods are present and the first mod comes after the second mod
-            if (first_plugin && second_plugin && first_plugin.index > second_plugin.index) {
-                note.resolved = false;
-                $scope.notes.unresolved_load_order.push(note);
-            } else {
-                note.resolved = true;
-            }
-        });
-    };
-    
-    $scope.compactRequiredPlugins = function() {
-        var requirements = $scope.required.plugins;
-        var req, prev;
-        for (var i = requirements.length - 1; i >= 0; i--) {
-            req = requirements[i];
-            if (prev && req.master_plugin.id == prev.master_plugin.id) {
-                prev.plugins.unshift(req.plugin);
-                requirements.splice(i, 1);
-            } else {
-                req.plugins = [req.plugin];
-                req.destroyed_plugins = [];
-                delete req.plugin;
-                prev = req;
-            }
-        }
-    };
-
-    $scope.buildMissingPlugins = function() {
-        $scope.required.missing_plugins = [];
-        $scope.required.plugins.forEach(function(requirement) {
-            // skip destroyed requirements
-            if (requirement._destroy) {
-                return;
-            }
-            var pluginPresent = $scope.findPlugin(requirement.master_plugin.id, true);
-            // TODO: Check if one of the plugins in the plugins array is present
-            if (!pluginPresent) {
-                $scope.required.missing_plugins.push(requirement);
-            }
-        });
-    };
-
     $scope.retrievePlugins = function() {
         $scope.retrieving.plugins = true;
         modListService.retrieveModListPlugins($scope.mod_list.id).then(function(data) {
@@ -134,10 +43,7 @@ app.controller('modListPluginsController', function($scope, modListService, colu
             $scope.originalModList.plugins = angular.copy($scope.mod_list.plugins);
             $scope.originalModList.groups = angular.copy($scope.mod_list.groups);
             $scope.buildPluginsModel();
-            $scope.compactRequiredPlugins();
-            $scope.buildMissingPlugins();
-            $scope.buildUnresolvedPluginCompatibility();
-            $scope.buildUnresolvedLoadOrder();
+            $scope.$broadcast('initializeModules');
             $scope.retrieving.plugins = false;
         }, function(response) {
             $scope.errors.plugins = response;
@@ -149,30 +55,20 @@ app.controller('modListPluginsController', function($scope, modListService, colu
         $scope.retrievePlugins();
     }
     
-    // requirement handling
-    $scope.getRequirerList = function(requirement, startIndex) {
-        return requirement.plugins.slice(startIndex).map(function(plugin) {
-            return plugin.filename;
-        }).join(', ');
-    };
-    
-    $scope.removeRequirers = function(requirement) {
-        // TODO
-    };
-    
     // plugin handling
-    $scope.reAddPlugin = function(modListPlugin) {
+    $scope.recoverPlugin = function(modListPlugin) {
         // if plugin is already present on the user's mod list but has been
         // removed, add it back
         if (modListPlugin._destroy) {
             delete modListPlugin._destroy;
             $scope.mod_list.plugins_count += 1;
-            $scope.reAddPluginRequirements(modListPlugin.plugin.id);
-            $scope.buildMissingPlugins();
-            $scope.buildUnresolvedCompatibility();
-            $scope.buildUnresolvedInstallOrder();
             $scope.updateTabs();
+
+            // upudate modules
+            $scope.$broadcast('pluginRecovered', modListPlugin.plugin.id);
             $scope.$broadcast('updateItems');
+
+            // success message
             $scope.$emit('successMessage', 'Added plugin ' + modListPlugin.plugin.filename+ ' successfully.');
         }
         // else inform the user that the plugin is already on their mod list
@@ -190,23 +86,17 @@ app.controller('modListPluginsController', function($scope, modListService, colu
         };
 
         modListService.newModListPlugin(mod_list_plugin).then(function(data) {
-            // push mod onto view
+            // push plugin onto view
             $scope.mod_list.plugins.push(data.mod_list_plugin);
             $scope.model.plugins.push(data.mod_list_plugin);
             $scope.mod_list.plugins_count += 1;
             $scope.updateTabs();
 
-            // handle requirements
-            Array.prototype.unite($scope.required.plugins, data.required_plugins);
-            $scope.buildMissingPlugins();
-
-            // handle notes
-            Array.prototype.unite($scope.notes.compatibility, data.compatibility_notes);
-            Array.prototype.unite($scope.notes.load_order, data.load_order);
-            $scope.$emit('rebuildUnresolved');
+            // update modules
+            $scope.$broadcast('pluginAdded', data);
+            $scope.$broadcast('updateItems');
 
             // success message
-            $scope.$broadcast('updateItems');
             var filename = data.mod_list_plugin.plugin.filename;
             $scope.$emit('successMessage', 'Added plugin ' + filename + ' successfully.');
         }, function(response) {
@@ -224,7 +114,7 @@ app.controller('modListPluginsController', function($scope, modListService, colu
         // see if the plugin is already present on the user's plugin list
         var existingPlugin = $scope.findPlugin(pluginId);
         if (existingPlugin) {
-            $scope.reAddPlugin(existingPlugin);
+            $scope.recoverPlugin(existingPlugin);
         } else {
             $scope.addNewPlugin(pluginId);
         }
@@ -237,13 +127,9 @@ app.controller('modListPluginsController', function($scope, modListService, colu
 
     $scope.removePlugin = function(modListPlugin) {
         modListPlugin._destroy = true;
-        $scope.removeRequirements(modListPlugin.plugin.id);
-        $scope.removeNotes(modListPlugin.plugin.id);
-        $scope.buildMissingPlugins();
-        $scope.buildUnresolvedPluginCompatibility();
-        $scope.buildUnresolvedLoadOrder();
         $scope.mod_list.plugins_count -= 1;
         $scope.updateTabs();
+        $scope.$broadcast('pluginRemoved', modListPlugin.plugin.id);
         $scope.$broadcast('updateItems');
     };
 
@@ -257,44 +143,9 @@ app.controller('modListPluginsController', function($scope, modListService, colu
         $scope.removePlugin(modListPlugin);
     });
 
-    $scope.$on('resolveCompatibilityNote', function(event, options) {
-        switch(options.action) {
-            case "add plugin":
-                $scope.addPlugin(options.note.compatibility_plugin.id);
-                break;
-            case "add custom plugin":
-                $scope.addCustomPlugin(options.note.id);
-                break;
-            case "ignore":
-                options.note.ignored = !options.note.ignored;
-                // TODO: Update $scope.plugin_list.ignored_notes
-                $scope.buildUnresolvedPluginCompatibility();
-                break;
-        }
-    });
-
-    $scope.$on('resolveLoadOrderNote', function(event, options) {
-        switch(options.action) {
-            case "move":
-                var moveOptions = {
-                    moveId: options.note.plugins[options.index].id,
-                    destId: options.note.plugins[+!options.index].id,
-                    after: !!options.index
-                };
-                $scope.$broadcast('moveItem', moveOptions);
-                break;
-            case "ignore":
-                options.note.ignored = !options.note.ignored;
-                // TODO: Update $scope.plugin_list.ignored_notes
-                $scope.buildUnresolvedLoadOrder();
-                break;
-        }
-    });
-
     // direct method trigger events
     $scope.$on('rebuildModels', $scope.buildPluginsModel);
-    $scope.$on('rebuildMissingPlugins', $scope.buildMissingPlugins);
-    $scope.$on('rebuildUnresolvedPluginCompatibility', $scope.buildUnresolvedPluginCompatibility);
-    $scope.$on('rebuildUnresolvedLoadOrder', $scope.buildUnresolvedLoadOrder);
-    $scope.$on('itemMoved', $scope.buildUnresolvedLoadOrder);
+    $scope.$on('itemMoved', function() {
+        $scope.$broadcast('rebuildUnresolvedLoadOrder');
+    });
 });
