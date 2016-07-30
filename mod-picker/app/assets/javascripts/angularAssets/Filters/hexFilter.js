8bcca42f34a9fadc4cf476619083c17ae464dc17
Mator <why101@msn.com>
2016-07-29 23:02:41 -0700

    hex filter now properly assigns default padding when input padding is undefined

--- mod-picker/app/assets/javascripts/angularAssets/Filters/hexFilter.js
+++ mod-picker/app/assets/javascripts/angularAssets/Filters/hexFilter.js
@@ -1,7 +1,8 @@
 app.filter('hex', function() {
     return function(number, padding) {
         // set up default padding
-        if (typeof padding !== 'undefined') padding = 2;
+        if (typeof padding === 'undefined') padding = 2;
+
         // convert number to hex
         var hex = Number(number).toString(16).toUpperCase();
         // add 0 padding as necessary
