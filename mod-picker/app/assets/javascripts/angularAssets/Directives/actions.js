39ca102653589d17851f828af74ea18566f1723d
Mator <why101@msn.com>
2016-07-29 17:49:02 -0700

    added actionsController with dropdown handling functions for actions directive

--- mod-picker/app/assets/javascripts/angularAssets/Directives/actions.js
+++ mod-picker/app/assets/javascripts/angularAssets/Directives/actions.js
@@ -2,6 +2,19 @@ app.directive('actions', function () {
     return {
         restrict: 'E',
         templateUrl: '/resources/directives/actions.html',
+        controller: 'actionsController',
         scope: false
     }
+});
+
+app.controller('actionsController', function($scope, $timeout) {
+    $scope.toggleDropdown = function(item, action) {
+        item[action.key] = !item[action.key];
+    };
+
+    $scope.blurDropdown = function(item, action) {
+        $timeout(function() {
+            item[action.key] = false;
+        }, 100);
+    };
 });
