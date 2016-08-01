ffca72a11de47bf8f6e225c7058cabb727620548
Mator <why101@msn.com>
2016-08-01 13:41:38 -0700

    created mod details modal directive and controller

---
+++ mod-picker/app/assets/javascripts/angularAssets/Directives/modDetailsModal.js
@@ -0,0 +1,28 @@
+app.directive('modDetailsModal', function () {
+    return {
+        restrict: 'E',
+        templateUrl: '/resources/directives/modDetailsModal.html',
+        controller: 'modDetailsModalController',
+        scope: false
+    };
+});
+
+app.controller('modDetailsModalController', function ($scope, modService, errorService) {
+    // display error messages
+    $scope.$on('modalErrorMessage', function(event, params) {
+        var errors = errorService.errorMessages(params.label, params.response);
+        errors.forEach(function(error) {
+            $scope.$broadcast('modalMessage', error);
+        });
+        // stop event propagation - we handled it
+        event.stopPropagation();
+    });
+
+    // display success message
+    $scope.$on('modalSuccessMessage', function(event, text) {
+        var successMessage = {type: "success", text: text};
+        $scope.$broadcast('modalMessage', successMessage);
+        // stop event propagation - we handled it
+        event.stopPropagation();
+    });
+});
