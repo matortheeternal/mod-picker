00550280955ff90e52ab76c8b56ad764f8999272
Mator <why101@msn.com>
2016-07-31 18:22:26 -0700

    created ModListIgnoredNote model

---
+++ mod-picker/app/models/mod_list_ignored_note.rb
@@ -0,0 +1,33 @@
+class ModListIgnoredNote < ActiveRecord::Base
+  belongs_to :mod_list, :inverse_of => 'mod_list_ignored_notes'
+  belongs_to :note, :polymorphic => true
+
+  # Validations
+  validates :mod_list_id, :note_id, :note_type, presence: true
+
+  # Callbacks
+  after_create :increment_counters
+  before_destroy :decrement_counters
+
+  def as_json(options={})
+    if JsonHelpers.json_options_empty(options)
+      default_options = {
+          :except => [:mod_list_id]
+      }
+      super(options.merge(default_options))
+    else
+      super(options)
+    end
+  end
+  
+  # Private Methods
+  private
+    # counter caches
+    def increment_counters
+      self.mod_list.update_counter(:ignored_notes_count, 1)
+    end
+
+    def decrement_counters
+      self.mod_list.update_counter(:ignored_notes_count, -1)
+    end
+end
