293cacbb1b7075337174cfc017e1005f0ba6253c
Mator <why101@msn.com>
2016-08-01 19:30:24 -0700

    added mods scope to ModAssetFile model

--- mod-picker/app/models/mod_asset_file.rb
+++ mod-picker/app/models/mod_asset_file.rb
@@ -1,6 +1,10 @@
 class ModAssetFile < ActiveRecord::Base
   self.primary_keys = :mod_id, :asset_file_id
 
+  # Scopes
+  scope :mods, -> (mod_ids) { where(mod_id: mod_ids) }
+
+  # Associations
   belongs_to :mod, :inverse_of => 'mod_asset_files'
   belongs_to :asset_file, :inverse_of => 'mod_asset_files'
 
