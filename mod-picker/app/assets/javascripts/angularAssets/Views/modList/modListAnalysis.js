6fc45d65b159beae5f518d4af58acb70f0eba25a
Mator <why101@msn.com>
2016-08-03 15:45:05 -0700

    added function to build conflicting overrides to modListAnalysisController

--- mod-picker/app/assets/javascripts/angularAssets/Views/modList/modListAnalysis.js
+++ mod-picker/app/assets/javascripts/angularAssets/Views/modList/modListAnalysis.js
@@ -1,7 +1,31 @@
-app.controller('modListAnalysisController', function($scope, modListService) {
+app.controller('modListAnalysisController', function($scope, modListService, pluginService) {
+    $scope.buildConflictingOverrides = function() {
+        $scope.analysis.conflicting_overrides = [];
+        $scope.analysis.load_order_overrides.forEach(function(override) {
+            if (override.plugin_ids.length > 1) {
+                $scope.analysis.conflicting_overrides.push(angular.copy(override));
+            }
+        });
+        $scope.analysis.conflicting_overrides.forEach(function(override) {
+            // associate master plugins
+            override.master_plugin = $scope.analysis.load_order.find(function(item) {
+                return override.fid >>> 24 == item.index;
+            });
+            // associate plugins and mods
+            override.plugins = override.plugin_ids.map(function(plugin_id) {
+                return pluginService.getLoadOrderPlugin($scope.analysis.load_order, plugin_id);
+            });
+            // sort plugins
+            override.plugins.sort(function(firstPlugin, secondPlugin) {
+                return firstPlugin.index - secondPlugin.index;
+            });
+        });
+    };
+
     $scope.retrieveAnalysis = function() {
         modListService.retrieveModListAnalysis($scope.mod_list.id).then(function(data) {
-            $scope.mod_list.analysis = data;
+            $scope.analysis = data;
+            $scope.buildConflictingOverrides();
             $scope.analysisReady = true;
         }, function(response) {
             $scope.errors.analysis = response;
