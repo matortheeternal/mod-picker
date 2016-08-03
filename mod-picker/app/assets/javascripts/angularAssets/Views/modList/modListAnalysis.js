b1ef7af11188e0cf174b93955db9ec60f8c04332
Mator <why101@msn.com>
2016-08-02 22:47:48 -0700

    created basic modListAnalysisController to retrieve the mod list analysis when visiting the analysis tab

--- mod-picker/app/assets/javascripts/angularAssets/Views/modList/modListAnalysis.js
+++ mod-picker/app/assets/javascripts/angularAssets/Views/modList/modListAnalysis.js
@@ -1,3 +1,13 @@
-app.controller('modListAnalysisController', function($scope) {
+app.controller('modListAnalysisController', function($scope, modListService) {
+    $scope.retrieveAnalysis = function() {
+        modListService.retrieveModListAnalysis($scope.mod_list.id).then(function(data) {
+            $scope.mod_list.analysis = data;
+            $scope.analysisReady = true;
+        }, function(response) {
+            $scope.errors.analysis = response;
+        });
+    };
 
+    // retrieve analysis when the state is first loaded
+    $scope.retrieveAnalysis();
 });
