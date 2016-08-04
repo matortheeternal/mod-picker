e4398af75e183204aef662b7e9887d7100f3b11c
Mator <why101@msn.com>
2016-08-04 15:32:18 -0700

    added association between mods and config files

--- mod-picker/app/models/config_file.rb
+++ mod-picker/app/models/config_file.rb
@@ -2,8 +2,9 @@ class ConfigFile < ActiveRecord::Base
   include RecordEnhancements
 
   belongs_to :game, :inverse_of => 'config_files'
+  belongs_to :mod, :inverse_of => 'config_files'
   has_many :mod_list_config_files, :inverse_of => 'config_file'
 
   # Validations
-  validates :game_id, :filename, :install_path, presence: true
+  validates :game_id, :mod_id, :filename, :install_path, presence: true
 end
