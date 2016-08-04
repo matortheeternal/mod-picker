3546c9fcc8bdf484ab7136f55b56301f68f45d9a
Mator <why101@msn.com>
2016-08-04 12:43:13 -0700

    fixed mod list mod having a validation for the now non-existent active field

--- mod-picker/app/models/mod_list_mod.rb
+++ mod-picker/app/models/mod_list_mod.rb
@@ -11,7 +11,6 @@ class ModListMod < ActiveRecord::Base
 
   # Validations
   validates :mod_list_id, :mod_id, :index, presence: true
-  validates :active, inclusion: [true, false]
   # can only have a mod on a given mod list once
   # TODO: If we don't allow the user to change the mod_id with nested attributes we could refactor this validation to be an after_create callback
   validates :mod_id, uniqueness: { scope: :mod_list_id, :message => "The mod is already present on the mod list." }
