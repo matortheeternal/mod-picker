90f09a38fff3dc82a0e7c99894bd0ae75e699ada
Mator <why101@msn.com>
2016-08-04 23:56:41 -0700

    added config file retrieval to modListConfigController

--- mod-picker/app/assets/javascripts/angularAssets/Views/modList/modListConfig.js
+++ mod-picker/app/assets/javascripts/angularAssets/Views/modList/modListConfig.js
@@ -1,3 +1,23 @@
-app.controller('modListConfigController', function($scope) {
+app.controller('modListConfigController', function($scope, $q, modListService, configFilesService) {
 
+    $scope.buildConfigModel = function() {
+        $scope.model.configs = configFilesService.groupConfigFiles($scope.mod_list.config_files);
+    };
+
+    $scope.retrieveConfig = function() {
+        modListService.retrieveModListConfigFiles($scope.mod_list.id).then(function(data) {
+            $scope.mod_list.config_files = data.config_files;
+            $scope.mod_list.custom_config_files = data.custom_config_files;
+            $scope.configs_store = data.config_files_store;
+            $scope.originalModList.config_files = angular.copy($scope.mod_list.config_files);
+            $scope.originalModList.custom_config_files = angular.copy($scope.mod_list.custom_config_files);
+            $scope.buildConfigModel();
+            $scope.configReady = true;
+        }, function(response) {
+            $scope.errors.config = response;
+        });
+    };
+
+    // retrieve config when the state is first loaded
+    $scope.retrieveConfig();
 });
