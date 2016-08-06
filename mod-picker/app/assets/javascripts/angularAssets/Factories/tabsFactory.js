a29ac0f7ec140c1795fad5911ca6b6155bb110df
Mator <why101@msn.com>
2016-08-05 23:50:27 -0700

    added updateModListTabs to tabsFactory

--- mod-picker/app/assets/javascripts/angularAssets/Factories/tabsFactory.js
+++ mod-picker/app/assets/javascripts/angularAssets/Factories/tabsFactory.js
@@ -75,5 +75,27 @@ app.factory("tabsFactory", function() {
         return tabs;
     };
 
+    this.updateModListTabs = function(modList, tabs) {
+        tabs.forEach(function(tab) {
+            switch(tab.name) {
+                case 'Tools':
+                    tab.count = modList.tools_count + modList.custom_tools_count;
+                    break;
+                case 'Mods':
+                    tab.count = modList.mods_count + modList.custom_mods_count;
+                    break;
+                case 'Plugins':
+                    tab.count = modList.plugins_count + modList.custom_plugins_count;
+                    break;
+                case 'Config':
+                    tab.count = modList.config_files_count + modList.custom_config_files_count;
+                    break;
+                case 'Comments':
+                    tab.count = modList.comments_count;
+                    break;
+            }
+        });
+    };
+
     return factory;
 });
