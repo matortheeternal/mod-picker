9d215646598036d86911f50b489bb201881a8870
Mator <why101@msn.com>
2016-08-11 19:19:58 -0700

    tableUtils.columnAvailable now returns true if the dataType is string OR function

--- mod-picker/app/assets/javascripts/angularAssets/Services/tableUtils.js
+++ mod-picker/app/assets/javascripts/angularAssets/Services/tableUtils.js
@@ -71,9 +71,10 @@ app.service('tableUtils', function (objectUtils) {
     // whether or not a column should be visible to the user
     this.columnAvailable = function(availableColumnData, column) {
         var data = column.data;
-        // if the data property of the column is a raw string, thus column is
-        // by definition always present, and should always be available.
-        if (typeof data === 'string' || data instanceof String) {
+        var dataType = typeof data;
+        // if the data property of the column is a string or a function the
+        // column should always be present
+        if (dataType === 'string' || dataType === 'function' || data instanceof String) {
             return true;
         }
         // else we need to check if one of the data property keys is contained
