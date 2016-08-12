1649029b5a199e68ede1ef5a8dd4149779f753c1
Mator <why101@msn.com>
2016-08-11 19:14:13 -0700

    adjusted default mod list json formatting to exclude some unneeded fields

--- mod-picker/app/models/mod_list.rb
+++ mod-picker/app/models/mod_list.rb
@@ -318,15 +318,11 @@ class ModList < ActiveRecord::Base
   def as_json(options={})
     if JsonHelpers.json_options_empty(options)
       default_options = {
-          :except => [:submitted_by],
+          :except => [:game_id, :hidden, :submitted_by],
           :include => {
               :submitter => {
-                  :only => [:id, :username, :role, :title],
-                  :include => {
-                      :reputation => {:only => [:overall]}
-                  },
-                  :methods => :avatar
-              },
+                  :only => [:id, :username]
+              }
           }
       }
       super(options.merge(default_options))
