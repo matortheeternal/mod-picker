4c2d6d548d9a7b87f9b9db5ffaee216fb65c84ba
Mator <why101@msn.com>
2016-08-14 18:48:05 -0700

    now properly displaying emitted success and error messages on the user settings page

--- mod-picker/app/assets/javascripts/angularAssets/Views/userSettings/userSettings.js
+++ mod-picker/app/assets/javascripts/angularAssets/Views/userSettings/userSettings.js
@@ -78,6 +78,24 @@ app.controller('userSettingsController', function($scope, $q, userObject, curren
         { name: 'Mod Lists' },
         { name: 'Authored Mods' }
     ];
+    
+    // display error messages
+    $scope.$on('errorMessage', function(event, params) {
+        var errors = errorService.errorMessages(params.label, params.response, $scope.mod.id);
+        errors.forEach(function(error) {
+            $scope.$broadcast('message', error);
+        });
+        // stop event propagation - we handled it
+        event.stopPropagation();
+    });
+
+    // display success message
+    $scope.$on('successMessage', function(event, text) {
+        var successMessage = { type: "success", text: text };
+        $scope.$broadcast('message', successMessage);
+        // stop event propagation - we handled it
+        event.stopPropagation();
+    });
 
     /* settings submission */
     $scope.submit = function() {
