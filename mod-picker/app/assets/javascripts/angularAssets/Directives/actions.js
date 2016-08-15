d04debdf18b77b20fceb9281cdb6d0b46d20ad53
Mator <why101@msn.com>
2016-08-14 18:43:37 -0700

    massively simplified actions directive html template, added support for custom action classes

--- mod-picker/app/assets/javascripts/angularAssets/Directives/actions.js
+++ mod-picker/app/assets/javascripts/angularAssets/Directives/actions.js
@@ -8,15 +8,28 @@ app.directive('actions', function () {
 });
 
 app.controller('actionsController', function($scope, $timeout) {
-    $scope.toggleDropdown = function(item, action) {
+    $scope.toggleDropdown = function(action, item) {
         if (!$scope.resolve(action.disabled, item)) {
             item[action.key] = !item[action.key];
         }
     };
 
-    $scope.blurDropdown = function(item, action) {
+    $scope.blurDropdown = function(action, item) {
         $timeout(function() {
             item[action.key] = false;
         }, 100);
+    };
+    $scope.actionClick = function(action, item) {
+        if (action.items) {
+            $scope.toggleDropdown(action, item);
+        } else {
+            $scope.resolve(action.execute, item);
+        }
+    };
+
+    $scope.actionBlur = function(action, item) {
+        if (action.items) {
+            $scope.blurDropdown(action, item);
+        }
     };
 });
