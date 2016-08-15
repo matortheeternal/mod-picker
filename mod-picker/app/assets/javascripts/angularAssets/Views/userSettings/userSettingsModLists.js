7f91d4d322f99cf174ca63ae014953e4f61a2ec8
Mator <why101@msn.com>
2016-08-14 18:26:01 -0700

    added mod list and mod collection creation to the user settings mod lists tab

--- mod-picker/app/assets/javascripts/angularAssets/Views/userSettings/userSettingsModLists.js
+++ mod-picker/app/assets/javascripts/angularAssets/Views/userSettings/userSettingsModLists.js
@@ -27,4 +27,27 @@ app.controller('userSettingsModListsController', function($scope, columnsFactory
 
     //retrieve the mod lists when the state is first loaded
     $scope.retrieveModLists();
+
+    // CREATE A NEW MOD LIST
+    $scope.newModList = function(is_collection) {
+        var mod_list = {
+            game_id: window._current_game_id,
+            name: $scope.currentUser.username + "'s Mod List",
+            is_collection: is_collection,
+            // TODO: Should have a default description set on the backend
+            description: "A brand new mod list!"
+        };
+        var model = is_collection ? $scope.collections : $scope.mod_lists;
+        var label = is_collection ? 'mod collection' : 'mod list';
+        modListService.newModList(mod_list, false).then(function(data) {
+            model.push(data.mod_list);
+            $scope.$emit('successMessage', 'Created new ' + label + ' successfully.');
+        }, function(response) {
+            var params = {
+                label: 'Error creating new ' + label,
+                response: response
+            };
+            $scope.$emit('errorMessage', params);
+        });
+    }
 });
