1d3901737a7bbb11dfa26a21e2bee43b58676bd5
Mator <why101@msn.com>
2016-08-15 21:41:44 -0700

    cleaned up linkGenericAccount to be more generic :sunglasses:

--- mod-picker/app/assets/javascripts/angularAssets/Directives/userSettings/linkGenericAccount.js
+++ mod-picker/app/assets/javascripts/angularAssets/Directives/userSettings/linkGenericAccount.js
@@ -5,30 +5,41 @@ app.directive('linkGenericAccount', function () {
         controller: 'linkGenericAccountController',
         scope: {
             bio: '=',
-            siteLabel: '=',
-            siteUsername: '=',
-            sitePostsCount: '=',
-            siteDateJoined: '=',
-            siteSubmissionsCount: '=',
-            siteFollowersCount: '=',
-            siteVerificationToken: '='
+            siteLabel: '@',
+            siteKey: '@'
         }
     }
 });
 
-app.controller('linkGenericAccountController', function ($scope, $timeout, userSettingsService, sitesFactory) {
+app.controller('linkGenericAccountController', function ($scope, $timeout, userSettingsService, sitesFactory, formUtils) {
     // initialize variables
     $scope.showModal = false;
     $scope.waiting = false;
     $scope.verified = false;
     $scope.site = sitesFactory.getSite($scope.siteLabel);
+    $scope.linkSteps = sitesFactory.getLinkSteps($scope.siteLabel);
+
+    // extended initialization using generic keys
+    $scope.initSiteVar = function(dataKey) {
+        var fullKey = $scope.siteKey + '_' + dataKey;
+        if ($scope.bio[fullKey]) {
+            $scope[dataKey] = $scope.bio[fullKey];
+        }
+    };
+    $scope.initSiteVar('username');
+    $scope.initSiteVar('submissions_count');
+    $scope.initSiteVar('followers_count');
+    $scope.initSiteVar('date_joined');
+    $scope.initSiteVar('posts_count');
+    $scope.initSiteVar('verification_token');
 
     // inherited functions
     $scope.focusText = formUtils.focusText;
 
+    // normal functions
     $scope.toggleModal = function(visible) {
         $scope.$emit('toggleModal', visible);
-        $scope.showModal = !$scope.showModal;
+        $scope.showModal = visible;
     };
 
     $scope.validateUrl = function() {
@@ -48,14 +59,14 @@ app.controller('linkGenericAccountController', function ($scope, $timeout, userS
             return;
         }
 
-        $scope.errors = [];
         $scope.waiting = true;
         var user_path = match[$scope.site.userIndex];
         userSettingsService.verifyAccount($scope.siteLabel, user_path).then(function(data) {
             $scope.verified = data.verified;
             if (!data.verified) {
-                $scope.errors.push({ message: "Failed to verify account. You can try again in 30 seconds." });
+                $scope.$emit('modalMessage', { type: 'error', message: 'Failed to verify account. You can try again in 30 seconds.' });
             } else {
+                $scope.$emit('modalMessage', { type: 'success', message: 'Account verified!' });
                 $scope.bio = data.bio;
             }
         });
