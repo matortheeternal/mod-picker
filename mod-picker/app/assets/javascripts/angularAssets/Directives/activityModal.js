597892f166195d8bf4477a0bc0f552cd70763ba4
Mator <why101@msn.com>
2016-08-19 16:32:14 -0700

    created activity modal directive and html template

---
+++ mod-picker/app/assets/javascripts/angularAssets/Directives/activityModal.js
@@ -0,0 +1,7 @@
+app.directive('activityModal', function () {
+    return {
+        restrict: 'E',
+        templateUrl: '/resources/directives/activityModal.html',
+        scope: false
+    };
+});
