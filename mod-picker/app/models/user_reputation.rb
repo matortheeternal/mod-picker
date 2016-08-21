4cf0707cf874f4b5a612995b8143b649c953d938
Mator <why101@msn.com>
2016-08-21 09:37:55 -0700

    added calculate_site_rep method to UserReputation model

--- mod-picker/app/models/user_reputation.rb
+++ mod-picker/app/models/user_reputation.rb
@@ -10,4 +10,42 @@ class UserReputation < ActiveRecord::Base
 
   # Validations
   validates :user_id, presence: true
+
+  # CONSTANTS
+  # site reputation
+  MP_ACCOUNT_AGE_RATIO = 0.01
+  SITE_ACCOUNT_AGE_RATIO = 0.005
+  SITE_POST_RATIO = 0.004
+  WORKSHOP_SUBMISSIONS_RATIO = 0.1
+  WORKSHOP_FOLLOWERS_RATIO = 0.005
+  MAX_SITE_REP = 50
+
+  def calculate_site_rep
+    # MOD PICKER
+    mp_account_age = (Date.now - self.user.joined)
+    self.site_rep += mp_account_age * MP_ACCOUNT_AGE_RATIO
+
+    # NEXUS MODS
+    if self.user.bio.nexus_user_path.present?
+      nexus_account_age = (Date.now - self.user.bio.nexus_date_joined)
+      self.site_rep += nexus_account_age * SITE_ACCOUNT_AGE_RATIO
+      self.site_rep += self.user.bio.nexus_posts_count * SITE_POST_RATIO
+    end
+
+    # LOVER'S LAB
+    if self.user.bio.lover_user_path.present?
+      lover_account_age = (Date.now - self.user.bio.lover_date_joined)
+      self.site_rep += lover_account_age * SITE_ACCOUNT_AGE_RATIO
+      self.site_rep += self.user.bio.lover_posts_count * SITE_POST_RATIO
+    end
+
+    # STEAM WORKSHOP
+    if self.user.bio.workshop_user_path.present?
+      self.site_rep += self.user.bio.workshop_submissions_count * WORKSHOP_SUBMISSIONS_RATIO
+      self.site_rep += self.user.bio.workshop_followers_count * WORKSHOP_FOLLOWERS_RATIO
+    end
+
+    # Cap at MAX_SITE_REP
+    self.site_rep = [self.site_rep, MAX_SITE_REP].max
+  end
 end
