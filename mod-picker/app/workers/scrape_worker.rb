34f22e31575d48b9e4391ef2325a088f99aabec7
Mator <why101@msn.com>
2016-08-22 11:20:28 -0700

    added log messages to ScrapeWorker.perform

--- mod-picker/app/workers/scrape_worker.rb
+++ mod-picker/app/workers/scrape_worker.rb
@@ -4,18 +4,21 @@ class ScrapeWorker
   def perform
     # rescrape nexus if we're allowed to scrape nexus statistics
     if Rails.application.config.scrape_nexus_statistics
+      puts "\nRescraping Nexus Mods"
       NexusInfo.can_recrape.find_each do |nexus_info|
         nexus_info.scrape
       end
     end
 
     # rescrape lab
+    puts "\nRescraping Lover's Lab"
     LoverInfo.can_recrape.find_each do |lover_info|
       lover_info.scrape
     end
 
     # rescrape workshop
     if Rails.application.config.scrape_workshop_statistics
+      puts "\nRescraping Steam Workshop"
       WorkshopInfo.can_recrape.find_each do |workshop_info|
         workshop_info.scrape
       end
