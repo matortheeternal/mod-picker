5533c5fd8ab7a0babced8122c857b9a3fd13cd9e
Mator <why101@msn.com>
2016-08-22 12:23:20 -0700

    added enable_scheduler config variable to development and production environment files

--- mod-picker/config/environments/development.rb
+++ mod-picker/config/environments/development.rb
@@ -22,6 +22,9 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
 
+  # Disable scheduled tasks in development environment
+  config.enable_scheduler = false
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
--- mod-picker/config/environments/production.rb
+++ mod-picker/config/environments/production.rb
@@ -47,6 +47,9 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
 
+  # Enable scheduled tasks in production
+  config.enable_scheduler = true
+
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
 
