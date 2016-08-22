2aab555c5521a8607d409469736415ffdce7b825
Mator <why101@msn.com>
2016-08-21 20:31:06 -0700

    updated reputation_worker with kirillian's approach, and AREL!  (to be removed soon)

--- mod-picker/app/workers/reputation_worker.rb
+++ mod-picker/app/workers/reputation_worker.rb
@@ -1,77 +1,68 @@
 class ReputationWorker
   include Sidekiq::Worker
 
-  # CONSTANTS
   NUM_ITERATIONS = 4
-  USER_ENDORSE_RATIO = 0.05
 
-  def calculate_given_rep(reputations)
-    def iterate(reputations)
-      @visited_rep_ids = {}
+  # AREL METHODS
+  def user_reputation_table
+    @user_reputation_table ||= UserReputation.arel_table
+  end
 
-      def traverse_from(src_rep)
-        @visited_rep_ids[src_rep.id] = true
-        src_rep.given_reputation.each do |dst_rep|
-          next if dst_rep.dont_compute
-          dst_rep.given_rep += src_rep.overall_rep * USER_ENDORSE_RATIO
-          unless @visited_rep_ids[dst_rep.id]
-            traverse_from(dst_rep)
-          end
-        end
-      end
+  def linked_reputations_table
+    @linked_reputations_table ||= UserReputation.arel_table.alias('linked_reputations')
+  end
 
-      # traverse each island
-      islands = 0
-      reputations.each do |rep|
-        unless @visited_rep_ids[rep.id]
-          traverse_from(rep)
-          islands += 1
-        end
-      end
-    end
+  def reputation_links_table
+    @reputation_links_table ||= ReputationLink.arel_table
+  end
 
-    # iterate 4 times
-    puts "Calculating reputation network"
-    islands = 0
-    NUM_ITERATIONS.times do |i|
-      # iteration
-      start_time = Time.now
-      puts "Iteration #{i} ..."
-      islands = iterate(reputations)
+  def update_reputations_query
+    manager.table(joined_table).
+        where(subquery[:r_id].eq(user_reputation_table[:id])).
+        where(user_reputation_table[:dont_compute].eq(false)).
+        set([[user_reputation_table[:given_rep], subquery[:sum]]])
+  end
 
-      # update overall_rep and reset given_rep
-      reputation.each do |rep|
-        rep.calculate_overall_rep
-        rep.given_rep = 0 if i != NUM_ITERATIONS
-      end
+  def joined_table
+    Arel::Nodes::JoinSource.new(
+        user_reputation_table,
+        [user_reputation_table.create_join(subquery)]
+    )
+  end
 
-      # tracking
-      time_diff = (Time.now - beginning_time) * 1000
-      puts "  #{time_diff}ms"
-    end
+  def manager
+    Arel::UpdateManager.new(UserReputation)
+  end
 
-    # reporting
-    puts "#{islands} islands"
+  def subquery
+    user_reputation_table.project(
+        user_reputation_table[:id].as('r_id'),
+        Arel.sql("SUM(linked_reputations.overall * 0.05) AS sum")
+    ).
+        join(reputation_links_table).on(user_reputation_table[:id].eq(reputation_links_table[:to_rep_id])).
+        join(linked_reputations_table).on(linked_reputations_table[:id].eq(reputation_links_table[:from_rep_id])).
+        where(linked_reputations_table[:dont_compute].eq(false)).
+        group(user_reputation_table[:id]).as('g')
   end
 
   def perform
-    # load all reputations so things go fast
-    reputations = UserReputation.includes(:given_reputation, :user => :bio)
+    Benchmark.bm do |benchmark|
+      puts "\nCalculating base reputation"
+      benchmark.report("B") {
+        UserReputation.computable.find_each do |user_reputation|
+          user_reputation.calculate_site_rep!
+          user_reputation.calculate_contribution_rep!
+          user_reputation.calculate_author_rep!
+          user_reputation.save!
+        end
+      }
 
-    # calculate reputation groups
-    reputations.each do |rep|
-      next if rep.dont_compute
-      rep.given_rep = 0
-      rep.calculate_site_rep
-      rep.calculate_contribution_rep
-      rep.calculate_author_rep
-      rep.calculate_overall_rep
+      puts "\nCalculating network reputation"
+      NUM_ITERATIONS.times do |iteration|
+        benchmark.report("I#{iteration + 1}") {
+          ActiveRecord::Base.connection.execute(update_reputations_query.to_sql)
+        }
+      end
     end
-
-    # traverse network to calculate given reputation to each user
-    calculate_given_rep(reputations)
-
-    # save changes
-    reputations.each(&:save)
   end
 end
