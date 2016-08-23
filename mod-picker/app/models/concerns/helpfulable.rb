b79c0a50d8faf961edd00b26d22a86c12b2ff941
Mator <why101@msn.com>
2016-08-23 00:58:01 -0700

    refactored some unneeded self.'s out of Helpfulable

--- mod-picker/app/models/concerns/helpfulable.rb
+++ mod-picker/app/models/concerns/helpfulable.rb
@@ -8,30 +8,30 @@ module Helpfulable
   end
 
   def recompute_helpful_counts
-    self.helpful_count = HelpfulMark.where(helpfulable_id: self.id, helpfulable_type: "InstallOrderNote", helpful: true).count
-    self.not_helpful_count = HelpfulMark.where(helpfulable_id: self.id, helpfulable_type: "InstallOrderNote", helpful: false).count
+    self.helpful_count = helpful_marks.helpful(true).count
+    self.not_helpful_count = helpful_marks.helpful(false).count
   end
 
   def compute_reputation
-    if self.respond_to?(:standing)
-      if self.standing == :bad
+    if respond_to?(:standing)
+      if standing == :bad
         self.helpfulness = -100
         return
-      elsif self.standing == :unknown
+      elsif standing == :unknown
         self.helpfulness = 0
         return
       end
     end
 
-    user_rep = self.submitter.reputation.overall
-    helpfulness = (self.helpful_count - self.not_helpful_count)
+    user_rep = submitter.reputation.overall
+    helpfulness = (helpful_count - not_helpful_count)
 
     # calculate reputation of contribution based on user reputation and helpfulness
     if user_rep < 0
       self.reputation = user_rep + helpfulness
     else
       user_rep_factor = 2 / (1 + Math::exp(-0.0075 * (user_rep - 640)))
-      if self.helpful_count < self.not_helpful_count
+      if helpful_count < not_helpful_count
         self.reputation = (1 - user_rep_factor / 2) * helpfulness
       else
         self.reputation = (1 + user_rep_factor / 2) * helpfulness
