ad308110686bc6d0361509ea2899a69ea0d1b8cf
Mator <why101@msn.com>
2016-08-23 00:35:42 -0700

    applied ScopeHelpers macros to UserTitle

--- mod-picker/app/models/user_title.rb
+++ mod-picker/app/models/user_title.rb
@@ -1,7 +1,7 @@
 class UserTitle < ActiveRecord::Base
-  include Filterable
+  include Filterable, ScopeHelpers
 
-  scope :game, -> (game) { where(game_id: game) }
+  game_scope
 
   belongs_to :game
 
