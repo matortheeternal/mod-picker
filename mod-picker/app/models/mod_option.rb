c1b802993ccef6cd29f50968e1501d6b7d290331
Mator <why101@msn.com>
2016-08-26 11:51:40 -0700

    added update_counters instance method to ModOption

--- mod-picker/app/models/mod_option.rb
+++ mod-picker/app/models/mod_option.rb
@@ -3,4 +3,13 @@ class ModOption < ActiveRecord::Base
 
   has_many :plugins, :inverse_of => 'mod_option'
   has_many :mod_asset_files, :inverse_of => 'mod_option'
+
+  def update_counters
+    self.asset_files_count = mod_asset_files.count
+    self.plugins_count = plugins.count
+    self.update_columns({
+        plugins_count: plugins_count,
+        asset_files_count: asset_files_count
+    })
+  end
 end
