09dba691bb290a7e18e52be3e6fe80986a79231b
Mator <why101@msn.com>
2016-08-28 15:57:04 -0700

    fixed mod list mod option validations

--- mod-picker/app/models/mod_list_mod_option.rb
+++ mod-picker/app/models/mod_list_mod_option.rb
@@ -4,6 +4,5 @@ class ModListModOption < ActiveRecord::Base
   belongs_to :mod_option, :inverse_of => 'mod_list_mod_options'
 
   # VALIDATIONS
-  validates :mod_list_id, :mod_option_id, presence: true
-  validates :enabled, inclusion: [true, false]
+  validates :mod_list_mod_id, :mod_option_id, presence: true
 end
