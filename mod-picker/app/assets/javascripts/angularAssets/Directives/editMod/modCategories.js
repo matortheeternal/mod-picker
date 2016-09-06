46c539e8ba41698c42caa281b44bb6c01f00a1cd
Mator <why101@msn.com>
2016-09-06 11:43:28 -0700

    fixed modCategories directive - now working

--- mod-picker/app/assets/javascripts/angularAssets/Directives/editMod/modCategories.js
+++ mod-picker/app/assets/javascripts/angularAssets/Directives/editMod/modCategories.js
@@ -1,79 +1,98 @@
-app.directive('modAnalysis', function () {
+app.directive('modCategories', function () {
     return {
         restrict: 'E',
-        templateUrl: '/resources/directives/editMod/modAnalysis.html',
+        templateUrl: '/resources/directives/editMod/modCategories.html',
         scope: false,
-        controller: 'modAnalysisController'
+        controller: 'modCategoriesController'
     }
 });
 
-app.controller('modAnalysisController', function ($scope, pluginService, objectUtils, assetUtils) {
-    $scope.changeAnalysisFile = function (event) {
-        var input = event.target;
-        if (input.files && input.files[0]) {
-            $scope.loadAnalysisFile(input.files[0]);
+app.controller('modCategoriesController', function ($scope, categoryService) {
+    $scope.getDominantIds = function(recessiveId) {
+        var dominantIds = [];
+        for (var i = 0; i < $scope.categoryPriorities.length; i++) {
+            var priority = $scope.categoryPriorities[i];
+            if (priority.recessive_id == recessiveId) {
+                dominantIds.push(priority.dominant_id);
+            }
+        }
+        return dominantIds;
+    };
+
+    $scope.getCategoryPriority = function(recessiveId, dominantId) {
+        for (var i = 0; i < $scope.categoryPriorities.length; i++) {
+            var priority = $scope.categoryPriorities[i];
+            if (priority.recessive_id == recessiveId &&
+                priority.dominant_id == dominantId)
+                return priority;
         }
     };
 
-    $scope.browseAnalysisFile = function () {
-        document.getElementById('analysis-input').click();
+    $scope.createPriorityMessage = function(recessiveId, dominantId) {
+        var recessiveCategory = categoryService.getCategoryById($scope.categories, recessiveId);
+        var dominantCategory = categoryService.getCategoryById($scope.categories, dominantId);
+        var categoryPriority = $scope.getCategoryPriority(recessiveId, dominantId);
+        var messageText = dominantCategory.name + " > " + recessiveCategory.name + "\n" + categoryPriority.description;
+        $scope.categoryMessages.push({
+            text: messageText,
+            klass: "priority-message"
+        });
     };
 
-    $scope.addRequirementFromPlugin = function(filename) {
-        pluginService.searchPlugins(filename).then(function(plugins) {
-            var plugin = plugins.find(function(plugin) {
-                return plugin.filename === filename;
-            });
-            if (plugin) {
-                var match = $scope.mod.requirements.find(function(requirement) {
-                    return requirement.required_id == plugin.mod_id;
-                });
-                if (!match) {
-                    $scope.mod.requirements.push({
-                        required_id: plugin.mod_id,
-                        name: plugin.mod.name
-                    });
-                }
+    $scope.getSuperCategories = function() {
+        var superCategories = [];
+        $scope.mod.categories.forEach(function (id) {
+            var superCategory = categoryService.getCategoryById($scope.categories, id).parent_id;
+            if (superCategory && superCategories.indexOf(superCategory) == -1) {
+                superCategories.push(superCategory);
             }
         });
+        return superCategories;
     };
 
-    $scope.getRequirementsFromAnalysis = function () {
-        // build list of masters
-        var masters = [];
-        var defaultPlugins = [];
-        $scope.mod.analysis.mod_options.forEach(function (option) {
-            if (option.default) defaultPlugins = defaultPlugins.concat(option.plugins);
-        });
-        defaultPlugins.forEach(function (plugin) {
-            plugin.master_plugins.forEach(function (master) {
-                if (masters.indexOf(master.filename) == -1) {
-                    masters.push(master.filename);
+    $scope.checkCategories = function() {
+        $scope.categoryMessages = [];
+        var selectedCategories = Array.prototype.concat($scope.getSuperCategories(), $scope.mod.categories);
+        selectedCategories.forEach(function(recessiveId) {
+            dominantIds = $scope.getDominantIds(recessiveId);
+            dominantIds.forEach(function(dominantId) {
+                var index = selectedCategories.indexOf(dominantId);
+                if (index > -1) {
+                    $scope.createPriorityMessage(recessiveId, dominantId);
                 }
             });
         });
-        // load requirements from masters
-        masters.forEach(function (filename) {
-            $scope.addRequirementFromPlugin(filename);
-        });
+        if ($scope.mod.categories.length > 2) {
+            $scope.categoryMessages.push({
+                text: "You have too many categories selected. \nThe maximum number of categories allowed is 2.",
+                klass: "cat-error-message"
+            });
+        } else if ($scope.mod.categories.length == 0) {
+            $scope.categoryMessages.push({
+                text: "You must select at least one category.",
+                klass: "cat-error-message"
+            });
+        } else if ($scope.categoryMessages.length == 0) {
+            $scope.categoryMessages.push({
+                text: "Categories look good!",
+                klass: "cat-success-message"
+            });
+            var primaryCategory = categoryService.getCategoryById($scope.categories, $scope.mod.categories[0]);
+            $scope.categoryMessages.push({
+                text: "Primary Category: " + primaryCategory.name,
+                klass: "cat-success-message"
+            });
+            if ($scope.mod.categories.length > 1) {
+                var secondaryCategory = categoryService.getCategoryById($scope.categories, $scope.mod.categories[1]);
+                $scope.categoryMessages.push({
+                    text: "Secondary Category: " + secondaryCategory.name,
+                    klass: "cat-success-message"
+                });
+            }
+        }
     };
 
-    $scope.loadAnalysisFile = function (file) {
-        var fileReader = new FileReader();
-        var jsonMap = {
-            plugin_record_groups: 'plugin_record_groups_attributes',
-            plugin_errors: 'plugin_errors_attributes',
-            overrides: 'overrides_attributes'
-        };
-        fileReader.onload = function (event) {
-            var fixedJson = objectUtils.remapProperties(event.target.result, jsonMap);
-            var analysis = JSON.parse(fixedJson);
-            analysis.mod_options.forEach(function (option) {
-                option.nestedAssets = assetUtils.getNestedAssets(option.assets);
-            });
-            $scope.mod.analysis = analysis;
-            $scope.getRequirementsFromAnalysis();
-        };
-        fileReader.readAsText(file);
+    $scope.swapCategories = function() {
+        $scope.mod.categories.reverse();
     };
 });
