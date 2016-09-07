3eccf15e66ee4b585889acc8b3262e67645e6b32
Mator <why101@msn.com>
2016-09-07 09:28:10 -0700

    extended sortable for sorting by multiple columns

--- mod-picker/app/models/concerns/sortable.rb
+++ mod-picker/app/models/concerns/sortable.rb
@@ -5,7 +5,11 @@ module Sortable
     def sort(options)
       results = self.where(nil)
       if options.present? && options.has_key?(:column) && options.has_key?(:direction)
-        if options[:column].include?(".")
+        if options[:column].include?(",")
+          results = results.
+              select(sanitize_sql_array(["MAX(?) as scol", options[:column]]).tr("'", "")).
+              order(sanitize_sql_array(["scol ?", options[:direction]]).tr("'", ""))
+        elsif options[:column].include?(".")
           results = results.order(sanitize_sql_array(["? ?", options[:column], options[:direction]]).tr("'", ""))
         else
           results = results.order(options[:column] => options[:direction])
