85e53b01da8442c3b71f250997bbfcac013d7d1e
Mator <why101@msn.com>
2016-09-08 16:01:54 -0700

    created Event model

---
+++ mod-picker/app/models/event.rb
@@ -0,0 +1,30 @@
+class Event < ActiveRecord::Base
+  include ScopeHelpers
+
+  # We could save bandwidth by doing this on the frontend, if we wanted to
+  enum event_type: [:added, :updated, :removed, :hidden, :unhidden, :approved, :unapproved, :status_changed, :action_soon, :message, :unused, :milestone1, :milestone2, :milestone3, :milestone4, :milestone5, :milestone6, :milestone7, :milestone8, :milestone9, :milestone10]
+
+  # SCOPES
+  enum_scope :event_type
+  polymorphic_scope :content
+
+  # ASSOCIATIONS
+  belongs_to :content, :polymorphic => true
+  has_many :read_notifications, :inverse_of => 'event'
+
+  # number of events per page on the notifications index
+  self.per_page = 50
+
+  # VALIDATIONS
+  validates :content_id, :content_type, :event_type, presence: true
+  validates :content_type, length: {in: 1..32}
+
+  # CALLBACKS
+  before_create :set_dates
+
+  # Private methods
+  private
+    def set_dates
+      self.created = DateTime.now
+    end
+end
