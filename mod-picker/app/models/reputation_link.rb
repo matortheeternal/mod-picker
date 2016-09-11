8c007276c02251ecbb54e9bae64c25f3b1ba9ee1
Mator <why101@msn.com>
2016-09-10 21:53:04 -0700

    added event tracking and notification subscriptions to ReputationLink

--- mod-picker/app/models/reputation_link.rb
+++ mod-picker/app/models/reputation_link.rb
@@ -1,6 +1,14 @@
 class ReputationLink < ActiveRecord::Base
+  include Trackable
+
   # ATTRIBUTES
   self.primary_keys = :from_rep_id, :to_rep_id
+
+  # EVENT TRACKING
+  track :added, :removed
+
+  # NOTIFICATION SUBSCRIPTIONS
+  subscribe :target_user, to: [:added]
 
   # ASSOCIATIONS
   belongs_to :target_reputation, :class_name => 'UserReputation', :foreign_key => 'to_rep_id', :inverse_of => 'incoming_reputation_links'
