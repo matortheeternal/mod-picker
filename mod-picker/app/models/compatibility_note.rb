3ee8bf5da1e51e056a7997865596a635f1f37595
Mator <why101@msn.com>
2016-09-10 21:51:03 -0700

    added event tracking and notification subscriptions to CompatibilityNote

--- mod-picker/app/models/compatibility_note.rb
+++ mod-picker/app/models/compatibility_note.rb
@@ -1,9 +1,17 @@
 class CompatibilityNote < ActiveRecord::Base
-  include Filterable, Sortable, RecordEnhancements, Correctable, Helpfulable, Reportable, ScopeHelpers
+  include Filterable, Sortable, RecordEnhancements, Correctable, Helpfulable, Reportable, ScopeHelpers, Trackable
 
   # ATTRIBUTES
   enum status: [ :incompatible, :"partially incompatible", :"compatibility mod", :"compatibility option", :"make custom patch" ]
   self.per_page = 25
+
+  # EVENT TRACKING
+  track :added, :approved, :hidden
+  track :message, :column => 'moderator_message'
+
+  # NOTIFICATION SUBSCRIPTION
+  subscribe :mod_author_users, to: [:added, :approved, :unhidden]
+  subscribe :submitter, to: [:message, :approved, :unapproved, :hidden, :unhidden]
 
   # SCOPES
   include_scope :hidden
@@ -68,6 +76,10 @@ class CompatibilityNote < ActiveRecord::Base
 
   def mods
     [first_mod, second_mod]
+  end
+
+  def mod_author_users
+    User.includes(:mod_authors).where(:mod_authors => {mod_id: [first_mod_id, second_mod_id]})
   end
 
   def create_history_entry
