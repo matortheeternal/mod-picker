15a6ccf059e5cb5f5d41692908294274a2344bbb
Mator <why101@msn.com>
2016-09-13 11:17:15 -0700

    notificationService.retrieveNotifications now resolves pagination information

--- mod-picker/app/assets/javascripts/angularAssets/BackendAPI/notificationService.js
+++ mod-picker/app/assets/javascripts/angularAssets/BackendAPI/notificationService.js
@@ -1,6 +1,13 @@
-app.service('notificationService', function($q, backend) {
-    this.retrieveNotifications = function() {
-        return backend.retrieve('/notifications');
+app.service('notificationService', function($q, backend, pageUtils) {
+    this.retrieveNotifications = function(options, pageInformation) {
+        var action = $q.defer();
+        backend.post('/notifications', options).then(function(data) {
+            pageUtils.getPageInformation(data, pageInformation, options.page);
+            action.resolve(data);
+        }, function(response) {
+            action.reject(response);
+        });
+        return action.promise;
     };
 
     this.retrieveRecent = function() {
