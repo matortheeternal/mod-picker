b63762666c7270870017663d115a00e02ff7e6c9
Mator <why101@msn.com>
2016-09-13 22:03:19 -0700

    added missing range scope for mod reputation

--- mod-picker/app/models/mod.rb
+++ mod-picker/app/models/mod.rb
@@ -26,6 +26,7 @@ class Mod < ActiveRecord::Base
   user_scope :author_users, :alias => 'mp_author'
   enum_scope :status
   date_scope :released, :updated
+  range_scope :reputation
   range_scope :average_rating, :alias => 'rating'
   counter_scope :plugins_count, :asset_files_count, :required_mods_count, :required_by_count, :tags_count, :stars_count, :mod_lists_count, :reviews_count, :compatibility_notes_count, :install_order_notes_count, :load_order_notes_count, :corrections_count
   source_scope :views, :sites =>  [:nexus, :lab, :workshop]
