d30a0f8132444ba29bf73dc4cfbd6a995e839452
BlueAccords <myoldemailname@gmail.com>
2016-09-20 13:34:54 -0700

    added review directive controller, show actions toggle for review directive, and show action to false for review reportable

--- mod-picker/app/assets/javascripts/angularAssets/Directives/contributions/review.js
+++ mod-picker/app/assets/javascripts/angularAssets/Directives/contributions/review.js
@@ -6,13 +6,23 @@ app.directive('review', function() {
     return {
         restrict: 'E',
         templateUrl: '/resources/directives/contributions/review.html',
+        controller: 'reviewController',
         scope: {
             review: '=',
             index: '=',
             currentUser: '=',
             edit: '=?',
             showUserColumn: '=?',
-            showMod: '=?'
+            showMod: '=?',
+            showActions: '=?'
         }
     };
+});
+
+app.controller('reviewController', function ($scope) {
+    // set defaults to true if not defined
+    $scope.showUserColumn = angular.isDefined($scope.showUserColumn) ? $scope.showUserColumn : true;
+    $scope.showActions = angular.isDefined($scope.showActions) ? $scope.showActions : true;
+    $scope.showMod = angular.isDefined($scope.showMod) ? $scope.showMod : true;
+
 });
