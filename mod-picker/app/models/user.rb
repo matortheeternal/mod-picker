28681ce985cf95d1c69e8e5034bf9b15d66b18e8
BlueAccords <myoldemailname@gmail.com>
2016-09-21 16:43:56 -0700

    added reportable for users

--- mod-picker/app/models/user.rb
+++ mod-picker/app/models/user.rb
@@ -253,7 +253,7 @@ class User < ActiveRecord::Base
 
   def reportable_json_options
     options = {
-        :only => [:id, :username, :role, :title, :about_me, :last_sign_in_at, :joined],
+        :only => [:id, :username, :role, :title, :about_me, :last_sign_in_at, :joined, :current_sign_in_at, :last_sign_in_at],
         :include => {
             :reputation => {
                 :only => [:overall, :rep_to_count]
