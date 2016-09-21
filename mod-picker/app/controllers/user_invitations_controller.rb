25cb4b048049fc50c8fc976ae87aa2061b972424
Mator <why101@msn.com>
2016-09-21 13:28:23 -0700

    added batch invitation actions to user invitations controller

--- mod-picker/app/controllers/user_invitations_controller.rb
+++ mod-picker/app/controllers/user_invitations_controller.rb
@@ -1,12 +1,35 @@
 class UserInvitationsController < Devise::InvitationsController
-  before_action :require_admin, only: [:new, :create, :destroy]
+  before_action :authorize_invite, only: [:new_batch, :create_batch, :new, :create, :destroy]
 
-  private
-
-  def require_admin
-    unless current_user.admin?
-      flash[:error] = "Only admins can invite new users to the beta"
-      redirect_to root_path
-    end
+  # GET /users/invitation/batch/new
+  def new_batch
+    self.resource = resource_class.new
+    render :new_batch
   end
+
+  # POST /users/invitation/batch
+  def create_batch
+    self.resource = resource_class.new
+    if batch_invite_resource
+      flash[:notice] = "Invitations sent!"
+    else
+      flash[:error] = "Invitations failed: " + resource_class.failed_emails.to_s
+    end
+
+    redirect_to :users_invitation_batch_new
+  end
+
+  protected
+    def authorize_invite
+      authorize! :invite, User
+    end
+
+    def batch_invite_params
+      params.require(:user).require(:email)
+    end
+
+    def batch_invite_resource
+      emails = batch_invite_params.split(",").collect(&:strip)
+      User.batch_invite!(emails, current_inviter)
+    end
 end
