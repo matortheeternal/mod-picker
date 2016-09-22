c410367f6f0df0ee752ecc3be4c9bba72a264a12
BlueAccords <myoldemailname@gmail.com>
2016-09-22 00:17:28 -0700

    refactored reportables partials to include their base report title/header html section

--- mod-picker/app/assets/javascripts/angularAssets/Directives/contributions/report.js
+++ mod-picker/app/assets/javascripts/angularAssets/Directives/contributions/report.js
@@ -18,14 +18,4 @@ app.directive('report', function() {
 app.controller('reportController', function($scope) {
     angular.inherit($scope, 'report');
 
-    // list of reportables where the template will override the outer container 
-    // in order to add extra misc information into the outer base report container
-    var overrideTemplates = ['Tag'];
-
-    $scope.overrideTemplate = false;
-
-    // checks if reportable_type is within overrideTemplates and toggles overrideTemplate option
-    if(overrideTemplates.indexOf($scope.report.reportable_type) > -1) {
-        $scope.overrideTemplate = true;
-    }
 });
