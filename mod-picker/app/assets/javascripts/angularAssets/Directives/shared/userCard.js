5e1241520a8ad4ebb2fec6d57d8a9cdc1a2474eb
Mator <why101@msn.com>
2016-09-22 23:45:14 -0700

    created userCardController

--- mod-picker/app/assets/javascripts/angularAssets/Directives/shared/userCard.js
+++ mod-picker/app/assets/javascripts/angularAssets/Directives/shared/userCard.js
@@ -8,3 +8,69 @@ app.directive('userCard', function () {
         controller: 'userCardController'
     }
 });
+
+app.controller('userCardController', function($scope, $rootScope, userService) {
+    // inherited variables
+    $scope.permissions = $rootScope.permissions;
+
+    // functions
+    $scope.addRep = function() {
+        if ($scope.added && !$scope.subtracted) return;
+        $scope.added = true;
+        userService.addRep($scope.user.id).then(function() {
+            $scope.$emit("successMessage", "Increased "+$scope.user.username+"'s reputation by 5");
+            $scope.user.reputation.overall += 5;
+        }, function(response) {
+            var params = {
+                message: "Error increasing reputation",
+                response: response
+            };
+            $scope.$emit("errorMessage", params);
+        });
+    };
+
+    $scope.subtractRep = function() {
+        if ($scope.subtracted && !$scope.added) return;
+        $scope.subtracted = true;
+        userService.subtractRep($scope.user.id).then(function() {
+            $scope.$emit("successMessage", "Reduced "+$scope.user.username+"'s reputation by 5");
+            $scope.user.reputation.overall -= 5;
+        }, function(response) {
+            var params = {
+                message: "Error reducing reputation",
+                response: response
+            };
+            $scope.$emit("errorMessage", params);
+        });
+    };
+
+    $scope.changeRole = function(label, role) {
+        if (!role) role = label;
+        userService.changeRole($scope.user.id, role).then(function() {
+            $scope.$emit("successMessage", label.capitalize() + " " + $scope.user.username);
+            $scope.user.role = role;
+        }, function(response) {
+            var params = {
+                message: "Error " + label.replace(/ing/g, "ed") + " user",
+                response: response
+            };
+            $scope.$emit("errorMessage", params);
+        });
+    };
+
+    $scope.restrictUser = function() {
+        $scope.changeRole("restricted");
+    };
+
+    $scope.unrestrictUser = function() {
+        $scope.changeRole("unrestricted", "user");
+    };
+
+    $scope.banUser = function() {
+        $scope.changeRole("banned");
+    };
+
+    $scope.unbanUser = function() {
+        $scope.changeRole("unbanned", "user");
+    };
+});
