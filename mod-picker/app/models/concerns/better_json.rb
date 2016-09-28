e9acf8406edb7fd170109a2b279ddcb2ebb15e62
Mator <why101@msn.com>
2016-09-27 21:00:52 -0700

    updated BetterJson concern to support view files

--- mod-picker/app/models/concerns/better_json.rb
+++ mod-picker/app/models/concerns/better_json.rb
@@ -1,26 +1,36 @@
+# Better name pending
 module BetterJson
   extend ActiveSupport::Concern
 
+  included do
+    class_attribute :_json_template_cache
+    self._json_template_cache = {}
+  end
+
   # noinspection RubySuperCallWithoutSuperclassInspection
   def as_json(options={})
-    if options[:format]
-      super(public_send("#{options[:format]}_json_format"))
-    elsif self.class.json_options_empty(options)
-      super(options.merge(self.class.base_json_format))
+    if json_options_empty(options)
+      super(get_json_template(options[:template] || :base))
     else
       super(options)
     end
   end
 
-  module ClassMethods
-    # STUB
-    def base_json_format
-      {}
-    end
+  def json_options_empty(options)
+    keys = [:include, :only, :except, :methods]
+    !(options.keys & keys).any?
+  end
 
-    def json_options_empty(options)
-      keys = [:include, :only, :except, :methods]
-      !(options.keys & keys).any?
-    end
+  def get_json_template(template)
+    self.class._json_template_cache[template] || load_json_template(template)
+  end
+
+  def load_json_template(template)
+    file = File.read(json_template_path(template))
+    self.class._json_template_cache[template] = JSON.parse(file)
+  end
+
+  def json_template_path(template)
+    Rails.root.join('app', 'views', self.class.name.underscore.pluralize, "#{template}.json")
   end
 end
