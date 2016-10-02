7a5e7d465a49088125e57089067c7d169e2e191b
Mator <why101@msn.com>
2016-10-02 01:17:51 -0700

    fixed nexus info scraping and exception logic

--- mod-picker/app/controllers/lover_infos_controller.rb
+++ mod-picker/app/controllers/lover_infos_controller.rb
@@ -2,19 +2,12 @@ class LoverInfosController < ApplicationController
   before_action :set_lover_info, only: [:show, :destroy]
 
   # GET /lover_infos/1
-  # GET /lover_infos/1.json
   def show
-    render :json => @lover_info
-  end
-
-  # DELETE /lover_infos/1
-  # DELETE /lover_infos/1.json
-  def destroy
-    authorize! :destroy, @lover_info
-    if @lover_info.destroy
-      render json: {status: :ok}
-    else
-      render json: @lover_info.errors, status: :unprocessable_entity
+    begin
+      @lover_info.rescrape
+      render :json => @lover_info
+    rescue Exception => e
+      render :json => {error: e.message}
     end
   end
 
@@ -24,11 +17,8 @@ class LoverInfosController < ApplicationController
       authorize! :create, Mod
       begin
         @lover_info = LoverInfo.find(params[:id])
-        @lover_info.rescrape
       rescue
         @lover_info = LoverInfo.new(id: params[:id])
-        @lover_info.scrape
-        @lover_info.save
       end
     end
 end
--- mod-picker/app/controllers/nexus_infos_controller.rb
+++ mod-picker/app/controllers/nexus_infos_controller.rb
@@ -2,19 +2,12 @@ class NexusInfosController < ApplicationController
   before_action :set_nexus_info, only: [:show, :destroy]
 
   # GET /nexus_infos/1
-  # GET /nexus_infos/1.json
   def show
-    render :json => @nexus_info
-  end
-
-  # DELETE /nexus_infos/1
-  # DELETE /nexus_infos/1.json
-  def destroy
-    authorize! :destroy, @nexus_info
-    if @nexus_info.destroy
-      render json: {status: :ok}
-    else
-      render json: @nexus_info.errors, status: :unprocessable_entity
+    begin
+      @nexus_info.rescrape
+      render :json => @nexus_info
+    rescue Exception => e
+      render :json => {error: e.message}
     end
   end
 
@@ -24,12 +17,9 @@ class NexusInfosController < ApplicationController
       authorize! :create, Mod
       begin
         @nexus_info = NexusInfo.find(params[:id])
-        @nexus_info.rescrape
       rescue
         if params.has_key?(:game_id)
           @nexus_info = NexusInfo.new(id: params[:id], game_id: params[:game_id])
-          @nexus_info.scrape
-          @nexus_info.save
         else
           raise "Cannot scrape Nexus Info with no game id."
         end
