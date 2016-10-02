565794a7396a7ad2fd3d0a3654dbd8d493259197
Mator <why101@msn.com>
2016-10-01 23:37:33 -0700

    removed redundant selfs from ModListStar callbacks and ModStar callbacks

--- mod-picker/app/models/mod_list_star.rb
+++ mod-picker/app/models/mod_list_star.rb
@@ -1,4 +1,6 @@
 class ModListStar < ActiveRecord::Base
+  include RecordEnhancements
+
   self.primary_keys = :mod_list_id, :user_id
 
   belongs_to :user, :inverse_of => 'mod_list_stars'
@@ -13,12 +15,12 @@ class ModListStar < ActiveRecord::Base
 
   private
     def increment_counters
-      self.user.update_counter(:starred_mod_lists_count, 1)
-      self.mod_list.update_counter(:stars_count, 1)
+      user.update_counter(:starred_mod_lists_count, 1)
+      mod_list.update_counter(:stars_count, 1)
     end
 
     def decrement_counters
-      self.user.update_counter(:starred_mod_lists_count, -1)
-      self.mod_list.update_counter(:stars_count, -1)
+      user.update_counter(:starred_mod_lists_count, -1)
+      mod_list.update_counter(:stars_count, -1)
     end
 end
--- mod-picker/app/models/mod_star.rb
+++ mod-picker/app/models/mod_star.rb
@@ -1,4 +1,6 @@
 class ModStar < ActiveRecord::Base
+  include RecordEnhancements
+
   self.primary_keys = :mod_id, :user_id
 
   belongs_to :user, :inverse_of => 'mod_stars'
@@ -13,12 +15,12 @@ class ModStar < ActiveRecord::Base
 
   private
     def increment_counters
-      self.user.update_counter(:starred_mod_lists_count, 1)
-      self.mod.update_counter(:stars_count, 1)
+      user.update_counter(:starred_mod_lists_count, 1)
+      mod.update_counter(:stars_count, 1)
     end
 
     def decrement_counters
-      self.user.update_counter(:starred_mod_lists_count, -1)
-      self.mod.update_counter(:stars_count, -1)
+      user.update_counter(:starred_mod_lists_count, -1)
+      mod.update_counter(:stars_count, -1)
     end
 end
