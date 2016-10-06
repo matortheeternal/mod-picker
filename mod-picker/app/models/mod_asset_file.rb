d12adcbfbc76dd58821d28ac24c3e2f3ed35efb7
Mator <why101@msn.com>
2016-10-05 18:38:55 -0700

    fixed conflicting assets scope and now using arel for it

--- mod-picker/app/models/mod_asset_file.rb
+++ mod-picker/app/models/mod_asset_file.rb
@@ -8,7 +8,19 @@ class ModAssetFile < ActiveRecord::Base
 
   # UNIQUE SCOPES
   scope :bsa, -> { joins(:asset_file).where("asset_files.path like '%.bsa'") }
-  scope :conflicting, -> { joins("JOIN mod_asset_files mod_asset_files_right ON mod_asset_files.asset_file_id = mod_asset_files_right.asset_file_id").where("mod_asset_files.mod_option_id <> mod_asset_files_right.mod_option_id").where("mod_asset_files.asset_file_id IS NOT NULL").order(:asset_file_id) }
+  scope :conflicting, -> (mod_option_ids) {
+    maf = arel_table
+    maf_right = arel_table.alias
+    mo = ModOption.arel_table
+    mo_right = ModOption.arel_table.alias
+
+    joins(conflicting_join_query(maf, maf_right, mo, mo_right).join_sources).
+        where(mo[:mod_id].not_eq(mo_right[:mod_id])).
+        where(maf[:asset_file_id].not_eq(nil)).
+        where(maf[:mod_option_id].in(mod_option_ids)).
+        where(maf_right[:mod_option_id].in(mod_option_ids)).
+        order(maf[:asset_file_id])
+  }
 
   # ASSOCIATIONS
   belongs_to :mod_option, :inverse_of => 'mod_asset_files'
@@ -18,6 +30,12 @@ class ModAssetFile < ActiveRecord::Base
   # CALLBACKS
   after_create :increment_counters
   before_destroy :decrement_counters
+
+  def self.conflicting_join_query(maf, maf_right, mo, mo_right)
+    maf.join(maf_right).on(maf[:asset_file_id].eq(maf_right[:asset_file_id])).
+        join(mo).on(mo[:id].eq(maf[:mod_option_id])).
+        join(mo_right).on(mo_right[:id].eq(maf_right[:mod_option_id]))
+  end
 
   def as_json(options={})
     if JsonHelpers.json_options_empty(options)
