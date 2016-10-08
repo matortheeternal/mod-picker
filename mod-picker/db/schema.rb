b135db42a69f1a1d69c681356246d63ce621832e
Mator <why101@msn.com>
2016-10-07 22:42:50 -0700

    ran migartions, updating gitignore

--- mod-picker/db/schema.rb
+++ mod-picker/db/schema.rb
@@ -386,8 +386,7 @@ ActiveRecord::Schema.define(version: 20161006191828) do
   end
 
   add_index "mod_asset_files", ["asset_file_id"], name: "maf_id", using: :btree
-  add_index "mod_asset_files", ["mod_option_id", "asset_file_id"], name: "mod_id", unique: true, using: :btree
-  add_index "mod_asset_files", ["mod_option_id"], name: "mv_id", using: :btree # TODO: DROP THIS
+  add_index "mod_asset_files", ["mod_option_id"], name: "mv_id", using: :btree
 
   create_table "mod_authors", force: :cascade do |t|
     t.integer "mod_id",  limit: 4,             null: false
