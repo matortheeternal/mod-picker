94b325394f3fac49f3d6984b7cc5da3192983927
Mator <why101@msn.com>
2016-10-09 10:34:22 -0700

    optimized compatibility notes index sql

--- mod-picker/app/controllers/compatibility_notes_controller.rb
+++ mod-picker/app/controllers/compatibility_notes_controller.rb
@@ -4,8 +4,8 @@ class CompatibilityNotesController < ContributionsController
   # GET /compatibility_notes
   def index
     # prepare compatibility notes
-    @compatibility_notes = CompatibilityNote.preload(:editor, :editors).includes(:submitter => :reputation).references(:submitter => :reputation).accessible_by(current_ability).filter(filtering_params).sort(params[:sort]).paginate(:page => params[:page])
-    count = CompatibilityNote.includes(:submitter => :reputation).references(:submitter => :reputation).accessible_by(current_ability).filter(filtering_params).count
+    @compatibility_notes = CompatibilityNote.preload(:editor, :editors, :submitter => :reputation).includes(:first_mod, :second_mod).accessible_by(current_ability).filter(filtering_params).sort(params[:sort]).paginate(:page => params[:page])
+    count = CompatibilityNote.preload(:submitter => :reputation).accessible_by(current_ability).filter(filtering_params).count
 
     # prepare helpful marks
     helpful_marks = HelpfulMark.submitter(current_user.id).helpfulables("CompatibilityNote", @compatibility_notes.ids)
