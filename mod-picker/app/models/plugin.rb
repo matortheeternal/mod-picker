b0c294390e2bb77f97920e7f5c74ce25aab2bfe5
Mator <why101@msn.com>
2016-10-09 14:46:24 -0700

    fixed bug with plugin search not working

--- mod-picker/app/models/plugin.rb
+++ mod-picker/app/models/plugin.rb
@@ -17,7 +17,7 @@ class Plugin < ActiveRecord::Base
 
 
   # UNIQUE SCOPES
-  scope :visible, -> { preload(:mod).where(:mods => {hidden: false}) }
+  scope :visible, -> { eager_load(:mod).where(:mods => {hidden: false}) }
   scope :mods, -> (mod_ids) { includes(:mod_option).references(:mod_option).where(:mod_options => { :mod_id => mod_ids }) }
   scope :esm, -> { where("filename like '%.esm'") }
   scope :categories, -> (categories) { joins(:mod).where("mods.primary_category_id IN (:ids) OR mods.secondary_category_id IN (:ids)", ids: categories) }
