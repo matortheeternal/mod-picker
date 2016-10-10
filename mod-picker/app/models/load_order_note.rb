36779bbb8fbe0aabf01733f1c079449b0e0ef0bd
Mator <why101@msn.com>
2016-10-09 17:42:21 -0700

    now including submitted_by column in note json

--- mod-picker/app/models/load_order_note.rb
+++ mod-picker/app/models/load_order_note.rb
@@ -110,7 +110,6 @@ class LoadOrderNote < ActiveRecord::Base
   def as_json(options={})
     if JsonHelpers.json_options_empty(options)
       default_options = {
-          :except => [:submitted_by],
           :include => {
               :submitter => {
                   :only => [:id, :username, :role, :title, :joined, :last_sign_in_at, :reviews_count, :compatibility_notes_count, :install_order_notes_count, :load_order_notes_count, :corrections_count, :comments_count],
