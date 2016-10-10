2ad8563b48d57322061d3cb37191762fd0d0a516
Mator <why101@msn.com>
2016-10-10 12:37:57 -0700

    Revert "created and ran migration so nexus_info records don't require general info"
    This reverts commit f7bde49cdced8f17ef684ad8bb04ea028060859b.

--- mod-picker/db/migrate/20161010183908_dont_require_nexus_general_info.rb
+++
@@ -1,7 +0,0 @@
-class DontRequireNexusGeneralInfo < ActiveRecord::Migration
-  def change
-    change_column :nexus_infos, :mod_name, :string, null: true
-    change_column :nexus_infos, :authors, :string, limit: 128, null: true
-    change_column :nexus_infos, :released, :datetime, null: true
-  end
-end
