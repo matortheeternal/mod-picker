a4dde20fbfbb38c4c48035422837b83756986932
Mator <why101@msn.com>
2016-10-11 18:00:26 -0700

    fixed some error captions

--- mod-picker/app/assets/javascripts/Factories/errorsFactory.js
+++ mod-picker/app/assets/javascripts/Factories/errorsFactory.js
@@ -28,7 +28,7 @@ app.service('errorsFactory', function() {
                 group: 3,
                 name: "Deleted References",
                 acronym: "UDR",
-                caption: "UDRs are dirty edits where a record has been overridden in a plugin file, \r\nbut hasn't been changed.  Click for more information.",
+                caption: "UDRs are dirty edits where an object reference has been deleted instead \r\nof being disabled.  Click for more information.",
                 errors: []
             },
             {
@@ -42,14 +42,14 @@ app.service('errorsFactory', function() {
                 group: 5,
                 name: "Unresolved References",
                 acronym: "URR",
-                caption: "URRs are errors where a record references a record that doesn't exist.  \r\nClick for more information.",
+                caption: "URRs are errors where a record references another record that doesn't exist.  \r\nClick for more information.",
                 errors: []
             },
             {
                 group: 6,
                 name: "Unexpected References",
                 acronym: "UER",
-                caption: "UERs are errors where a record references a record in an abnormal fashion. \r\n  Click for more information.",
+                caption: "UERs are errors where a record references another record in an abnormal \r\nfashion. Click for more information.",
                 benign: true,
                 errors: []
             }
