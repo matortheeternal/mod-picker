37888ad1043b355ea533e8b658a4852d4eb72ee3
Mator <why101@msn.com>
2016-10-14 21:08:46 -0700

    Revert "completed refactor"
    This reverts commit 1857596982c5a155ef18a7bdca015ccab5fd97df.

--- mod-picker/app/models/concerns/trackable.rb
+++ mod-picker/app/models/concerns/trackable.rb
@@ -111,7 +111,7 @@ module Trackable
       end
     end
 
-    def track_changed(options={})
+    def track_updated(options={})
       class_eval do
         after_update :changed_event
         def changed_event
