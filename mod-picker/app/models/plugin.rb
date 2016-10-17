3e0fdc374f6ab1fe11cc14f62583e4c7be9ff426
Mator <why101@msn.com>
2016-10-16 18:47:21 -0700

    fixed bug with plugin associations

--- mod-picker/app/models/plugin.rb
+++ mod-picker/app/models/plugin.rb
@@ -42,8 +42,8 @@ class Plugin < ActiveRecord::Base
 
   # notes
   has_many :compatibility_notes, :foreign_key => 'compatibility_plugin_id', :inverse_of => 'compatibility_plugin', :dependent => :destroy
-  has_many :first_load_order_notes, :foreign_key => 'first_plugin_id', :inverse_of => 'first_plugin', :dependent => :destroy
-  has_many :second_load_order_notes, :foreign_key => 'second_plugin_id', :inverse_of => 'second_plugin', :dependent => :destroy
+  has_many :first_load_order_notes, :class_name => 'LoadOrderNote', :foreign_key => 'first_plugin_id', :inverse_of => 'first_plugin', :dependent => :destroy
+  has_many :second_load_order_notes, :class_name => 'LoadOrderNote', :foreign_key => 'second_plugin_id', :inverse_of => 'second_plugin', :dependent => :destroy
 
   accepts_nested_attributes_for :plugin_record_groups, :overrides, :plugin_errors
 
