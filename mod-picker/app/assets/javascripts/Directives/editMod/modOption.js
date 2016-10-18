ca712d8efaec601975c36b91dbf5b92e6fc53029
Mator <why101@msn.com>
2016-10-18 14:20:14 -0700

    now loading existing plugins/destroying unused mod option plugins in mod options

--- mod-picker/app/assets/javascripts/Directives/editMod/modOption.js
+++ mod-picker/app/assets/javascripts/Directives/editMod/modOption.js
@@ -33,9 +33,58 @@ app.controller('modOptionController', function($scope, formUtils, assetUtils) {
         $scope.$emit('destroyUnusedOldOptions');
     };
 
+    $scope.oldPluginChanged = function() {
+        $scope.destroyUnusedOldPlugins();
+    };
+
+    $scope.destroyUnusedOldPlugins = function() {
+        if (!$scope.oldOption) return;
+        var oldPlugins = $scope.oldOption.plugins;
+        oldPlugins.forEach(function(oldPlugin) {
+            var plugin = $scope.option.plugins.find(function(plugin) {
+                return plugin.id == oldPlugin.id;
+            });
+            if (!plugin) {
+                oldPlugin._destroy = true;
+            } else if (oldPlugin.hasOwnProperty('_destroy')) {
+                delete oldPlugin._destroy;
+            }
+        });
+    };
+
+    $scope.pluginMatches = function(a, b) {
+        var filenamesMatch = a.filename === b.filename;
+        var crcsMatch = a.crc_hash === b.crc_hash;
+        var mastersMatch = a.master_plugins === b.master_plugins;
+        var filesizesMatch = a.file_size === b.file_size;
+        var matchSum = filenamesMatch + mastersMatch + filesizesMatch;
+        return crcsMatch || matchSum > 2;
+    };
+
+    $scope.loadExistingPlugin = function(plugin) {
+        var oldPlugins = $scope.oldOption.plugins;
+        var oldPlugin = oldPlugins.find(function(oldPlugin) {
+            return $scope.pluginMatches(plugin, oldPlugin);
+        });
+        if (oldPlugin) {
+            plugin.id = oldPlugin.id;
+        }
+    };
+
+    $scope.loadExistingPlugins = function() {
+        if (!$scope.option.plugins || !$scope.oldOption.plugins) return;
+        $scope.option.plugins.forEach($scope.loadExistingPlugin);
+    };
+
     // prepare nested assets
     if (!$scope.option.nestedAssets) {
         var assets = $scope.option.asset_file_paths || $scope.option.assets;
         $scope.option.nestedAssets = assetUtils.getNestedAssets(assets);
+    }
+
+    // load old option into scope
+    if ($scope.option.id && $scope.oldOptions) {
+        $scope.oldOption = $scope.findOldOption($scope.option.id);
+        $scope.loadExistingPlugins();
     }
 });
