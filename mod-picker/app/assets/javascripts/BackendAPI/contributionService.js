00e3b8dffa9f76a1bde1abe2e8434ffbc8b9971b
Adam <adamtritle@yahoo.com>
2016-10-24 15:45:09 -0500

    reverted merge

--- mod-picker/app/assets/javascripts/BackendAPI/contributionService.js
+++ mod-picker/app/assets/javascripts/BackendAPI/contributionService.js
@@ -21,21 +21,21 @@ app.service('contributionService', function(backend, $q, userTitleService, pageU
     };
 
     this.helpfulMark = function(route, id, helpful) {
-        var helpfulObj = helpful == undefined ? {} : { helpful: helpful };
+        var helpfulObj = helpful == undefined ? {} : {helpful: helpful};
         return backend.post('/' + route + '/' + id + '/helpful', helpfulObj);
     };
 
     this.agreementMark = function(route, id, agree) {
-        var agreeObj = agree == undefined ? {} : { agree: agree };
+        var agreeObj = agree == undefined ? {} : {agree: agree};
         return backend.post('/' + route + '/' + id + '/agreement', agreeObj)
     };
 
     this.hide = function(route, id, hidden) {
-        return backend.post('/' + route + '/' + id + '/hide', { hidden: hidden });
+        return backend.post('/' + route + '/' + id + '/hide', {hidden: hidden});
     };
 
     this.approve = function(route, id, approved) {
-        return backend.post('/' + route + '/' + id + '/approve', { approved: approved });
+        return backend.post('/' + route + '/' + id + '/approve', {approved: approved});
     };
 
     this.submitContribution = function(route, contribution) {
@@ -147,11 +147,6 @@ app.service('contributionService', function(backend, $q, userTitleService, pageU
                 contribution.helpful = helpfulMark.helpful;
             }
         });
-    };
-
-    this.removeModeratorMessage = function(route, id, contribution) {
-        contribution.moderator_message = null;
-        return this.updateContribution(route, id, contribution);
     };
 
     this.removePrompts = function(text_body) {
--- mod-picker/app/assets/javascripts/Directives/contributions/contributionText.js
+++ mod-picker/app/assets/javascripts/Directives/contributions/contributionText.js
@@ -2,27 +2,8 @@ app.directive('contributionText', function() {
     return {
         restrict: 'E',
         templateUrl: '/resources/directives/contributions/contributionText.html',
-        controller: 'contributionTextController',
         scope: {
-            target: '=',
-            modelName: '@'
+            target: '='
         }
-    };
-});
-
-app.controller('contributionTextController', function($scope, $rootScope, contributionService, contributionFactory) {
-    $scope.modelObj = contributionFactory.getModel($scope.modelName);
-    $scope.permissions = angular.copy($rootScope.permissions);
-
-    $scope.removeModeratorMessage = function() {
-        var oldMessage = $scope.target.moderator_message;
-        contributionService.removeModeratorMessage($scope.modelObj.route, $scope.target.id, $scope.target).then(function() {
-            $scope.$emit("successMessage", "Moderator Note removed successfully.");
-        }, function(response) {
-            var params = { label: 'Error removing Moderator Note', response: response };
-            $scope.$emit('errorMessage', params);
-
-            $scope.target.moderator_message = oldMessage;
-        });
     };
 });
