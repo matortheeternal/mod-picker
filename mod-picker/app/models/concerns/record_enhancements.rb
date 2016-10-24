a1ed3271dabca1560afa2d5432d96f464a262f50
Mator <why101@msn.com>
2016-10-24 14:09:45 -0700

    fixed some bugs with save_columns!

--- mod-picker/app/models/concerns/record_enhancements.rb
+++ mod-picker/app/models/concerns/record_enhancements.rb
@@ -5,12 +5,12 @@ module RecordEnhancements
     column_changes = changes.inject({}) { |h, (k, v)|
       h[k] = v[1] if self.class.column_names.include?(k); h }
     return column_changes unless names.present?
-    column_changes.slice(*names)
+    column_changes.symbolize_keys.slice(*names)
   end
 
   def save_columns!(*names)
     column_values = column_changes_hash(names)
-    update_columns(column_values)
+    update_columns(column_values) unless column_values.empty?
     changes_applied
   end
 
