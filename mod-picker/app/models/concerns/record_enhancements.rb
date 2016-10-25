bdc0543db20fd43fb887b314bb8e95a189ca06bb
Mator <why101@msn.com>
2016-10-25 15:13:15 -0700

    removed unused methods from record enhancements concern

--- mod-picker/app/models/concerns/record_enhancements.rb
+++ mod-picker/app/models/concerns/record_enhancements.rb
@@ -13,33 +13,4 @@ module RecordEnhancements
     update_columns(column_values) unless column_values.empty?
     changes_applied
   end
-
-  # Custom counter updating code
-  def update_counter(column, offset)
-    # this updates it in memory
-    self[column] += offset
-    # we do it this way to avoid calling save - we don't want to trigger callbacks
-    operator = offset < 0 ? '-' : '+'
-    self.raw_update("#{column} = #{column} #{operator} #{offset.abs}")
-  end
-
-  def save_counters(columns)
-    hash = {}
-    instance = self
-    columns.each do |column|
-      hash[column] = instance.public_send(column)
-    end
-    self.raw_update(hash)
-  end
-
-  # performs a raw update query to a single record
-  def raw_update(args)
-    self.class.unscoped.where(self.class.primary_key => id).update_all(args)
-  end
-
-  module ClassMethods
-    def update_all_counters(model, column, foreign_key)
-      model.update_all("#{column} = (SELECT COUNT(*) from #{table_name} WHERE #{model.table_name}.id = #{table_name}.#{foreign_key})")
-    end
-  end
 end
