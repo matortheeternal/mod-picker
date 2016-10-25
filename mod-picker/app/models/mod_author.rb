85ef1ce56e2600ff509b89234123dba1f4233a35
Mator <why101@msn.com>
2016-10-25 13:11:31 -0700

    now using counter cache concern for mod authors

--- mod-picker/app/models/mod_author.rb
+++ mod-picker/app/models/mod_author.rb
@@ -15,14 +15,16 @@ class ModAuthor < ActiveRecord::Base
   belongs_to :mod, :inverse_of => 'mod_authors'
   belongs_to :user, :foreign_key => 'user_id', :inverse_of => 'mod_authors'
 
+  # COUNTER CACHE
+  counter_cache_on :user, column: 'authored_mods_count'
+
   # VALIDATIONS
   validates :mod_id, :user_id, presence: true
   validates :user_id, uniqueness: { scope: :mod_id, :message => "Mod Author duplication is not allowed." }
 
   # CALLBACKS
-  after_create :increment_counters
   after_save :update_user_role, :hide_reviews
-  before_destroy :decrement_counters, :update_user_role, :unhide_reviews
+  before_destroy :update_user_role, :unhide_reviews
 
   def self.link_author(model, user_id, username)
     infos = model.where(uploaded_by: username)
@@ -41,10 +43,6 @@ class ModAuthor < ActiveRecord::Base
   end
 
   private
-    def decrement_counters
-      user.update_counter(:authored_mods_count, -1)
-    end
-
     def update_user_role
       user.update_mod_author_role
     end
@@ -56,9 +54,5 @@ class ModAuthor < ActiveRecord::Base
 
     def unhide_reviews
       user.reviews.where(mod_id: mod_id).update_all(hidden: false)
-    end
-
-    def increment_counters
-      user.update_counter(:authored_mods_count, 1)
     end
 end
