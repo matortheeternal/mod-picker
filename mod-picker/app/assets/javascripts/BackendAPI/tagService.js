513653a12a8a966155585d03e2b79666bf4abfd4
Mator <why101@msn.com>
2016-10-26 15:53:29 -0700

    fixed bug with removing last tag from mod or mod list failing

--- mod-picker/app/assets/javascripts/BackendAPI/tagService.js
+++ mod-picker/app/assets/javascripts/BackendAPI/tagService.js
@@ -13,7 +13,7 @@ app.service('tagService', function(backend, $q) {
     this.updateModTags = function(mod, tags) {
         var putData = {
             game_id: mod.game_id,
-            tags: tags
+            tags: tags || []
         };
         return backend.update('/mods/' + mod.id + '/tags', putData);
     };
@@ -21,7 +21,7 @@ app.service('tagService', function(backend, $q) {
     this.updateModListTags = function(mod_list, tags) {
         var putData = {
             game_id: mod_list.game_id,
-            tags: tags
+            tags: tags || []
         };
         return backend.update('/mod_lists/' + mod_list.id + '/tags', putData);
     };
