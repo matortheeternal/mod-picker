b0e84c1080e11622937aa841fff5af2cb85abb2a
Mator <why101@msn.com>
2016-10-26 11:23:19 -0700

    added counter caches to user for authored mods and submitted mods

--- mod-picker/app/models/user.rb
+++ mod-picker/app/models/user.rb
@@ -82,7 +82,8 @@ class User < ActiveRecord::Base
   counter_cache :profile_comments, column: 'comments_count', conditional: { hidden: false }
   counter_cache :comments, column: 'submitted_comments_count', conditional: { hidden: false }
   counter_cache :starred_mods, :starred_mod_lists, :mod_tags, :mod_list_tags, :helpful_marks, :agreement_marks
-  counter_cache :reviews, :compatibility_notes, :install_order_notes, :load_order_notes, :corrections, :tags, conditional: { hidden: false }
+  counter_cache :mod_authors, column: 'authored_mods_count'
+  counter_cache :submitted_mods, :reviews, :compatibility_notes, :install_order_notes, :load_order_notes, :corrections, :tags, conditional: { hidden: false }
   bool_counter_cache :mod_lists, :is_collection, { true => :mod_collections, false => :mod_lists }
 
   # VALIDATIONS
