e5700d372d6005a1fc00c42d2710bae26acad06e
Mator <why101@msn.com>
2016-10-28 22:30:41 -0700

    sticky directive now supports sticking to the bottom of the screen

--- mod-picker/app/assets/javascripts/Directives/shared/sticky.js
+++ mod-picker/app/assets/javascripts/Directives/shared/sticky.js
@@ -1,19 +1,19 @@
 app.directive('sticky', function($timeout) {
     return {
         restrict: 'A',
+        scope: {
+            stickyType: '@stickyType'
+        },
         link: function(scope, element, attrs) {
             var $placeholder, $elem = element[0];
-            var placeholderHeight, bottom, top;
+            var placeholderHeight, phTop;
             var fixed = false;
+            var stickBottom = scope.stickyType === 'bottom';
 
-            // we have to set top on a timeout else the values may be wrong
-            $timeout(function() {
-                top = $elem.getBoundingClientRect().top;
-            });
-
-            var toggleSticky = function(scrollPos) {
+            var toggleStickyTop = function(scrollPos) {
+                var top = $elem.offsetTop;
                 if (fixed) {
-                    // unfix if we are at or above the top of the element + the different in the placeholder and the element's height
+                    // unfix if we are at or above the top of the element + the difference in the placeholder and the element's height
                     var heightDiff = placeholderHeight - $elem.offsetHeight;
                     if (scrollPos <= top + heightDiff) {
                         fixed = false;
@@ -27,14 +27,37 @@ app.directive('sticky', function($timeout) {
                     $placeholder = document.createElement('div');
                     $placeholder.style.height = placeholderHeight + 'px';
                     $elem.parentElement.appendChild($placeholder);
-                    bottom = $placeholder.getBoundingClientRect().bottom;
+                    phTop = $placeholder.offsetTop;
                     $elem.classList.add('sticky');
                 }
             };
 
+            var toggleStickyBottom = function(scrollPos) {
+                scrollPos = scrollPos + window.innerHeight;
+                var bottom = $elem.offsetTop + $elem.offsetHeight;
+                if (fixed) {
+                    // unfix if we are at or below the top of the element
+                    if (scrollPos >= phTop) {
+                        fixed = false;
+                        if ($placeholder) $placeholder.remove();
+                        $elem.classList.remove('sticky');
+                    }
+                } else if (scrollPos < bottom) {
+                    fixed = true;
+                    placeholderHeight = $elem.offsetHeight;
+                    $placeholder = document.createElement('div');
+                    $placeholder.style.height = placeholderHeight + 'px';
+                    $elem.parentElement.appendChild($placeholder);
+                    phTop = $placeholder.offsetTop;
+                    $elem.classList.add('sticky');
+                }
+            };
+
+            var toggleSticky = stickBottom ? toggleStickyBottom : toggleStickyTop;
             window.onscroll = function() {
                 toggleSticky(window.scrollY);
-            }
+            };
+            toggleSticky();
         }
     }
 });
