189848c490042152a3ec219752482a9ecad0a1f4
Mator <why101@msn.com>
2016-10-30 16:52:50 -0700

    fixed bug with add_plugins being inaccessible to the mod list mod model because it was a private method

--- mod-picker/app/models/mod_list_mod_option.rb
+++ mod-picker/app/models/mod_list_mod_option.rb
@@ -14,17 +14,16 @@ class ModListModOption < ActiveRecord::Base
   # VALIDATIONS
   validates :mod_list_mod_id, :mod_option_id, presence: true
 
-  private
-    def add_plugins
-      mod_list = mod_list_mod.mod_list
-      plugin_ids = mod_list.mod_list_plugin_ids
-      mod_option.plugins.each do |plugin|
-        next if plugin_ids.include?(plugin.id)
-        ModListPlugin.create!({
-            mod_list_id: mod_list.id,
-            plugin_id: plugin.id,
-            index: mod_list.plugins_count + 1
-        })
-      end
+  def add_plugins
+    mod_list = mod_list_mod.mod_list
+    plugin_ids = mod_list.mod_list_plugin_ids
+    mod_option.plugins.each do |plugin|
+      next if plugin_ids.include?(plugin.id)
+      ModListPlugin.create!({
+          mod_list_id: mod_list.id,
+          plugin_id: plugin.id,
+          index: mod_list.plugins_count + 1
+      })
     end
+  end
 end
