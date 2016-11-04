a805eed81ba255ceb0872ae6d323429820b06110
Mator <why101@msn.com>
2016-11-03 21:17:42 -0700

    updated approvable module to allow customization of the approval method that is called

--- mod-picker/app/models/concerns/approveable.rb
+++ mod-picker/app/models/concerns/approveable.rb
@@ -3,10 +3,13 @@ module Approveable
 
   included do
     before_create :auto_approve
+
+    class_attribute :approval_method
+    self.approval_method = :has_auto_approval?
   end
 
   def auto_approve
-    self.approved = submitter.has_auto_approval?
+    self.approved = submitter.public_send(self.approval_method)
     true
   end
 end
