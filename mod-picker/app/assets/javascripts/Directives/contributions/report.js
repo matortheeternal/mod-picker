bdcf20ddd23faaefed6c40367cfbbf74fdb0e1cf
Mator <why101@msn.com>
2016-11-04 12:32:28 -0700

    reportController now has a toggleResolved function - marking reports as resolved/unresolved is full working!

--- mod-picker/app/assets/javascripts/Directives/contributions/report.js
+++ mod-picker/app/assets/javascripts/Directives/contributions/report.js
@@ -15,9 +15,21 @@ app.directive('report', function() {
     };
 });
 
-app.controller('reportController', function($scope, $sce, $interpolate, reportsFactory) {
+app.controller('reportController', function($scope, $sce, $interpolate, reportService, reportsFactory) {
     angular.inherit($scope, 'report');
 
     var linkTemplate = reportsFactory.contentLink($scope.report);
     $scope.reportableUrl = $sce.trustAsHtml($interpolate(linkTemplate)($scope));
+
+    $scope.toggleResolved = function() {
+        var newResolvedValue = !$scope.report.resolved;
+        reportService.resolveReport($scope.report.id, newResolvedValue).then(function() {
+            $scope.report.resolved = newResolvedValue;
+            var resolvedText = newResolvedValue ? 'resolved' : 'unresolved';
+            $scope.$emit('successMessage', 'Report marked as '+resolvedText);
+        }, function(response) {
+            var params = { label: 'Error resolving report', response: response };
+            $scope.$emit('errorMessage', params);
+        });
+    };
 });
