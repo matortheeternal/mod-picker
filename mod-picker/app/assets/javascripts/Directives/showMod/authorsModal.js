222194459c9128812bd23f71b65619e8ba93231c
Mator <why101@msn.com>
2016-11-07 14:00:58 -0800

    added logic to authorsModalController for creating new curator requests

--- mod-picker/app/assets/javascripts/Directives/showMod/authorsModal.js
+++ mod-picker/app/assets/javascripts/Directives/showMod/authorsModal.js
@@ -7,7 +7,7 @@ app.directive('authorsModal', function() {
     };
 });
 
-app.controller('authorsModalController', function($scope, formUtils) {
+app.controller('authorsModalController', function($scope, curatorRequestService, contributionService, eventHandlerFactory, contributionFactory, formUtils) {
     // inherited functions
     $scope.unfocusAuthorsModal = formUtils.unfocusModal($scope.toggleAuthorsModal);
 
@@ -18,4 +18,59 @@ app.controller('authorsModalController', function($scope, formUtils) {
     } else {
         $scope.curatorRequestTitle = 'You do not have enough reputation to submit a curator request for \nthis mod.';
     }
+
+    // shared function setup
+    eventHandlerFactory.buildModalMessageHandlers($scope);
+
+    // update the markdown editor
+    $scope.updateEditor = function() {
+        $scope.updateMDE = ($scope.updateMDE || 0) + 1;
+    };
+
+    // start a new curator request
+    $scope.startNewCuratorRequest = function() {
+        $scope.activeRequest = {
+            text_body: contributionFactory.getDefaultTextBody("CuratorRequest")
+        };
+
+        // update editor
+        $scope.updateEditor(true);
+    };
+
+    $scope.validateCuratorRequest = function() {
+        var sanitized_text = contributionService.removePrompts($scope.activeRequest.text_body);
+        $scope.activeRequest.valid = sanitized_text.length > 32;
+    };
+
+    // discard a new curator request object
+    $scope.discardCuratorRequest = function() {
+        delete $scope.activeRequest;
+    };
+
+    // save a curator request
+    $scope.saveCuratorRequest = function() {
+        // return if the appeal is invalid
+        if (!$scope.activeRequest.valid) {
+            return;
+        }
+
+        // submit the appeal
+        var sanitized_text = contributionService.removePrompts($scope.activeRequest.text_body);
+        var curatorRequestObj = {
+            curator_request: {
+                mod_id: $scope.mod.id,
+                text_body: sanitized_text
+            }
+        };
+        $scope.activeRequest.submitting = true;
+
+        // use update or submit contribution
+        curatorRequestService.submitCuratorRequest(curatorRequestObj).then(function() {
+            $scope.$emit("modalSuccessMessage", "Curator request submitted successfully.");
+            $scope.discardCuratorRequest();
+        }, function(response) {
+            var params = {label: 'Error submitting curator request', response: response};
+            $scope.$emit('modalErrorMessage', params);
+        });
+    };
 });
