8bfa39ce4bb1494371603e613a0a41cb091bfe76
Mator <why101@msn.com>
2016-11-06 23:25:05 -0800

    the mod authors modal now displays the uploaded by field from sources and who submitted the mod (and when they submitted it) also added create curator request button

--- mod-picker/app/assets/javascripts/Directives/showMod/authorsModal.js
+++ mod-picker/app/assets/javascripts/Directives/showMod/authorsModal.js
@@ -10,4 +10,12 @@ app.directive('authorsModal', function() {
 app.controller('authorsModalController', function($scope, formUtils) {
     // inherited functions
     $scope.unfocusAuthorsModal = formUtils.unfocusModal($scope.toggleAuthorsModal);
+
+    // initialize variables
+    $scope.canCreateCuratorRequest = $scope.currentUser.reputation.overall > 20;
+    if ($scope.canCreateCuratorRequest) {
+        $scope.curatorRequestTitle = 'Click here if you would like to curate this mod.';
+    } else {
+        $scope.curatorRequestTitle = 'You do not have enough reputation to submit a curator request for \nthis mod.';
+    }
 });
