8866aa5ec317882132b9a440cc857ee3274a78d1
Mator <why101@msn.com>
2016-11-06 16:42:54 -0800

    added a mod filtering param to strong curator request filtering params

--- mod-picker/app/controllers/curator_requests_controller.rb
+++ mod-picker/app/controllers/curator_requests_controller.rb
@@ -56,7 +56,7 @@ class CuratorRequestsController < ApplicationController
 
     # Params we allow filtering on
     def filtering_params
-      params[:filters].permit(:search, :submitter, :submitted, :updated, :state)
+      params[:filters].permit(:search, :submitter, :submitted, :updated, :state, :mod)
     end
 
 end
