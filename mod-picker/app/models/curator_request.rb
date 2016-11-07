0e1ab279dc0dd98d7008a48f30cf14fade7b1ce3
Mator <why101@msn.com>
2016-11-07 13:58:36 -0800

    added logic to add/remove users as curators when curator requests are approved/changed from the approved state to open or denied

--- mod-picker/app/models/curator_request.rb
+++ mod-picker/app/models/curator_request.rb
@@ -22,4 +22,29 @@ class CuratorRequest < ActiveRecord::Base
 
   # VALIDATIONS
   validates :submitted_by, :mod_id, :text_body, presence: true
+
+  # CALLBACKS
+  before_update :toggle_curator
+
+  def curator_attributes
+    { mod_id: mod_id, user_id: submitted_by, role: 2 }
+  end
+
+  def create_curator
+    curator_record = ModAuthor.where(curator_attributes.except(:role)).first
+    ModAuthor.create!(curator_attributes) if curator_record.nil?
+  end
+
+  def destroy_curator
+    curator_record = ModAuthor.where(curator_attributes).first
+    curator_record.destroy unless curator_record.nil?
+  end
+
+  def toggle_curator
+    if attribute_changed?(:state)
+      create_curator if state == "approved"
+      destroy_curator if attribute_was(:state) == "approved"
+    end
+    true
+  end
 end
