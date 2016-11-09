f1156abda3dda3b2b81810f0ea0f8e65ea0e41fc
Mator <why101@msn.com>
2016-11-09 01:54:11 -0800

    created landing controller

--- mod-picker/app/assets/javascripts/landing.js
+++ mod-picker/app/assets/javascripts/landing.js
@@ -10,3 +10,26 @@ var app = angular.module('landing', [
 app.config(['$compileProvider', function($compileProvider) {
     $compileProvider.debugInfoEnabled(false);
 }]);
+
+app.controller('landingController', function($scope) {
+    $scope.games = [
+        {
+            available: true,
+            display_name: "Skyrim",
+            name: "skyrim",
+            explanation: "The Elder Scrolls V: Skyrim"
+        },
+        {
+            available: false,
+            display_name: "Skyrim SE",
+            name: "skyrimse",
+            explanation: "Coming Soon"
+        },
+        {
+            available: false,
+            display_name: "Fallout 4",
+            name: "fallout4",
+            explanation: "Coming Soon"
+        }
+    ];
+});
