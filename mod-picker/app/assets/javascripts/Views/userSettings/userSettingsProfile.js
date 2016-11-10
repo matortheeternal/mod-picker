7073c9c6430e535f8689a0f701acb7c2de257e5f
Mator <why101@msn.com>
2016-11-10 11:12:33 -0800

    applied multiple sizes to user avatar image upload

--- mod-picker/app/assets/javascripts/Views/userSettings/userSettingsProfile.js
+++ mod-picker/app/assets/javascripts/Views/userSettings/userSettingsProfile.js
@@ -1,3 +1,17 @@
 app.controller('userSettingsProfileController', function($scope, titleQuote) {
     $scope.titleQuote = titleQuote;
+    $scope.avatarSizes = [
+        {
+            label: "big",
+            size: 250
+        },
+        {
+            label: 'medium',
+            size: 96
+        },
+        {
+            label: 'small',
+            size: 48
+        }
+    ];
 });
