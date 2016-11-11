e19925b0171b13504fa576179dcc1c63f12967b6
Mator <why101@msn.com>
2016-11-11 12:32:03 -0800

    adjusted after sign up path with devise to go to the sign in page

--- mod-picker/config/routes.rb
+++ mod-picker/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # disable registration
-  devise_for :users, :controllers => { :invitations => "user_invitations" }
+  devise_for :users, :controllers => { :registrations => "registrations", :invitations => "user_invitations" }
   devise_scope :user do
     get '/users/invitation/batch/new' => 'user_invitations#new_batch'
     post '/users/invitation/batch' => 'user_invitations#create_batch'
