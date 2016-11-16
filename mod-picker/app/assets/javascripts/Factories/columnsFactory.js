d8b2134c70d7b3df4f2eafe6121ad7adb1d2d981
Mator <why101@msn.com>
2016-11-16 14:02:36 -0800

    removed unused sort data from columnsFactory.modListModColumns, now rendering mod list custom mod links in list view

--- mod-picker/app/assets/javascripts/Factories/columnsFactory.js
+++ mod-picker/app/assets/javascripts/Factories/columnsFactory.js
@@ -488,11 +488,11 @@ app.service('columnsFactory', function() {
                 link: function(item) {
                     if (item.mod) {
                         return "#/mod/" + item.mod.id;
+                    } else if (item.url) {
+                        return item.url;
                     }
                 },
                 class: "primary-column",
-                sortData: "mods.name",
-                invertSort: true,
                 dynamic: true
             },
             {
@@ -500,32 +500,28 @@ app.service('columnsFactory', function() {
                 visibility: false,
                 label: "Aliases",
                 data: "mod.aliases",
-                class: "aliases-column",
-                invertSort: true
+                class: "aliases-column"
             },
             {
                 group: "General",
                 visibility: true,
                 label: "Authors",
                 data: "mod.authors",
-                class: "author-column",
-                invertSort: true
+                class: "author-column"
             },
             {
                 group: "General",
                 visibility: true,
                 label: "Primary Category",
                 data: "mod.primary_category.name",
-                class: "category-column",
-                unsortable: true
+                class: "category-column"
             },
             {
                 group: "General",
                 visibility: false,
                 label: "Secondary Category",
                 data: "mod.secondary_category.name",
-                class: "category-column",
-                unsortable: true
+                class: "category-column"
             },
             {
                 group: "General",
