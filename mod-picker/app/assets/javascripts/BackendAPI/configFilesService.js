55c6245a490ad5d5adca04c155c7af387fd85073
Mator <why101@msn.com>
2016-11-16 11:50:09 -0800

    fixed some bugs with config files (groups not recovering, active config not being properly set when recovered)

--- mod-picker/app/assets/javascripts/BackendAPI/configFilesService.js
+++ mod-picker/app/assets/javascripts/BackendAPI/configFilesService.js
@@ -1,6 +1,32 @@
 app.service('configFilesService', function() {
     var service = this;
 
+    this.groupHasChildren = function(group) {
+        for (var i = 0; i < group.children.length; i++) {
+            var child = group.children[i];
+            if (!child._destroy) return true;
+        }
+        return false;
+    };
+
+    this.firstAvailableConfig = function(group) {
+        return group.children.find(function(child) {
+            return !child._destroy;
+        });
+    };
+
+    this.recoverConfigFileGroups = function(model) {
+        model.forEach(function(group) {
+            if (group._destroy && service.groupHasChildren(group)) {
+                delete group._destroy;
+                if (!group.activeConfig) {
+                    group.activeConfig = service.firstAvailableConfig(group);
+                    group.activeConfig.active = true;
+                }
+            }
+        });
+    };
+
     this.addConfigFile = function(model, configFile) {
         var foundGroup = model.find(function(group) {
             return group.id == configFile.config_file.mod.id;
