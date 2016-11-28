02d40b6d4f584c4fef218485aa89bc121db74bdb
Mator <why101@msn.com>
2016-11-28 00:01:24 -0800

    created site activity page

---
+++ mod-picker/app/assets/javascripts/Views/base/activity.js
@@ -0,0 +1,51 @@
+app.config(['$stateProvider', function($stateProvider) {
+    $stateProvider.state('base.activity', {
+        templateUrl: '/resources/partials/base/activity.html',
+        controller: 'activityController',
+        url: '/activity',
+        resolve: {
+            permissions: function($q, currentUser, errorService) {
+                var permissions = $q.defer();
+                if (currentUser.permissions.canModerate) {
+                    permissions.resolve(currentUser.permissions);
+                } else {
+                    var errorObj = errorService.frontendError('Error accessing Site Activity.', 'base.activity', 403, 'Not Authorized');
+                    permissions.reject(errorObj);
+                }
+                return permissions.promise;
+            }
+        }
+    });
+}]);
+
+app.controller('activityController', function($scope, $rootScope, permissions, notificationService, helpFactory, notificationsFactory) {
+    // initialize local variables
+    $scope.errors = {};
+    $scope.pages = {
+        events: {}
+    };
+
+    // set page title
+    $scope.$emit('setPageTitle', 'Site Activity');
+    $scope.permissions = permissions;
+    // set help context
+    helpFactory.setHelpContexts($scope, []);
+
+    // prepare notifications factory
+    notificationsFactory.setCurrentUserID($rootScope.currentUser.id);
+
+    // data retrieval
+    $scope.retrieveEvents = function(page) {
+        var options = {
+            page: page || 1
+        };
+        notificationService.retrieveEvents(options, $scope.pages.events).then(function(data) {
+            $scope.events = data.events;
+        }, function(response) {
+            $scope.errors.events = response;
+        });
+    };
+
+    // retrieve data when we first visit the page
+    $scope.retrieveEvents();
+});
