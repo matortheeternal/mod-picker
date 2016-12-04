58ecef389d762991203007f31ff77e5924e0f755
Mator <why101@msn.com>
2016-12-04 00:35:35 -0800

    heavily refactored the mod builder

--- mod-picker/app/builders/mod_builder.rb
+++ mod-picker/app/builders/mod_builder.rb
@@ -1,52 +1,20 @@
 class ModBuilder
-  attr_accessor :mod, :current_user, :params, :tag_names, :nexus_info_id, :lover_info_id, :workshop_info_id, :plugin_ids, :curate
+  attr_accessor :tag_names, :nexus_info_id, :lover_info_id, :workshop_info_id, :plugin_ids, :curate
 
+  # core
   def builder_attributes
     [:tag_names, :nexus_info_id, :lover_info_id, :workshop_info_id, :curate]
   end
 
-  def initialize(current_user, params={})
-    @current_user = current_user
-    @params = params.except(*builder_attributes)
-    builder_attributes.each do |attribute|
-      send(:"#{attribute}=", params[attribute]) if params.has_key?(attribute)
-    end
+  def resource_class
+    Mod
   end
 
-  def mod
-    if @mod.present?
-      @mod
-    elsif @params && @params[:id]
-      @mod = Mod.find_or_initialize_by(id: @params[:id])
-    else
-      @mod = Mod.new
-    end
+  def created_by_key
+    "submitted_by"
   end
 
-  def errors
-    @mod.errors
-  end
-
-  def update
-    mod.updated_by = @current_user.id
-    update!
-    true
-  rescue Exception => x
-    raise x unless mod.errors.present?
-    false
-  end
-
-  def update!
-    ActiveRecord::Base.transaction do
-      mod.attributes = @params
-      self.before_save
-      self.before_update
-      mod.save!
-      self.after_update
-      self.after_save
-    end
-  end
-
+  # callbacks
   def before_update
     hide_contributions
     swap_mod_list_mods_tools_counts
@@ -55,24 +23,6 @@ class ModBuilder
   def after_update
     update_adult
     update_mod_list_mods
-  end
-
-  def save
-    mod.assign_attributes(@params)
-    mod.submitted_by = @current_user.id
-    save!
-    true
-  rescue Exception => x
-    raise x unless mod.errors.present?
-    false
-  end
-
-  def save!
-    ActiveRecord::Base.transaction do
-      self.before_save
-      mod.save!
-      self.after_save
-    end
   end
 
   def before_save
@@ -90,7 +40,7 @@ class ModBuilder
   def set_config_file_game_ids
     if @config_files_attributes
       @config_files_attributes.each do |config_file|
-        config_file[:game_id] = mod.game_id
+        config_file[:game_id] = resource.game_id
       end
     end
   end
@@ -100,7 +50,7 @@ class ModBuilder
   end
 
   def old_sources_present
-    mod.nexus_infos.present? || mod.lover_infos.present? || mod.workshop_infos.present? || mod.custom_sources.present?
+    resource.nexus_infos.present? || resource.lover_infos.present? || resource.workshop_infos.present? || resource.custom_sources.present?
   end
 
   def validate_sources
@@ -111,25 +61,17 @@ class ModBuilder
   end
 
   def hide_contributions
-    if mod.attribute_changed?(:hidden) && mod.hidden
-      # prepare some helper variables
-      plugin_ids = mod.plugins.ids
-      cnote_ids = mod.compatibility_notes.ids
-      inote_ids = mod.install_order_notes.ids
-      lnote_ids = mod.load_order_notes.ids
-
-      # hide content
-      mod.mod_list_mods.destroy_all
-      mod.reviews.update_all(:hidden => true)
-      mod.corrections.update_all(:hidden => true)
-      mod.compatibility_notes.update_all(:hidden => true)
-      mod.install_order_notes.update_all(:hidden => true)
-      mod.load_order_notes.update_all(:hidden => true)
-      Correction.correctables("CompatibilityNote", cnote_ids).update_all(:hidden => true)
-      Correction.correctables("InstallOrderNote", inote_ids).update_all(:hidden => true)
-      Correction.correctables("LoadOrderNote", lnote_ids).update_all(:hidden => true)
-    elsif mod.attribute_changed?(:disable_reviews) && mod.disable_reviews
-      mod.reviews.update_all(:hidden => true)
+    if resource.attribute_changed?(:hidden) && resource.hidden
+      resource.reviews.update_all(:hidden => true)
+      resource.corrections.update_all(:hidden => true)
+      resource.compatibility_notes.update_all(:hidden => true)
+      resource.install_order_notes.update_all(:hidden => true)
+      resource.load_order_notes.update_all(:hidden => true)
+      Correction.correctables("CompatibilityNote", resource.compatibility_notes.ids).update_all(:hidden => true)
+      Correction.correctables("InstallOrderNote", resource.install_order_notes.ids).update_all(:hidden => true)
+      Correction.correctables("LoadOrderNote", resource.load_order_notes.ids).update_all(:hidden => true)
+    elsif resource.attribute_changed?(:disable_reviews) && resource.disable_reviews
+      resource.reviews.update_all(:hidden => true)
     end
   end
 
@@ -140,10 +82,10 @@ class ModBuilder
   end
 
   def update_mod_list_mods
-    if mod.previous_changes.has_key?(:is_utility)
-      mod.mod_list_mods.includes(:mod_list).each do |mod_list_mod|
+    if resource.previous_changes.has_key?(:is_utility)
+      resource.mod_list_mods.includes(:mod_list).each do |mod_list_mod|
         mod_list_mod.group_id = nil
-        mod_list_mod.is_utility = mod.is_utility
+        mod_list_mod.is_utility = resource.is_utility
         mod_list_mod.get_index
         mod_list_mod.save!
       end
@@ -170,7 +112,7 @@ class ModBuilder
   def link_source(info_id, model)
     if info_id
       info = model.find(info_id)
-      info.mod_id = mod.id
+      info.mod_id = resource.id
       info.link_uploader
       info.save!
     end
@@ -178,32 +120,14 @@ class ModBuilder
 
   def create_tags
     if @tag_names
-      @tag_names.each do |text|
-        tag = Tag.find_by(text: text, game_id: mod.game_id)
-
-        # create tag if we couldn't find it
-        if tag.nil?
-          tag = Tag.create!({
-              text: text,
-              game_id: mod.game_id,
-              submitted_by: @current_user.id
-          })
-        end
-
-        # associate tag with mod
-        ModTag.create!({
-            mod_id: mod.id,
-            tag_id: tag.id,
-            submitted_by: @current_user.id
-        })
-      end
+      TagBuilder.create_tags(resource, @current_user, @tag_names)
     end
   end
 
   def create_curator
     if @curate
       ModAuthor.create!({
-          mod_id: mod.id,
+          mod_id: resource.id,
           user_id: @current_user.id,
           role: 2
       })
@@ -211,34 +135,30 @@ class ModBuilder
   end
 
   def swap_mod_list_mods_tools_counts
-    if mod.attribute_changed?(:is_utility)
-      tools_operator = mod.is_utility ? "+" : "-"
-      mods_operator = mod.is_utility ? "-" : "+"
-      mod_list_ids = mod.mod_lists.ids
+    if resource.attribute_changed?(:is_utility)
+      tools_operator = resource.is_utility ? "+" : "-"
+      mods_operator = resource.is_utility ? "-" : "+"
+      mod_list_ids = resource.mod_lists.ids
       ModList.where(id: mod_list_ids).update_all("tools_count = tools_count #{tools_operator} 1, mods_count = mods_count #{mods_operator} 1")
     end
   end
 
   def substitute_custom_mods
-    mod.sources_array.each do |source|
+    resource.sources_array.each do |source|
       ModListCustomMod.substitute_for_url(source.url, mod)
     end
   end
 
   def make_custom_mods
-    mod.mod_list_mods.find_each do |mod_list_mod|
+    resource.mod_list_mods.find_each do |mod_list_mod|
       ModListCustomMod.create_from_mod_list_mod(mod_list_mod)
       mod_list_mod.destroy
     end
   end
 
   def manage_custom_mods
-    if mod.was_visible != mod.visible
-      mod.visible ? substitute_custom_mods : make_custom_mods
+    if resource.was_visible != resource.visible
+      resource.visible ? substitute_custom_mods : make_custom_mods
     end
-  end
-
-  def self.model_name
-    'Mod'
   end
 end
