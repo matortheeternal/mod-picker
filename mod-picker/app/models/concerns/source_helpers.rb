9811ad9a7a9973b95be67f7a2e02aa77a3b469ff
Mator <why101@msn.com>
2016-12-10 13:28:56 -0800

    created source_search_scope helper and refactored source_scope helper

--- mod-picker/app/models/concerns/source_helpers.rb
+++ mod-picker/app/models/concerns/source_helpers.rb
@@ -2,46 +2,35 @@ module SourceHelpers
   extend ActiveSupport::Concern
 
   module ClassMethods
-    def get_source_table(source_key)
-      case source_key.to_sym
-        when :nexus
-          return :nexus_infos
-        when :lab
-          return :lover_infos
-        when :workshop
-          return :workshop_infos
-        when :other
-          return :custom_sources
-        else
-          raise("Invalid source key!")
+    def get_source_class(source_key)
+      {
+          nexus: NexusInfo,
+          lab: LoverInfo,
+          workshop: WorkshopInfo,
+          other: CustomSource
+      }[source_key]
+    end
+
+    def source_search_scope(attribute, options={})
+      class_eval do
+          scope attribute, -> (search) {
+            sources = search[:sources]
+            where(sources.map { |source|
+              get_source_class(source).arel_table[attribute.to_sym].matches("%#{search[:value]}%")
+            }.inject(:or))
+          }
       end
     end
 
     def source_scope(attribute, options={})
-      # single site scope
-      if options[:sites].length == 1
-        column = attribute || options[:alias][0]
-        source_table = get_source_table(options[:sites][0])
-        class_eval <<-buildscope
-          scope :#{attribute}, -> (range) {
-            where(:#{source_table} => {#{column}: (range[:min]..range[:max])}) }
-        buildscope
-      else
-        scope_wheres = []
-        options[:sites].each_with_index do |site,index|
-          source_table = get_source_table(site)
-          column_name = options[:alias] ? options[:alias][index] : attribute
-          scope_wheres.push("query = query.where(:#{source_table} => { #{column_name}: (range[:min]..range[:max]) }) if sources[:#{site}]")
-        end
-        class_eval <<-buildscope
-          scope :#{attribute}, -> (range) {
-            sources = range[:sources]
-
-            query = where(nil)
-            #{scope_wheres.join("\n")}
-            query
-          }
-        buildscope
+      class_eval do
+        scope attribute, -> (range) {
+          sources = range[:sources]
+          where(sources.map.with_index { |source, index|
+            column = options[:alias] ? options[:alias][index] : attribute
+            get_source_class(source).arel_table[column.to_sym].in(range[:min]..range[:max])
+          }.inject(:or))
+        }
       end
     end
   end
