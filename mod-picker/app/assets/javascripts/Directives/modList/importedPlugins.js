2381ac0b3dfd8569777bc114352520bdcbdf35b5
Mator <why101@msn.com>
2016-12-14 13:17:26 -0800

    created importedPlugins directive and controller

---
+++ mod-picker/app/assets/javascripts/Directives/modList/importedPlugins.js
@@ -0,0 +1,62 @@
+app.directive('importedPlugins', function() {
+    return {
+        restrict: 'E',
+        templateUrl: '/resources/directives/modList/importedPlugins.html',
+        controller: 'importedPluginsController',
+        scope: {
+            importedPlugins: '=?'
+        }
+    };
+});
+
+app.controller('importedPluginsController', function($scope, pluginService, columnsFactory, actionsFactory, tableUtils) {
+    // inherit scope attributes
+    angular.inherit($scope, 'importedPlugins');
+
+    // initialize variables
+    $scope.columns = columnsFactory.pluginImportColumns();
+    $scope.actions = actionsFactory.modListImportActions();
+    $scope.sort = {
+        column: '',
+        direction: 'ASC'
+    };
+
+    // expose service functions
+    $scope.sortColumn = tableUtils.sortColumn;
+    $scope.resolve = tableUtils.resolve($scope);
+
+    // local functions
+    $scope.addMatchingPlugins = function(sourceData, matchData) {
+        var importedPlugins = matchData.map(function(matchData, index) {
+            return {
+                filename: sourceData[index].plugin_filename,
+                mod_name: matchData && matchData.mod.name,
+                mod_id: matchData && matchData.mod.id,
+                id: matchData && matchData.id,
+                custom: !matchData
+            };
+        });
+        $scope.$applyAsync(function() {
+            $scope.loading.importedPlugins = false;
+            $scope.importedPlugins = ($scope.importedPlugins || []).concat(importedPlugins);
+        });
+    };
+
+    $scope.errorFindingPlugins = function(response) {
+        $scope.loading = false;
+        var params = { text: "Error finding matching plugins", response: response };
+        $scope.$emit('modalErrorMessage', params);
+    };
+
+    $scope.findMatchingPlugins = function(sourceData) {
+        $scope.loading = true;
+        pluginService.searchPluginsBatch(sourceData).then(function(matchData) {
+            $scope.addMatchingPlugins(sourceData, matchData);
+            $scope.$parent.$broadcast('addMatchingPluginMods', matchData);
+        }, $scope.errorFindingPlugins);
+    };
+
+    $scope.$on('findMatchingPlugins', function(e, sourceData) {
+        $scope.findMatchingPlugins(sourceData);
+    });
+});
