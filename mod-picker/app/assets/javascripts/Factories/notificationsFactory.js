b94c03aeccb799b9bacd35bc925e7f14f12cf7e0
Mator <why101@msn.com>
2016-12-20 18:29:08 -0800

    added missing notification for user status change (helper role)

--- mod-picker/app/assets/javascripts/Factories/notificationsFactory.js
+++ mod-picker/app/assets/javascripts/Factories/notificationsFactory.js
@@ -154,6 +154,7 @@ app.service('notificationsFactory', function() {
             banned: "You have been banned.",
             restricted: "Your account has been restricted.  You can no longer make public contributions.",
             user: "Your account has been returned to normal status.",
+            helper: "You are now a helper on Mod Picker.",
             moderator: "You are now a moderator on Mod Picker!",
             admin: "You are now a site admin.  Remember, with great power comes great responsibility."
         },
