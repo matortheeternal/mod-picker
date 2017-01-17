4e4e4e81bf588d2197783e59e08b81d62db6337b
Mator <why101@msn.com>
2017-01-16 20:33:19 -0800

    added seeding of user titles to skyrim special edition migration

--- mod-picker/db/migrate/20170116215253_add_skyrim_special_edition.rb
+++ mod-picker/db/migrate/20170116215253_add_skyrim_special_edition.rb
@@ -4,8 +4,12 @@ class AddSkyrimSpecialEdition < ActiveRecord::Migration
   def change
     change_column :games, :nexus_name, :string, limit: 32
 
+    # helper variables
     mator = User.find(1)
     gameBethesda = Game.find_by(abbr_name: "bg")
+    gameSkyrim = Game.find_by(abbr_name: "sk")
+
+    # game
     gameSkyrimSE = Game.create(
         parent_game_id: gameBethesda.id,
         display_name: "Skyrim SE",
@@ -16,7 +20,16 @@ class AddSkyrimSpecialEdition < ActiveRecord::Migration
         nexus_name: "skyrimspecialedition",
         steam_app_ids: "489830"
     )
+
+    # record groups
     load_record_groups(gameSkyrimSE, "skyrimse.json")
+
+    # official mod and plugins
     seed_skyrimse_official_content(mator)
+
+    # user titles
+    UserTitle.where(game_id: gameSkyrim.id).each do |t|
+      UserTitle.create(game_id: gameSkyrimSE.id, title: t.title, rep_required: t.rep_required)
+    end
   end
 end
