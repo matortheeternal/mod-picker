a759e8e44995b7a237fb563216cc0dfd5fa4d4ff
Mator <why101@msn.com>
2017-01-23 12:32:27 -0800

    moderation cp now displays game-specific counts

--- mod-picker/app/assets/javascripts/BackendAPI/moderationService.js
+++ mod-picker/app/assets/javascripts/BackendAPI/moderationService.js
@@ -1,5 +1,7 @@
 app.service('moderationService', function(backend) {
     this.retrieveStats = function() {
-        return backend.retrieve("/moderator_cp");
+        return backend.retrieve("/moderator_cp", {
+            game: window._current_game_id
+        });
     };
 });
