aaafb15eb74312b70029f628bfcb1d68f564ec19
Mator <why101@msn.com>
2017-01-23 15:28:40 -0800

    plugin master associations are now constructed in a game-specific fashion

--- mod-picker/app/models/plugin.rb
+++ mod-picker/app/models/plugin.rb
@@ -77,9 +77,9 @@ class Plugin < ActiveRecord::Base
     end
   end
 
-  def self.find_master_plugin(master)
-    Plugin.where(filename: master[:filename], crc_hash: master[:crc_hash]).first ||
-        Plugin.where(filename: master[:filename]).first
+  def self.find_master_plugin(game, master)
+    Plugin.game(game).where(filename: master[:filename], crc_hash: master[:crc_hash]).first ||
+        Plugin.game(game).where(filename: master[:filename]).first
   end
 
   def update_lazy_counters
@@ -96,7 +96,7 @@ class Plugin < ActiveRecord::Base
   def create_associations
     if @master_plugins
       @master_plugins.each_with_index do |master, index|
-        master_plugin = Plugin.find_master_plugin(master)
+        master_plugin = Plugin.find_master_plugin(game_id, master)
         if master_plugin.nil?
           dummy_masters.create(filename: master[:filename], index: index)
         else
