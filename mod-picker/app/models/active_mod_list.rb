73d5d93a60197c4cd2b81a5f4f3abe124a00be1f
Mator <why101@msn.com>
2017-01-23 13:34:47 -0800

    created active mod list model

---
+++ mod-picker/app/models/active_mod_list.rb
@@ -0,0 +1,17 @@
+class ActiveModList < ActiveRecord::Base
+  include BetterJson
+
+  belongs_to :game
+  belongs_to :user
+  belongs_to :mod_list
+
+  def self.apply(game, user, mod_list)
+    if mod_list.present?
+      ActiveModList.create(game_id: game, user_id: user.id, mod_list_id: mod_list.id)
+    end
+  end
+
+  def self.clear(game, user)
+    ActiveModList.where(user_id: user.id, game_id: game).delete_all
+  end
+end
