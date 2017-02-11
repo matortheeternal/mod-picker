b8066e726dd87f93375721f6a94c8618d2e8d1e2
Mator <why101@msn.com>
2017-02-10 22:04:31 -0800

    added notice_type enum column to mod details migration

--- mod-picker/db/migrate/20170210191437_add_mod_details.rb
+++ mod-picker/db/migrate/20170210191437_add_mod_details.rb
@@ -2,7 +2,8 @@ class AddModDetails < ActiveRecord::Migration
   def change
     add_column :mods, :description, :string, limit: 512, after: :authors
     add_column :mods, :notice, :string, limit: 128, after: :description
-    add_column :mods, :support_link, :string, limit: 256, after: :notice
+    add_column :mods, :notice_type, :integer, limit: 1, after: :notice
+    add_column :mods, :support_link, :string, limit: 256, after: :notice_type
     add_column :mods, :issues_link, :string, limit: 256, after: :support_link
     add_column :mods, :show_details_tab, :boolean, default: false, null: false, after: :is_utility
   end
