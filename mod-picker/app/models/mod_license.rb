fdc12fa106ec524ffd5db03cbe4a801a9ba89464
Mator <why101@msn.com>
2017-02-15 14:43:44 -0800

    created ModLicense model

---
+++ mod-picker/app/models/mod_license.rb
@@ -0,0 +1,23 @@
+class ModLicense < ActiveRecord::Base
+  include RecordEnhancements, CounterCache, BetterJson
+
+  # ASSOCIATIONS
+  belongs_to :mod
+  belongs_to :license
+  belongs_to :license_option
+
+  # VALIDATIONS
+  validates :text_body, length: { max: 65536 }
+
+  # CALLBACKS
+  before_create :inherit_terms
+
+  def term_keys
+    %w(credit commercial redistribution modification private_use include)
+  end
+
+  def inherit_terms
+    return if license_id.nil?
+    term_keys.each { |term| public_send("#{term}=", license.public_send(term)) }
+  end
+end
