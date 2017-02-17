bd023690b29167d142c2e04a9112913d113f79a1
Mator <why101@msn.com>
2017-02-17 14:20:44 -0800

    changed ModLicense association on Mod to be a has_many association instead of a has_one

--- mod-picker/app/models/mod.rb
+++ mod-picker/app/models/mod.rb
@@ -122,7 +122,7 @@ class Mod < ActiveRecord::Base
   has_many :config_files, :inverse_of => 'mod', :dependent => :destroy
 
   # license
-  has_one :mod_license
+  has_many :mod_licenses
 
   # users who can edit the mod
   has_many :mod_authors, :inverse_of => 'mod', :dependent => :destroy
@@ -148,7 +148,7 @@ class Mod < ActiveRecord::Base
   accepts_nested_attributes_for :mod_options, allow_destroy: true
   accepts_nested_attributes_for :custom_sources, allow_destroy: true
   accepts_nested_attributes_for :config_files, allow_destroy: true
-  accepts_nested_attributes_for :mod_license, allow_destroy: true
+  accepts_nested_attributes_for :mod_licenses, allow_destroy: true
   # cannot update required mods
   accepts_nested_attributes_for :required_mods, reject_if: proc {
       |attributes| attributes[:id] && !attributes[:_destroy]
