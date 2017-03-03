f33e9ac37199b23d675105eaceb928eaac416148
Mator <why101@msn.com>
2017-03-03 13:56:26 -0800

    updated sortControl to work with dynamic sort options

--- mod-picker/app/assets/javascripts/Directives/browse/sortControl.js
+++ mod-picker/app/assets/javascripts/Directives/browse/sortControl.js
@@ -14,4 +14,14 @@ app.controller('sortController', function($scope) {
     // inherited scope attributes
     angular.inherit($scope, 'sort');
     angular.inherit($scope, 'sortOptions');
+
+    $scope.$parent.$watch('sortOptions', function() {
+        $scope.sortOptions = $scope.$parent.sortOptions;
+        var currentSortOption = $scope.sortOptions.find(function(sortOption) {
+            return sortOption.value === $scope.sort.column;
+        });
+        if (!currentSortOption) {
+            $scope.sort.column = $scope.sortOptions[0].value;
+        }
+    });
 });
