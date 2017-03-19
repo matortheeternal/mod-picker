44275d41ba136744d192cb4900489bec95df4457
Mator <why101@msn.com>
2017-03-19 01:39:42 -0700

    updated tag_groups#index with includes and game filter

--- mod-picker/app/controllers/tag_groups_controller.rb
+++ mod-picker/app/controllers/tag_groups_controller.rb
@@ -1,7 +1,7 @@
 class TagGroupsController < ApplicationController
   # GET /tag_groups
   def index
-    @tag_groups = TagGroup.category(params[:category])
+    @tag_groups = TagGroup.game(params[:game]).category(params[:category]).includes(:tag_group_tags => :tag)
     respond_with_json(@tag_groups)
   end
 end
