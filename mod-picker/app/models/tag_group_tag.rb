7ef871044a56af334067f55b9aaef9e2ba8a3415
Mator <why101@msn.com>
2017-03-19 01:25:04 -0700

    created TagGroupTag model

---
+++ mod-picker/app/models/tag_group_tag.rb
@@ -0,0 +1,21 @@
+class TagGroupTag < ActiveRecord::Base
+  include RecordEnhancements, CounterCache, BetterJson
+
+  # ASSOCIATIONS
+  belongs_to :tag_group
+  belongs_to :tag
+  has_many :mod_tags, -> (object) { joins(:mod).where("mods.primary_category_id in (:ids) OR mods.secondary_category_id in (:ids)", ids: object.tag_group.category_id) }, through: "tag"
+
+  # COUNTER CACHE
+  counter_cache :mod_tags, column: 'mod_tags_count'
+
+  # VALIDATIONS
+  validates :tag_group_id, :tag_id, :index, presence: true
+
+  # CALLBACKS
+  after_create :reset_mod_tags_counter
+
+  def reset_mod_tags_counter
+    reset_counter!(:mod_tags)
+  end
+end
