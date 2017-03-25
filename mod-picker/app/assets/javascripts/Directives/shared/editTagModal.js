e160c06dd439e37c2027fbcc04970ca4947e188d
Mator <why101@msn.com>
2017-03-24 17:28:51 -0700

    updated editTagModal for replacing tags

--- mod-picker/app/assets/javascripts/Directives/shared/editTagModal.js
+++ mod-picker/app/assets/javascripts/Directives/shared/editTagModal.js
@@ -7,16 +7,27 @@ app.directive('editTagModal', function() {
     }
 });
 
-app.controller('editTagModalController', function($scope, tagService, formUtils, eventHandlerFactory) {
+app.controller('editTagModalController', function($scope, $q, $timeout, tagService, formUtils, eventHandlerFactory) {
     // inherited functions
     $scope.unfocusTagModal = formUtils.unfocusModal($scope.toggleTagModal);
 
     // shared function setup
     eventHandlerFactory.buildModalMessageHandlers($scope);
 
+    $scope.searchTags = function(s) {
+        var action = $q.defer();
+        $timeout(function() {
+            var str = s.toLowerCase();
+            var matches = tagService.getTagMatches($scope.allTags, str);
+            tagService.sortTagMatches(str, matches);
+            action.resolve(matches);
+        }, 100);
+        return action.promise;
+    };
+
     $scope.saveTag = function() {
         tagService.updateTag($scope.activeTag).then(function() {
-            $scope.$emit('modalSuccessMessage', 'Updated tag "' + $scope.activeTag.text + '"successfully');
+            $scope.$emit('modalSuccessMessage', 'Updated tag "' + $scope.activeTag.text + '" successfully');
             $scope.$applyAsync(function() {
                 $scope.originalTag.text = $scope.activeTag.text;
             });
@@ -28,4 +39,29 @@ app.controller('editTagModalController', function($scope, tagService, formUtils,
             $scope.$emit('modalErrorMessage', params);
         });
     };
+
+    $scope.replaceTag = function() {
+        var oldTagId = $scope.originalTag.id, newTagId = $scope.activeTag.id;
+        tagService.replaceTag(oldTagId, newTagId).then(function() {
+            $scope.$emit('modalSuccessMessage', 'Replaced tag "' + $scope.originalTag.text + '" successfully');
+            $scope.$applyAsync(function() {
+                $scope.originalTag.hidden = true;
+                $scope.originalTag.mods_count = 0;
+                $scope.originalTag.mod_lists_count = 0;
+            });
+        }, function(response) {
+            var params = {
+                text: 'Error replacing tag: '+$scope.originalTag.text,
+                response: response
+            };
+            $scope.$emit('modalErrorMessage', params);
+        });
+    };
+
+    // tag retrieval
+    if (!$scope.allTags) {
+        tagService.retrieveAllTags().then(function(data) {
+            $scope.allTags = data;
+        });
+    }
 });
