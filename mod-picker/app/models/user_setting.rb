da99073d407251f94eb3e178169afae5bbac205c
Mator <why101@msn.com>
2017-04-08 11:54:57 -0700

    now setting default theme values when user settings created

--- mod-picker/app/models/user_setting.rb
+++ mod-picker/app/models/user_setting.rb
@@ -7,4 +7,14 @@ class UserSetting < ActiveRecord::Base
   validates :user_id, presence: true
   validates :skyrim_theme, :skyrimse_theme, :fallout4_theme, :fallout3_theme, :falloutnv_theme, :oblivion_theme, length: {maximum: 64}
   validates :allow_comments, :show_notifications, :email_notifications, :email_public, :allow_adult_content, :allow_nexus_mods, :allow_lovers_lab, :allow_steam_workshop, inclusion: [true, false]
+
+  # SET DEFAULT THEMES
+  before_create :set_default_themes
+
+  private
+    def set_default_themes
+      self.skyrim_theme = 'High Hrothgar'
+      self.skyrimse_theme = 'Falkreath'
+      self.fallout4_theme = 'Workshop'
+    end
 end
