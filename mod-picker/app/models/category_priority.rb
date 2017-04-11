deba2ded26a1816ca3c716fb2a307956edb40647
Mator <why101@msn.com>
2017-04-10 19:43:43 -0700

    adjusted some association foreign_keys

--- mod-picker/app/models/category_priority.rb
+++ mod-picker/app/models/category_priority.rb
@@ -1,8 +1,8 @@
 class CategoryPriority < ActiveRecord::Base
   self.primary_keys = :dominant_id, :recessive_id
 
-  belongs_to :dominant_category, :class_name => 'Category', :foreign_key => 'category_id'
-  belongs_to :recessive_category, :class_name => 'Category', :foreign_key => 'category_id'
+  belongs_to :dominant_category, :class_name => 'Category', :foreign_key => 'dominant_id'
+  belongs_to :recessive_category, :class_name => 'Category', :foreign_key => 'recessive_id'
 
   # VALIDATIONS
   validates :dominant_id, :recessive_id, :description, presence: true
