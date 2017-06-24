8a6dca80a9e6ca8fda409b63f986f840924919c0
Mator <why101@msn.com>
2017-06-24 13:06:02 -0700

    added caching to worldspaces controller

--- mod-picker/app/controllers/worldspaces_controller.rb
+++ mod-picker/app/controllers/worldspaces_controller.rb
@@ -1,7 +1,9 @@
-class WorkshopInfosController < ApplicationController
-  # GET /worldspaces?game_id=2
+class WorldspacesController < ApplicationController
+  # GET /worldspaces?game=2
   def index
-    @worldspaces = Worldspace.game(params[:game]).includes(:plugin, :cells)
-    render json: @worldspaces
+    render json: Rails.cache.fetch("worldspaces/#{params[:game]}", expires_in: 1.year) do
+      @worldspaces = Worldspace.game(params[:game]).includes(:plugin, :cells)
+      @worldspaces.to_json
+    end
   end
 end
