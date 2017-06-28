5749bd465b80a991d4906927d599353d28a307b4
Mator <why101@msn.com>
2017-06-27 20:11:28 -0700

    added static caching for games

--- mod-picker/app/controllers/games_controller.rb
+++ mod-picker/app/controllers/games_controller.rb
@@ -1,8 +1,8 @@
 class GamesController < ApplicationController
   # GET /games
   def index
-    @games = Game.all
-
-    render json: @games
+    render json: static_cache("games") {
+      Game.all.to_json
+    }
   end
 end
