3d02d03074f756c76d32a62bcc01db412b10cf96
Mator <why101@msn.com>
2017-06-27 20:12:06 -0700

    added static caching for review sections

--- mod-picker/app/controllers/api/v1/review_sections_controller.rb
+++ mod-picker/app/controllers/api/v1/review_sections_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::ReviewSectionsController < Api::ApiController
   # GET /review_sections
   def index
-    render json: ReviewSection.all
+    render json: static_cache("review_sections") {
+      ReviewSection.all.to_json
+    }
   end
 end
--- mod-picker/app/controllers/review_sections_controller.rb
+++ mod-picker/app/controllers/review_sections_controller.rb
@@ -1,6 +1,8 @@
 class ReviewSectionsController < ApplicationController
   # GET /review_sections
   def index
-    render json: ReviewSection.all
+    render json: static_cache("review_sections") {
+      ReviewSection.all.to_json
+    }
   end
 end
