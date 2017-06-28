a386591683b5a32ab1c469f9e8a1d5f642a58caf
Mator <why101@msn.com>
2017-06-27 18:54:54 -0700

    created mod analysis decorator and view

---
+++ mod-picker/app/decorators/mod_analysis_decorator.rb
@@ -0,0 +1,11 @@
+class ModAnalyisDecorator < Decorator
+  attr_accessor :mod
+
+  def initialize(mod)
+    @mod = mod
+  end
+
+  def method_missing(method_sym, *arguments, &block)
+    @mod.public_send(method_sym, *arguments)
+  end
+end
