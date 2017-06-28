1d0653416151e096688d48ace88a437eeccf0f56
Mator <why101@msn.com>
2017-06-27 20:11:59 -0700

    added static caching for record groups

--- mod-picker/app/controllers/api/v1/record_groups_controller.rb
+++ mod-picker/app/controllers/api/v1/record_groups_controller.rb
@@ -2,11 +2,13 @@ class Api::V1::RecordGroupsController < Api::ApiController
   # GET /record_groups
   def index
     if params.has_key?(:game_id)
-      @record_groups = RecordGroup.where(game_id: params[:game_id])
+      render json: static_cache("record_groups/#{params[:game_id]}") {
+        RecordGroup.game(params[:game_id]).to_json
+      }
     else
-      @record_groups = RecordGroup.all
+      render json: static_cache("record_groups") {
+        RecordGroup.all.to_json
+      }
     end
-
-    render json: @record_groups
   end
 end
