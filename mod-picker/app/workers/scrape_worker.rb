9eed6dbf77eda9650939223d24452292c94eea11
Colin <why101@msn.com>
2018-03-30 08:05:53 -0700

    fixed scrape worker and rescrape method - author opt out exceptions are now ignored and sites are rescraped regardless of whether statistics are allowed to be scraped

--- mod-picker/app/workers/scrape_worker.rb
+++ mod-picker/app/workers/scrape_worker.rb
@@ -3,25 +3,21 @@ class ScrapeWorker
 
   def perform
     # rescrape nexus if we're allowed to scrape nexus statistics
-    if Rails.application.config.scrape_nexus_statistics
-      puts "\nRescraping Nexus Mods"
-      NexusInfo.can_recrape.find_each do |nexus_info|
-        nexus_info.scrape
-      end
+    puts "\nRescraping Nexus Mods"
+    NexusInfo.can_recrape.find_each do |nexus_info|
+      nexus_info.rescrape
     end
 
     # rescrape lab
     puts "\nRescraping Lover's Lab"
     LoverInfo.can_recrape.find_each do |lover_info|
-      lover_info.scrape
+      lover_info.rescrape
     end
 
     # rescrape workshop
-    if Rails.application.config.scrape_workshop_statistics
-      puts "\nRescraping Steam Workshop"
-      WorkshopInfo.can_recrape.find_each do |workshop_info|
-        workshop_info.scrape
-      end
+    puts "\nRescraping Steam Workshop"
+    WorkshopInfo.can_recrape.find_each do |workshop_info|
+      workshop_info.rescrape
     end
   end
 end
