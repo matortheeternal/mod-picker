443b509ad62323cb424a06326eb9f84486ba5b3c
Colin <why101@msn.com>
2018-05-17 11:15:08 -0700

    fixed exception handling when scraping

--- mod-picker/app/workers/scrape_worker.rb
+++ mod-picker/app/workers/scrape_worker.rb
@@ -5,19 +5,31 @@ class ScrapeWorker
     # rescrape nexus if we're allowed to scrape nexus statistics
     puts "\nRescraping Nexus Mods"
     NexusInfo.can_recrape.find_each do |nexus_info|
-      nexus_info.rescrape
+      begin
+        nexus_info.rescrape
+      rescue Exception => e
+        puts e
+      end
     end
 
     # rescrape lab
     puts "\nRescraping Lover's Lab"
     LoverInfo.can_recrape.find_each do |lover_info|
-      lover_info.rescrape
+      begin
+        lover_info.rescrape
+      rescue Exception => e
+        puts e
+      end
     end
 
     # rescrape workshop
     puts "\nRescraping Steam Workshop"
     WorkshopInfo.can_recrape.find_each do |workshop_info|
-      workshop_info.rescrape
+      begin
+        workshop_info.rescrape
+      rescue Exception => e
+        puts e
+      end
     end
   end
 end
